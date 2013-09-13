20088be11379bfd7c6e5ae539a301a033a7fb0eb
Michael Telahun Makonnen <mmakonnen@gmail.com>
2013-09-13 13:44:11 +0300

    Bugfix: hr_schedule: in the wizard for generating schedules make sure the contract has a schedule template

--- hr_schedule/wizard/generate_schedules.py
+++ hr_schedule/wizard/generate_schedules.py
@@ -66,6 +66,8 @@ class hr_schedule_generate(osv.TransientModel):
         sched_ids = []
         if len(data['employee_ids']) > 0:
             for ee in ee_obj.browse(cr, uid, data['employee_ids'], context=context):
+                if not ee.contract_id or not ee.contract_id.schedule_template_id:
+                    continue
                 sched = {
                     'name': ee.name,
                     'employee_id': ee.id,
