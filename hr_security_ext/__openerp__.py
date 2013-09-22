668a5e667637feb26e66dace92779f320167273a
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2013-09-22 11:46:37 -0400

    [FIX] PEP8

--- hr_security_ext/__openerp__.py
+++ hr_security_ext/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+#
 #
 #    Daniel Reis
 #    2012
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 
 {
--- hr_security_ext/hr.py
+++ hr_security_ext/hr.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+#
 #
 #    Daniel Reis, 2011
-#    
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
@@ -16,10 +16,10 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 from osv import fields, osv
-import reis_base as util 
+import reis_base as util
 
 
 class hr_employee(osv.osv):
@@ -27,13 +27,14 @@ class hr_employee(osv.osv):
     _columns = {
         'code': fields.char('Internal code', size=16),
     }
-    
+
     def name_get(self, cr, uid, ids, context=None):
-        return util.ext_name_get(self, cr, uid, ids, '[%(code)s] %(name)s', ['code','name'], context=context)
-    
+        return util.ext_name_get(self, cr, uid, ids, '[%(code)s] %(name)s', ['code', 'name'], context=context)
+
     def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=100):
-        return util.ext_name_search(self, cr, user, name, args, operator, context=context, limit=limit, 
-                                keys=['code','name'])
+        return util.ext_name_search(
+            self, cr, user, name, args, operator, context=context, limit=limit,
+            keys=['code', 'name'])
 hr_employee()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
