19517d112109084e3d2a041383ca6db93dc1b962
EL HADJI DEM <elhadji.dem@savoirfairelinux.com>
2014-01-30 16:59:21 -0500

    [IMP] add 'from .' to import hr line in employee_firstname module

--- hr_employee_firstname/__init__.py
+++ hr_employee_firstname/__init__.py
@@ -20,6 +20,6 @@
 #
 ##############################################################################
 
-import hr
+from . import hr
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
