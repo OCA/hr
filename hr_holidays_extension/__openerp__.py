d84b97ad2cd27ef95e46309547e326721fac99a6
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-07-31 23:35:13 -0400

    PEP8 fix on hr_holidays_extension with line length 80

--- hr_holidays_extension/__openerp__.py
+++ hr_holidays_extension/__openerp__.py
@@ -5,8 +5,8 @@
 #    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -27,18 +27,21 @@
 Extended Capabilities for HR Holidays (Leaves)
 ==============================================
 
-    * When calculating the number of leave days take into account the employee's schedule and
-      public holidays
-    * The 'Need Action' mechanism assumes the HR Manager approves leave requests
-    * Rename 'Leave Requests' menu item to 'My Leaves' (which is closer to its intent)
+    * When calculating the number of leave days take into account the
+      employee's schedule and public holidays
+    * The 'Need Action' mechanism assumes the HR Manager approves leave
+      requests
+    * Rename 'Leave Requests' menu item to 'My Leaves'
+      (which is closer to its intent)
     * Add a new menu item: All Leave Requests
     * New way of entering leaves based on the number of days requested, rather
       than by specifying a start and end date. You tell it how many days to
-      grant and it calculates the start and end dates based on the employee's schedule.
-    * Allow a manager to approve the leave requests of subordinates (manager must be
-      immediate superior of employee or manager of employee's department and have
-      leave approval rights)
-    """,
+      grant and it calculates the start and end dates based on the employee's
+      schedule.
+    * Allow a manager to approve the leave requests of subordinates (manager
+      must be immediate superior of employee or manager of employee's
+      department and have leave approval rights)
+""",
     'author': 'Michael Telahun Makonnen <mmakonnen@gmail.com>',
     'website': 'http://miketelahun.wordpress.com',
     'depends': [
