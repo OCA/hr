6ef2fc0c894a33fc28664a4fd62e8b11bdd2380d
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-07-31 23:38:22 -0400

    PEP8 fix on hr_report_payroll_net with line length 80

--- hr_report_payroll_net/report/payroll_net.py
+++ hr_report_payroll_net/report/payroll_net.py
@@ -4,8 +4,8 @@
 #    Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
