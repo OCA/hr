05ee143bf89c040c920df9532f57af4d3b5a3539
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-07-31 23:32:56 -0400

    PEP8 fix on hr_department_sequence with line length 80

--- hr_department_sequence/hr_department.py
+++ hr_department_sequence/hr_department.py
@@ -4,8 +4,8 @@
 #    Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -19,6 +19,7 @@
 #
 
 from openerp.osv import fields, orm
+from openerp.tools.translate import _
 
 
 class hr_department(orm.Model):
@@ -27,36 +28,69 @@ class hr_department(orm.Model):
     _inherit = 'hr.department'
 
     _columns = {
-        'code': fields.char('Code', size=64),
-        'sequence': fields.integer(
-            'Sequence', select=True,
-            help="Gives the sequence order when displaying a list of departments."
+        'code': fields.char(
+            'Code',
+            size=64,
         ),
-        'parent_id': fields.many2one('hr.department', 'Parent Department', select=True, ondelete='cascade'),
-        'parent_left': fields.integer('Left Parent', select=1),
-        'parent_right': fields.integer('Right Parent', select=1),
+        'sequence': fields.integer(
+            'Sequence',
+            select=True,
+            help="Gives the sequence order when displaying a list of "
+                 "departments.",
+        ),
+        'parent_id': fields.many2one(
+            'hr.department',
+            'Parent Department',
+            select=True,
+            ondelete='cascade',
+        ),
+        'parent_left': fields.integer(
+            'Left Parent',
+            select=1,
+        ),
+        'parent_right': fields.integer(
+            'Right Parent',
+            select=1,
+        ),
     }
 
     _parent_name = "parent_id"
     _parent_store = True
     _parent_order = 'sequence, name'
     _order = 'parent_left'
+
+    def _rec_message(self, cr, uid, ids, context=None):
+        return _('The code for the department must be unique per company!')
+
     _sql_constraints = [
-        ('code_uniq', 'unique(code, company_id)', 'The code for the department must be unique per company !'),
+        ('code_uniq', 'unique(code, company_id)', _rec_message),
     ]
 
     def name_get(self, cr, uid, ids, context=None):
         """
         Show department code with name
         """
+        if context is None:
+            context = {}
         if isinstance(ids, (int, long)):
             ids = [ids]
-        return [(record.id, '[%s] %s' % (record.code, record.name) if record.code else record.name)
-                for record in self.browse(cr, uid, ids, context=context or {})]
+        return [
+            (
+                record.id,
+                '[%s] %s' % (record.code, record.name)
+                if record.code else record.name
+            )
+            for record in self.browse(cr, uid, ids, context=context)
+        ]
 
-    def name_search(self, cr, uid, name='', args=None, operator='ilike',  context=None, limit=100):
+    def name_search(
+            self, cr, uid, name='', args=None, operator='ilike',
+            context=None, limit=100):
         if args is None:
             args = []
-        ids = self.search(cr, uid, ['|', ('code', 'ilike', name), ('name', 'ilike', name)] + args,
-                          limit=limit, context=context)
+        ids = self.search(
+            cr, uid,
+            ['|', ('code', 'ilike', name), ('name', 'ilike', name)] + args,
+            limit=limit, context=context
+        )
         return self.name_get(cr, uid, ids, context=context)
