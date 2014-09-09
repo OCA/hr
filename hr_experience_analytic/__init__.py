64e0abb7e990b6397b1c2d673794164792b23f6a
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2014-09-08 21:55:28 -0400

    [IMP] Remove useless line

--- hr_experience_analytic/__init__.py
+++ hr_experience_analytic/__init__.py
@@ -19,4 +19,3 @@
 #
 ###############################################################################
 from . import hr_experience_analytic
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
