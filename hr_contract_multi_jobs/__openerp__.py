75b37e047491c4c5fe53137308c1d1c91ff83fc0
dufresnedavid <david.dufresne@savoirfairelinux.com>
2014-11-12 11:23:28 -0500

    added module hr_contract_multi_jobs

---
+++ hr_contract_multi_jobs/__openerp__.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Contract Multi Jobs',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules/Human Resources',
+    'description': """
+Contract Multi Jobs
+===================
+In some companies, one employee may have more than one job position with a
+different hourly rate and a different cost for the company.
+
+This module enables multiple job positions for one contract
+
+Contributors
+------------
+* David Dufresne <david.dufresne@savoirfairelinux.com>
+* Pierre Lamarche <pierre.lamarche@savoirfairelinux.com>
+""",
+    'author': 'Savoir-faire Linux',
+    'website': 'https://www.savoirfairelinux.com/',
+    'depends': [
+        'hr_contract'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'hr_contract_view.xml',
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'js': ['static/src/js/timesheet.js', ],
+    'css': ['static/src/css/timesheet.css', ],
+    'qweb': ['static/src/xml/timesheet.xml', ],
+}
---
+++ hr_contract_multi_jobs/hr_contract_job.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class hr_contract_job(orm.Model):
+    """
+    An instance of a job position for an employee's contract.
+
+    This model may look trivial for now, but the purpose is that other modules
+    add fields to it, e.g. a salary class or a wage scale. These are fields
+    that depend on both the contract and the job position.
+    """
+    _name = 'hr.contract.job'
+    _description = 'Relational object between contract and job'
+
+    _columns = {
+        'name': fields.related(
+            'job_id',
+            'name',
+            string='Job',
+        ),
+        'job_id': fields.many2one(
+            'hr.job',
+            'Job',
+            required=True,
+            ondelete='cascade',
+        ),
+        'contract_id': fields.many2one(
+            'hr.contract',
+            'Contract',
+            required=True,
+            ondelete='cascade',
+        ),
+    }
