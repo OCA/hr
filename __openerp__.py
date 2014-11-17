ea652a145a42a49da3253eab914e5f2be7b2cf34
Chang Phui-Hock <phuihock@codekaki.com>
2014-11-17 17:58:31 +0800

    [FIX] license and updated module description.

--- __openerp__.py
+++ __openerp__.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Codekaki Systems (<http://codekaki.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': 'Duty Roster',
     'version': '0.1',
@@ -6,20 +24,23 @@
     'description': """
 Duty Roster
 ===========
+This module allows easy encoding of duty roster that is usually created and
+distributed in spreadsheet. It follows the common format used by
+manufacturing to enroll their general workers for duty each month.
 
-This is a generic module to allow easy management of staff duty roster.
-It should be used together with hr_holidays such that leaves application
-that spans multiple days takes into account of half/off duty days.
+Shift codes must be defined prior to creation of duty roster.  At the moment,
+shift code is exactly 1 letter. While it may seems like a non-ideal
+restriction, it allows duty roster to be created very quickly (with the help
+of custom widgets).
+
+The creation of a duty roster is usually responsiblity of department
+supervisor, and validated by department manager. However, this module does
+not distinguish supervisor from manager. User of this module should edit
+the groups accordingly.
 """,
-    'author': "CODEKAKI SYSTEMS (R49045/14)",
+    'author': "Codekaki Systems",
     'website': 'http://codekaki.com',
     'depends': ['hr'],
-    'images': [
-        'images/shift_code_tree.png',
-        'images/shift_code.png',
-        'images/duty_roster.png',
-        'images/duty_roster_err.png',
-    ],
     'data': [
         'security/ir.model.access.csv',
         'security/ir.rule.xml',
