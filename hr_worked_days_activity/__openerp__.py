aa510110f6b683147679db567aeee477445241ef
dufresnedavid <david.dufresne@savoirfairelinux.com>
2014-11-19 13:33:51 -0500

    Add hr_worked_days_activity

---
+++ hr_worked_days_activity/__openerp__.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Worked Days Activity',
+    'category': 'Localization',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules/Human Resources',
+    'description': """
+Worked Days Activity
+====================
+This module adds the activity field on payslip worked days. It is used to
+distinguish the leaves types and job positions in worked days.
+
+Contributors
+------------
+* David Dufresne <david.dufresne@savoirfairelinux.com>
+* Pierre Lamarche <pierre.lamarche@savoirfairelinux.com>
+""",
+    'author': 'Savoir-faire Linux',
+    'website': 'https://www.savoirfairelinux.com/',
+    'depends': [
+        'hr_payroll',
+    ],
+    'data': [
+        'data/hr_holiday_status_data.xml',
+        'data/hr_activity_data.xml',
+        'view/hr_holiday_status_view.xml',
+        'view/hr_payslip_view.xml',
+        'view/hr_activity_view.xml',
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ hr_worked_days_activity/hr_holiday_status.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class hr_holidays_status(orm.Model):
+    _inherit = 'hr.holidays.status'
+    _columns = {
+        'paid_leave': fields.boolean(
+            'Is Paid Leave',
+            help="Whether this leave is paid or not",
+        ),
+        'activity_ids': fields.one2many(
+            'hr.activity',
+            'leave_id',
+            'Activity',
+        ),
+    }
+    _defaults = {
+        # Creates an leave type automatically
+        'activity_ids': [
+            {'type': 'leave'}
+        ]
+    }
