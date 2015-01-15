73e5dbc89e20cd61d6d4a1d7fb14a146561b3dbb
Adrian-Ioan Vasile <yoyo@opennet.ro>
2015-01-15 14:24:16 +0200

    - ported hr_webcam to 8.0 - updated __openerp__.py and hr_webcam_view.xml to reflect changes to 8.0 api - modified hr_webcam.css (wider camera viewport)

--- hr_webcam/__openerp__.py
+++ hr_webcam/__openerp__.py
@@ -1,12 +1,12 @@
-# -*- coding:utf-8 -*-
+#-*- coding:utf-8 -*-
 #
 #
 #    Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 #    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -50,7 +50,6 @@ Capture employee pictures with an attached web cam.
         'hr_webcam_data.xml',
         'hr_webcam_view.xml',
     ],
-    'test': [
-    ],
-    'installable': False,
+    'installable': True,
+    'active': False,
 }
