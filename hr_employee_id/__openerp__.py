60ab06d28ecaa831a5f03899e76203be3f780d3f
dufresnedavid <david.dufresne@savoirfairelinux.com>
2015-03-06 16:15:39 -0500

    Fix PEP8 E501

--- hr_employee_id/__openerp__.py
+++ hr_employee_id/__openerp__.py
@@ -28,7 +28,8 @@ Employee Identification Numbers
 ===============================
 Company wide unique employee ID
     """,
-    'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>,Odoo Community Association (OCA)",
+    'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>, "
+    "Odoo Community Association (OCA)",
     'website': 'http://miketelahun.wordpress.com',
     'depends': [
         'hr',
