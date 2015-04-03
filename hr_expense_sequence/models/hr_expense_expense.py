3d8f50f39f76cbeb07136c75d6e65dc4132d7aa2
Alexis de Lattre <alexis@via.ecp.fr>
2015-04-03 12:25:51 +0200

    Write expense number on account move

--- hr_expense_sequence/models/hr_expense_expense.py
+++ hr_expense_sequence/models/hr_expense_expense.py
@@ -16,3 +16,11 @@ class HrExpense(models.Model):
         if vals.get('number', '/') == '/':
             vals['number'] = self.env['ir.sequence'].get('hr.expense')
         return super(HrExpense, self).create(vals)
+
+    @api.model
+    def account_move_get(self, expense_id):
+        '''Write expense number on account move'''
+        vals = super(HrExpense, self).account_move_get(expense_id)
+        expense = self.browse(expense_id)
+        vals['ref'] = expense.number
+        return vals
