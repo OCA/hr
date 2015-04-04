6381c70750cf4e8c8706d571fc1bbcacba6eaeef
sebalix <seb@usr-src.org>
2015-04-04 10:32:31 +0200

    [ADD] Move 'hr_language' out of '__unported__'

---
+++ hr_language/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    "name": "Language Management",
+    "version": "0.1",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "description": """
+    This module allows you to manage your employee languages.
+    """,
+    "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
+    "website": "http://www.savoirfairelinux.com",
+    "depends": ["hr"],
+    'data': ["security/ir.model.access.csv",
+             "hr_language_view.xml",
+             ],
+    "demo": [],
+    "test": [],
+    'installable': False,
+    "auto_install": False,
+    "images": [],
+}
