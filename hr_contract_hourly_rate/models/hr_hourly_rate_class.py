617b664eb0034ff17731aa0413da9e689ab70d1c
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2015-04-20 13:42:11 +0300

    Fix fields declarations, line endings.

--- hr_contract_hourly_rate/models/hr_hourly_rate_class.py
+++ hr_contract_hourly_rate/models/hr_hourly_rate_class.py
@@ -27,23 +27,23 @@ class hr_hourly_rate_class(models.Model):
     _name = 'hr.hourly.rate.class'
     _description = 'Hourly rate class'
 
-    name = fields.char(string='Class Name', required=True, index=True)
+    name = fields.Char(string='Class Name', required=True, index=True)
     line_ids = fields.One2many('hr.hourly.rate',
                                'class_id',
-                               string='Hourly Rates'),
+                               string='Hourly Rates')
     contract_job_ids = fields.One2many('hr.contract.job',
                                        'hourly_rate_class_id',
                                        string='Contract Jobs')
 
     @api.multi
     @api.constrains('line_ids')
-    def _check_overlapping_rates(self, cr, uid, ids, context=None):
+    def _check_overlapping_rates(self):
         """
         Checks if a class has two rates that overlap in time.
         """
         for hourly_rate_class in self:
             for r1, r2 in permutations(hourly_rate_class.line_ids, 2):
-                if r1.date_end and (r1.date_start <= r2.date_start) and
+                if r1.date_end and (r1.date_start <= r2.date_start) and \
                 (r2.date_start <= r1.date_end):
                     return exceptions.Warning(
                         _("Error! You cannot have overlapping rates"))
