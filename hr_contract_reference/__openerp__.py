9115af7da7f154feb52cf7d3aa8104158a9c7629
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2015-04-21 10:24:12 +0300

    Port hr_contract_reference to V8 and new api.

--- hr_contract_reference/__openerp__.py
+++ hr_contract_reference/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
-#
+##############################################################################
 #
 #    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 #    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -17,8 +18,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-
+##############################################################################
 {
     "name": "HR Contract Reference",
     "version": "1.0",
@@ -35,8 +35,8 @@ This module provides :
     "license": "AGPL-3",
     "depends": ["hr_contract"],
     "data": [
-        'hr_contract_view.xml',
-        'hr_contract_sequence.xml',
+        'views/hr_contract_view.xml',
+        'data/hr_contract_sequence.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
--- hr_contract_reference/models/hr_contract.py
+++ hr_contract_reference/models/hr_contract.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
-#
+##############################################################################
 #
 #    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 #    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -17,30 +18,21 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
+##############################################################################
 
-from openerp.osv import fields, orm
+from openerp import models, fields, api
 
 
-class hr_contract(orm.Model):
-
+class hr_contract(models.Model):
     _inherit = 'hr.contract'
 
-    _columns = {
-        'name': fields.char(
-            'Contract Reference',
-            size=32,
-            required=False,
-            readonly=True,
-        ),
-    }
+    name = fields.Char('Contract Reference', required=False,
+            readonly=True, copy=False)
 
-    def create(self, cr, uid, vals, context=None):
-
-        cid = super(hr_contract, self).create(cr, uid, vals, context)
-        if cid:
-            ref = self.pool.get('ir.sequence').next_by_code(
-                cr, uid, 'contract.ref', context=context)
-            self.pool.get('hr.contract').write(
-                cr, uid, cid, {'name': ref}, context=context)
-        return cid
+    @api.model
+    def create(self, vals):
+        contract = super(hr_contract, self).create(vals)
+        if contract:
+            contract[0].name = self.env['ir.sequence'].next_by_code(
+                'contract.ref')
+        return contract
