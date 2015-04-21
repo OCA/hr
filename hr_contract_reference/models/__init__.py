a419f65d0d7c90f44a05e1378ea5cfdaf80a3713
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2015-04-21 10:24:12 +0300

    Port hr_contract_reference to V8 and new api.

--- hr_contract_reference/models/__init__.py
+++ hr_contract_reference/models/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
+##############################################################################
 #
-#
-#    Copyright (C) 2011 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 #    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -17,6 +18,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
+##############################################################################
 
 from . import hr_contract
