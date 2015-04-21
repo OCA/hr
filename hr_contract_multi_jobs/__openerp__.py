c6a9d4581ca0023372e74279f75e1bdcc72c87e6
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2015-04-21 13:48:01 +0300

    Update readme.

--- hr_contract_multi_jobs/__openerp__.py
+++ hr_contract_multi_jobs/__openerp__.py
@@ -19,11 +19,13 @@
 ##############################################################################
 
 {
-    'name': 'Contract Multi Jobs',
+    'name': 'HR Contract Multi Jobs',
     'version': '1.0',
     'license': 'AGPL-3',
     'category': 'Generic Modules/Human Resources',
-    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
+    'author': 'Savoir-faire Linux, '
+              'Fekete Mihai (Forest and Biomass Services Romania, '
+              'Odoo Community Association (OCA)',
     'website': 'https://www.savoirfairelinux.com/',
     'depends': [
         'hr_contract'
