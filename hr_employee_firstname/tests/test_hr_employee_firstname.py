74f97de5083530a16ebe2f7449e3c00ab8741716
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2015-04-27 11:11:19 +0300

    Fix travis errors on tests.

--- hr_employee_firstname/tests/test_hr_employee_firstname.py
+++ hr_employee_firstname/tests/test_hr_employee_firstname.py
@@ -28,15 +28,23 @@ class TestEmployeeFirstname(TransactionCase):
         super(TestEmployeeFirstname, self).setUp()
         self.employee_model = self.env['hr.employee']
 
-        # Create 3 employees to concatenate the firstname and lastname in name_related
-        self.employee1_id = self.employee_model.create({'firstname': 'Jan', 'lastname': 'Van Eyck'})
-        self.employee2_id = self.employee_model.create({'firstname': 'Jean-Pierre', 'lastname': 'Carnaud'})
-        self.employee3_id = self.employee_model.create({'firstname': 'Famke', 'lastname': 'Jenssens'})         
+        # Create 3 employees to concatenate the firstname and lastname
+        # in name_related
+        self.employee1_id = self.employee_model.create(
+            {'firstname': 'Jan', 'lastname': 'Van Eyck'})
+        self.employee2_id = self.employee_model.create(
+            {'firstname': 'Jean-Pierre', 'lastname': 'Carnaud'})
+        self.employee3_id = self.employee_model.create(
+            {'firstname': 'Famke', 'lastname': 'Jenssens'})
 
-        # Create 3 employees for split the name_related to firstname and lastname
-        self.employee10_id = self.employee_model.create({'name': ' Jan Van Eyck', 'firstname': ' ', 'lastname':  ' '})
-        self.employee20_id = self.employee_model.create({'name': 'Jean-Pierre Carnaud', 'firstname': ' ', 'lastname': ' '})
-        self.employee30_id = self.employee_model.create({'name': 'FamkeJenssens', 'firstname': ' ', 'lastname': ' '})         
+        # Create 3 employees for split the name_related to
+        # firstname and lastname
+        self.employee10_id = self.employee_model.create(
+            {'name': ' Jan Van Eyck', 'firstname': ' ', 'lastname':  ' '})
+        self.employee20_id = self.employee_model.create(
+            {'name': 'Jean-Pierre Carnaud', 'firstname': ' ', 'lastname': ' '})
+        self.employee30_id = self.employee_model.create(
+            {'name': 'FamkeJenssens', 'firstname': ' ', 'lastname': ' '})
 
     def test_get_name(self):
         """
@@ -45,13 +53,14 @@ class TestEmployeeFirstname(TransactionCase):
         """
         # Check for employee1
         self.assertTrue(self.employee1_id.name_related == 'Jan Van Eyck')
-        
+
         # Check for employee2
-        self.assertTrue(self.employee2_id.name_related == 'Jean-Pierre Carnaud')
-        
+        self.assertTrue(self.employee2_id.name_related == 'Jean-Pierre '
+                                                          'Carnaud')
+
         # Check for employee3
         self.assertTrue(self.employee3_id.name_related == 'Famke Jenssens')
-        
+
     def test_auto_init_name(self):
         """
         Validate the _auto_init if the name related is split
@@ -60,11 +69,11 @@ class TestEmployeeFirstname(TransactionCase):
         # Check for employee10
         self.assertTrue(self.employee10_id.firstname == "Jan")
         self.assertTrue(self.employee10_id.lastname == 'Van Eyck')
-        
+
         # Check for employee20
         self.assertTrue(self.employee20_id.firstname == 'Jean-Pierre')
         self.assertTrue(self.employee20_id.lastname == 'Carnaud')
-        
+
         # Check for employee30
         self.assertTrue(self.employee30_id.firstname == 'FamkeJenssens')
         self.assertTrue(self.employee30_id.lastname == ' ')
