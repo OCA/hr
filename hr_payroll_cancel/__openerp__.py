bba1a966b4584d194ad52e71215863771798bba3
Nhomar Hernandez <nhomar@gmail.com>
2015-07-25 16:16:55 -0500

    [IMP] changing versions forcing *.5 on versions to have them as reference. [IMP] .version.py now works with ReGex

--- hr_payroll_cancel/__openerp__.py
+++ hr_payroll_cancel/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "Hr Payroll Cancel", 
-    "version": "1.0", 
+    "version": "1.5", 
     "author": "Vauxoo", 
     "category": "Localization/Mexico", 
     "website": "http://www.vauxoo.com/", 
