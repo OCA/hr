bd4d5aaa40d162d14616e4e9af684ab716ccd980
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:43 -0500

    [IMP] Update hr_payroll_cancel module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- hr_payroll_cancel/__openerp__.py
+++ hr_payroll_cancel/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "Hr Payroll Cancel", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Localization/Mexico", 
     "website": "http://www.vauxoo.com/", 
