9c55d4f5364bb00f57bd332575f7560e751f96dc
Moises Lopez <moylop260@vauxoo.com>
2015-08-10 14:36:04 -0500

    [REF] __openerp__.py: Fix pep8 trailing whitespace

--- hr_payroll_cancel/__openerp__.py
+++ hr_payroll_cancel/__openerp__.py
@@ -24,26 +24,26 @@
 #
 ##############################################################################
 {
-    "name": "Hr Payroll Cancel", 
-    "version": "1.6", 
-    "author": "Vauxoo", 
-    "category": "Localization/Mexico", 
-    "website": "http://www.vauxoo.com/", 
-    "license": "AGPL-3", 
+    "name": "Hr Payroll Cancel",
+    "version": "1.6",
+    "author": "Vauxoo",
+    "category": "Localization/Mexico",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
     "depends": [
         "hr_payroll"
-    ], 
-    "demo": [], 
+    ],
+    "demo": [],
     "data": [
-        "hr_payslip_view.xml", 
-        "hr_payslip_workflow.xml", 
+        "hr_payslip_view.xml",
+        "hr_payslip_workflow.xml",
         "test/update_payroll_workflow.yml"
-    ], 
-    "test": [], 
-    "js": [], 
-    "css": [], 
-    "qweb": [], 
-    "installable": True, 
-    "auto_install": False, 
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
     "active": False
 }
