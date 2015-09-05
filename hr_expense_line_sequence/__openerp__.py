7c80504a959afb603738478ddee893b310a175aa
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2015-09-05 17:59:15 -0400

    [IMP] Version and description

--- hr_expense_line_sequence/__openerp__.py
+++ hr_expense_line_sequence/__openerp__.py
@@ -21,12 +21,75 @@
 
 {
     "name": "Sequence on expense line",
-    "version": "1.0",
+    "version": "7.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "category": "Human Resources",
     "description": """
-        This module adds a sequence number field on the expense line.
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===========================
+Sequence on HR expense line
+===========================
+
+This module adds a sequence number field on the expense line.
+
+Usage
+=====
+
+To use this module, you need to:
+
+* go to HR > Expenses > Expenses
+* create a new expense with 2 lines and set the sequence
+* print the HR Expense report to see that the lines are sorted by the sequence 
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/116/7.0
+
+For further information, please visit:
+
+* https://www.odoo.com/forum/help-1
+
+Known issues / Roadmap
+======================
+
+* ...
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/hr/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
+`here <https://github.com/OCA/hr/issues/new?body=module:%20hr_expense_line_sequence%0Aversion:%207.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sandy Carter <sandy.carter@savoirfairelinux.com>
+* Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
     """,
     "depends": ['hr_expense'],
     "data": [
