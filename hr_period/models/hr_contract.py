b1aa5dccbf6894a236b987b5ea1ac0b1213145f4
Salton <smassally@idtlabs.sl>
2015-10-03 21:51:27 +0000

    Migrate hr_period from 7.0 to 8.0

--- hr_period/models/hr_contract.py
+++ hr_period/models/hr_contract.py
@@ -5,8 +5,7 @@
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
-#    by
-#    the Free Software Foundation, either version 3 of the License, or
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,16 +17,16 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp.osv import orm, fields
+from openerp import models, fields
 from .hr_fiscal_year import get_schedules
 
 
-class HrContract(orm.Model):
+class HrContract(models.Model):
     _inherit = 'hr.contract'
 
-    _columns = {
-        # Add semi-monthly to payroll schedules
-        'schedule_pay': fields.selection(
-            get_schedules, 'Scheduled Pay', select=True),
-    }
+    # Add semi-monthly to payroll schedules
+    shedule_pay = fields.Selection(
+        get_schedules,
+        'Scheduled Pay',
+        index=True
+    )
