8d8cc0733f5548be2026d3488778986d3ca41f13
Salton <smassally@idtlabs.sl>
2015-10-03 21:51:27 +0000

    Migrate hr_period from 7.0 to 8.0

--- hr_period/models/hr_payslip_employees.py
+++ hr_period/models/hr_payslip_employees.py
@@ -5,8 +5,7 @@
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
-#    by
-#    the Free Software Foundation, either version 3 of the License, or
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,21 +17,22 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 from .hr_fiscal_year import get_schedules
 
 
-class HrPayslipEmployees(orm.TransientModel):
+class HrPayslipEmployees(models.TransientModel):
 
     _inherit = 'hr.payslip.employees'
 
-    _columns = {
-        'company_id': fields.many2one(
-            'res.company', 'Company', readonly=True
-        ),
-        'schedule_pay': fields.selection(
-            get_schedules, 'Scheduled Pay', readonly=True
-        ),
-    }
+    company_id = fields.Many2one(
+        'res.company',
+        'Company',
+        readonly=True
+    )
+    schedule_pay = fields.Selection(
+        get_schedules,
+        'Scheduled Pay',
+        readonly=True
+    )
