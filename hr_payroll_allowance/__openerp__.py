dc4267663cf915b7f5b4e64e1670bb449ffb90b9
Salton Massally <salton.massally@gmail.com>
2015-10-05 09:50:40 +0000

    Update __openerp__.py
    add OCA as author

--- hr_payroll_allowance/__openerp__.py
+++ hr_payroll_allowance/__openerp__.py
@@ -3,7 +3,8 @@
     "name" : "Employee Payroll Allowance",
     "version" : "1.0",
     "depends" : ["hr_payroll", "hr_employee_service_length"],
-    "author" : "IDT Labs",
+    'author': "Salton Massally <smassally@idtlabs.sl>, "
+               "Odoo Community Association (OCA)",
     'category': 'Human Resources',
     "summary": "Allows us to easily create payroll allowance",
     "data" : [
