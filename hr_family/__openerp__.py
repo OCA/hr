e5abd2f8300c51654c282d608aa2dc59f3d03228
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-10-08 10:20:42 +0200

    [CHG][hr_family] Change version

--- hr_family/__openerp__.py
+++ hr_family/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Employee Family Information',
-    'version': '1.0',
+    'version': '8.0.1.1.0',
     'category': 'Generic Modules/Human Resources',
     'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>,"
               "Odoo Community Association (OCA)",
