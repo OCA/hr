b62c633f88c20464a18fce40eee78b3b9dcfe6fb
Salton <smassally@idtlabs.sl>
2015-10-09 19:34:40 +0000

    Apply the followig suggested changes: * Decouple from hr_contract_usability * Fix Pep8 warnings

--- hr_employee_service_length/models/hr_employee.py
+++ hr_employee_service_length/models/hr_employee.py
@@ -19,49 +19,55 @@
 ###############################################################################
 import operator
 from functools import partial
-from datetime import datetime, date, timedelta
+from datetime import date
 from dateutil.relativedelta import relativedelta
 
-from openerp import fields, models, _, api
-from openerp.exceptions import Warning, ValidationError
+from openerp import fields, models, api
+from openerp.exceptions import ValidationError
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     initial_employment_date = fields.Date(
-                                          'Initial Date of Employment',
         help='Date of first employment if it was before the start of the '
              'first contract in the system.',
     )
     date_start = fields.Date(
-                             'Start Date', 
-                             readonly=True, 
-                             store=True, 
-                             compute='_compute_service_details'
-                             )    
+        'Start Date',
+        readonly=True,
+        store=True,
+        compute='_compute_service_details'
+    )
     length_of_service = fields.Float(
-                                     'Length of Service', 
-                                     readonly=True, 
-                                     compute='_compute_employment_length'
-                                     )
+        'Length of Service',
+        readonly=True,
+        compute='_compute_employment_length'
+    )
     length_of_service_str = fields.Char(
-                                        'Length of Service', 
-                                        readonly=True,
-                                        compute='_compute_employment_length'
-                                        )
-    
+        'Length of Service',
+        readonly=True,
+        compute='_compute_employment_length'
+    )
+
+    def _first_contract(self):
+        return self.contract_ids\
+            .sorted(key=operator.itemgetter('date_start', 'id'))[0]
+
     @api.one
-    @api.constrains('initial_employment_date', 'first_contract_id')
+    @api.constrains('initial_employment_date', 'contract_ids',
+                    'contract_ids.date_start')
     def _check_initial_employment_date(self):
-        if self.initial_employment_date and self.first_contract_id:
-            if fields.Date.from_string(self.initial_employment_date) > \
-            fields.Date.from_string(self.first_contract_id.date_start):
+        if self.initial_employment_date and len(self.contract_ids):
+            initial_dt = fields.Date.from_string(self.initial_employment_date)
+            first_contract_dt = fields.Date.from_string(
+                self._first_contract().date_start)
+            if initial_dt > first_contract_dt:
                 raise ValidationError("The initial employment date cannot be "
-                      "after the first contract in the system.\n")
-    
+                                      "after the first contract in the system")
+
     @staticmethod
-    def _get_contract_interval(contract, dt_ref=None): 
+    def _get_contract_interval(contract, dt_ref=None):
         if not dt_ref:
             dt_ref = date.today()
         end_date = dt_ref
@@ -71,7 +77,7 @@ class HrEmployee(models.Model):
                 end_date = dt_ref
         return relativedelta(end_date,
                              fields.Date.from_string(contract.date_start))
-    
+
     def get_service_length_delta_at_time(self, dt=None):
         '''
         get's the service length for an employee at a specific date
@@ -82,30 +88,31 @@ class HrEmployee(models.Model):
             dt_today = date.today()
         else:
             dt_today = isinstance(dt, date) and dt \
-                                    or fields.Date.from_string(dt)        
-        dt_date_start = fields.Date.from_string(self.date_start)    
-        mapfunc = partial(self._get_contract_interval, dt_ref=dt_today)    
+                or fields.Date.from_string(dt)
+        dt_date_start = fields.Date.from_string(self.date_start)
+        mapfunc = partial(self._get_contract_interval, dt_ref=dt_today)
         delta = relativedelta(dt_today, dt_today)
-        if not len(self.contract_ids): # if employee has no contracts
+        if not len(self.contract_ids):  # if employee has no contracts
             delta = relativedelta(dt_today, dt_date_start)
         else:
             # if employee has contracts let's first find the months for which
             # employee has been employed according to the contracts
             deltas = self.contract_ids \
-                        .sorted(key=operator.itemgetter('date_start', 'id')) \
-                        .mapped(mapfunc)
-            
+                .sorted(key=operator.itemgetter('date_start', 'id')) \
+                .mapped(mapfunc)
+
             # if initial employment date is before the first contract date then
             # let's take the difference into account
             if self.date_start == self.initial_employment_date:
-                deltas.append(relativedelta(fields.Date.
-                  from_string(self.first_contract_id.date_start), dt_date_start))
+                first_contract_dt = fields.Date.from_string(
+                    self._first_contract().date_start)
+                deltas.append(relativedelta(first_contract_dt, dt_date_start))
             for d in deltas:
                 delta += d
         return delta
-    
-    @api.one 
-    @api.depends('contract_ids', 'initial_employment_date', 
+
+    @api.one
+    @api.depends('contract_ids', 'initial_employment_date',
                  'contract_ids.date_start', 'contract_ids.employee_id')
     def _compute_service_details(self):
         date_start = False
@@ -116,26 +123,26 @@ class HrEmployee(models.Model):
             else:
                 date_start = self.initial_employment_date
         else:
-            first_contract = self.first_contract_id
+            first_contract = self._first_contract()
             date_start = first_contract.date_start
             if self.initial_employment_date \
-            and fields.Date.from_string(first_contract.date_start) \
-            > fields.Date.from_string(self.initial_employment_date):
+                and fields.Date.from_string(first_contract.date_start) \
+                    > fields.Date.from_string(self.initial_employment_date):
                 date_start = self.initial_employment_date
         self.date_start = date_start
-    
+
     @staticmethod
     def _convert_timedelta_to_str(delta):
-        str_since = ''  
-        year_label = delta.years > 1 and 'yrs' or 'yr'   
-        month_label = delta.months > 1 and 'mnths' or 'mnth' 
+        str_since = ''
+        year_label = delta.years > 1 and 'yrs' or 'yr'
+        month_label = delta.months > 1 and 'mnths' or 'mnth'
         year_str = month_str = ''
         if delta.years:
             year_str = '%s %s' % (delta.years, year_label)
-                           
+
         if delta.months:
             month_str = '%s %s' % (delta.months, month_label)
-        
+
         str_since += year_str
         if year_str and month_str:
             str_since += ' and ' + month_str
@@ -143,15 +150,15 @@ class HrEmployee(models.Model):
             str_since += month_str
         return str_since
 
-    @api.one 
+    @api.one
     def _compute_employment_length(self):
-        dt =  'date_now' in self.env.context and self.env.context['date_now']\
-                or fields.Date.today()
-            
+        dt = 'date_now' in self.env.context and self.env.context['date_now']\
+            or fields.Date.today()
+
         delta = self.get_service_length_delta_at_time(dt)
         if not delta:
             return
         length_of_service = float(((delta.years * 12) + delta.months))\
-         + delta.days/30.0
+            + delta.days/30.0
         self.length_of_service = round(length_of_service, 1)
         self.length_of_service_str = self._convert_timedelta_to_str(delta)
--- hr_employee_service_length/tests/test_employee_service_length.py
+++ hr_employee_service_length/tests/test_employee_service_length.py
@@ -24,58 +24,55 @@ from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 from openerp.exceptions import ValidationError
 
 
-
-class test_employee_service_length(common.TransactionCase):
+class TestEmployeeServiceLength(common.TransactionCase):
 
     def setUp(self):
-        super(test_employee_service_length, self).setUp()
+        super(TestEmployeeServiceLength, self).setUp()
         self.contract_model = self.env["hr.contract"]
         self.employee_model = self.env['hr.employee']
 
         # Create an employees
         self.employee = self.employee_model.create({
-                'name': 'Employee 1',
-            })
-        
+            'name': 'Employee 1',
+        })
+
         dt = date.today() - relativedelta(months=6)
-        self.six_months_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)  
-                
+        self.six_months_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)
+
         dt = date.today() - relativedelta(years=1)
-        self.one_year_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)  
-        
+        self.one_year_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)
+
         dt = date.today() - relativedelta(years=2)
-        self.two_year_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)  
-        
+        self.two_year_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)
+
         dt = date.today() - relativedelta(years=3)
-        self.three_year_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)  
-        
+        self.three_year_ago = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)
+
         dt = date.today() + relativedelta(years=1)
-        self.one_year_in_future = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)  
-        
+        self.one_year_in_future = dt.strftime(DEFAULT_SERVER_DATE_FORMAT)
+
         self.now = date.today().strftime(DEFAULT_SERVER_DATE_FORMAT)
-        
-        
+
     def test_initial_employment_date_only_supplied(self):
         # if we are supplying only the initial employment date that should
         # be used to calculate service length
-        
-        self.employee.write({'initial_employment_date' : self.one_year_ago})
+        self.employee.write({'initial_employment_date': self.one_year_ago})
         self.assertAlmostEqual(self.employee.length_of_service, 12.0)
-        
+
     def test_contract_start_date_only_supplied(self):
-        # initial employment date not supplied but we do have a contract 
+        # initial employment date not supplied but we do have a contract
         # start date
         self.contract_model.create(
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 1',
-                'date_start' : self.one_year_ago,
-                'date_end' : self.one_year_in_future,
-                'wage' : 5000
+                'date_start': self.one_year_ago,
+                'date_end': self.one_year_in_future,
+                'wage': 5000
             }
         )
         self.assertAlmostEqual(self.employee.length_of_service, 12.0)
-    
+
     def test_inital_employment_before_first_contract_date(self):
         # initial employment date is supplied and we have a contract
         # however in this case the initial employment date is before that
@@ -84,14 +81,14 @@ class test_employee_service_length(common.TransactionCase):
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 1',
-                'date_start' : self.one_year_ago,
-                'date_end' : self.one_year_in_future,
-                'wage' : 5000
+                'date_start': self.one_year_ago,
+                'date_end': self.one_year_in_future,
+                'wage': 5000
             }
         )
-        self.employee.write({'initial_employment_date' : self.two_year_ago})
+        self.employee.write({'initial_employment_date': self.two_year_ago})
         self.assertAlmostEqual(self.employee.length_of_service, 24.0)
-        
+
     def test_inital_employment_after_first_contract_date(self):
         # initial employment date is supplied and we have a contract
         # however in this case the initial employment date is after that
@@ -100,23 +97,24 @@ class test_employee_service_length(common.TransactionCase):
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 1',
-                'date_start' : self.two_year_ago,
-                'date_end' : self.one_year_in_future,
-                'wage' : 5000
+                'date_start': self.two_year_ago,
+                'date_end': self.one_year_in_future,
+                'wage': 5000
             }
         )
-        with self.assertRaises(ValidationError): 
-            self.employee.write({'initial_employment_date' : self.one_year_ago})
-        
+        with self.assertRaises(ValidationError):
+            self.employee.write({'initial_employment_date': self.one_year_ago})
+
     def test_length_of_service_at_period_in_history(self):
-        # we want to know the employee's length of service at a point in history      
-        self.employee.write({'initial_employment_date' : self.one_year_ago})
-        self.employee = self.employee.with_context(date_now=self.six_months_ago)
+        # we want to know employee length of service at a point in history
+        self.employee.write({'initial_employment_date': self.one_year_ago})
+        self.employee = self.employee\
+            .with_context(date_now=self.six_months_ago)
         self.assertAlmostEqual(self.employee.length_of_service, 6.0)
-    
+
     def test_length_of_service_at_period_in_future(self):
-        # we want to know how long employee would have worked x months from now  
-        self.employee.write({'initial_employment_date' : self.one_year_ago})
+        # we want to know how long employee would have worked x months from now
+        self.employee.write({'initial_employment_date': self.one_year_ago})
         self.employee = self.employee\
             .with_context(date_now=self.one_year_in_future)
         self.assertAlmostEqual(self.employee.length_of_service, 24.0)
@@ -127,9 +125,9 @@ class test_employee_service_length(common.TransactionCase):
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 1',
-                'date_start' : self.two_year_ago,
-                'date_end' : self.one_year_ago,
-                'wage' : 5000
+                'date_start': self.two_year_ago,
+                'date_end': self.one_year_ago,
+                'wage': 5000
             }
         )
         dt = date.today() - relativedelta(years=1) + relativedelta(days=1)
@@ -137,9 +135,9 @@ class test_employee_service_length(common.TransactionCase):
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 2',
-                'date_start' : dt.strftime(DEFAULT_SERVER_DATE_FORMAT),
-                'date_end' : self.now,
-                'wage' : 5000
+                'date_start': dt.strftime(DEFAULT_SERVER_DATE_FORMAT),
+                'date_end': self.now,
+                'wage': 5000
             }
         )
         self.assertAlmostEqual(self.employee.length_of_service, 24.0)
@@ -150,9 +148,9 @@ class test_employee_service_length(common.TransactionCase):
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 1',
-                'date_start' : self.three_year_ago,
-                'date_end' : self.two_year_ago,
-                'wage' : 5000
+                'date_start': self.three_year_ago,
+                'date_end': self.two_year_ago,
+                'wage': 5000
             }
         )
         dt = date.today() - relativedelta(years=1) + relativedelta(days=1)
@@ -160,9 +158,9 @@ class test_employee_service_length(common.TransactionCase):
             {
                 'employee_id': self.employee.id,
                 'name': 'Contract 2',
-                'date_start' : dt.strftime(DEFAULT_SERVER_DATE_FORMAT),
-                'date_end' : self.now,
-                'wage' : 5000
+                'date_start': dt.strftime(DEFAULT_SERVER_DATE_FORMAT),
+                'date_end': self.now,
+                'wage': 5000
             }
         )
         self.assertAlmostEqual(self.employee.length_of_service, 24.0)
