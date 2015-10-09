f71d834394c4148420d135d6e90d8fbb4fdd67d6
Salton <smassally@idtlabs.sl>
2015-10-09 18:29:01 +0000

    apply suggeste changes * Use identification_id provided by hr addon rather than employee_no * use new api in configuration * pep8 changes

--- hr_employee_id/models/res_config.py
+++ hr_employee_id/models/res_config.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Business Applications
-#    Copyright (C) 2004-2012 OpenERP S.A. (<http://openerp.com>).
+#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,50 +18,51 @@
 #
 ##############################################################################
 
-from openerp import models, fields, SUPERUSER_ID
+from openerp import models, fields, api
 
 
-class human_resources_configuration(models.TransientModel):
+class HumanResourcesConfiguration(models.TransientModel):
     _inherit = 'hr.config.settings'
 
-    employee_id_gen_method = fields.Selection((('random', 'Random'),
-                                               ('sequence', 'Sequence')), string="ID Generation Method", default='random',
-                                              help="Method by which employee ID is generated; \
-                                              Random - IDs are generated randomly \
-                                              Sequence - IDs are generated based on a supplied sequence")
-    
-    employee_id_random_digits = fields.Integer('# of Digits', default=5, help="Number of digits making up the ID")
-    employee_id_sequence = fields.Many2one('ir.sequence', 'Sequence', help="Pattern to be used for used for ID Generation")
-    
-    def get_default_employee_id_gen_method(self, cr, uid, ids, context=None):
-        employee_id_gen_method  = self.pool.get("ir.config_parameter").get_param(cr, uid, "hr_employee_id.employee_id_gen_method", default=None, context=context)
-        return {'employee_id_gen_method': employee_id_gen_method  or False}
+    def _default_id_sequence(self):
+        sequence = self.env.ref('hr_employee_id.seq_employeeid_ref')
+        return sequence and sequence.id or False
 
-    def set_employee_id_gen_method (self, cr, uid, ids, context=None):
-        config_parameters = self.pool.get("ir.config_parameter")
-        for record in self.browse(cr, uid, ids, context=context):
-            config_parameters.set_param(cr, uid, "hr_employee_id.employee_id_gen_method", record.employee_id_gen_method  or '', context=context)
-            
-    def get_default_employee_id_random_digits(self, cr, uid, ids, context=None):
-        employee_id_random_digits  = self.pool.get("ir.config_parameter").get_param(cr, uid, "hr_employee_id.employee_id_random_digits", default=None, context=context)
-        return {'employee_id_random_digits': int(employee_id_random_digits)  or False}
+    employee_id_gen_method = fields.Selection(
+        [
+            ('random', 'Random'),
+            ('sequence', 'Sequence'),
+        ],
+        string="ID Generation Method",
+        default='random'
+    )
+    employee_id_random_digits = fields.Integer(
+        '# of Digits', default=5,
+        help="Number of digits making up the ID"
+    )
+    employee_id_sequence = fields.Many2one(
+        'ir.sequence', 'Sequence',
+        help="Pattern to be used for used for ID Generation",
+        default=_default_id_sequence
+    )
 
-    def set_employee_id_random_digits (self, cr, uid, ids, context=None):
-        config_parameters = self.pool.get("ir.config_parameter")
-        for record in self.browse(cr, uid, ids, context=context):
-            config_parameters.set_param(cr, uid, "hr_employee_id.employee_id_random_digits", record.employee_id_random_digits  or '', context=context)
-            
-    def get_default_employee_id_sequence(self, cr, uid, ids, context=None):
-        employee_id_sequence  = self.pool.get("ir.config_parameter").get_param(cr, uid, "hr_employee_id.employee_id_sequence", default=None, context=context)
-        if not employee_id_sequence:
-            dataobj = self.pool.get('ir.model.data')
-            try:
-                dummy, employee_id_sequence = dataobj.get_object_reference(cr, SUPERUSER_ID, 'hr_employee_id', 'seq_employeeid_ref')
-            except ValueError:
-                pass
-        return {'employee_id_sequence': int(employee_id_sequence)  or False}
+    @api.model
+    def get_default_employee_id_values(self, fields):
+        company = self.env.user.company_id
+        return {
+            'employee_id_gen_method': company.employee_id_gen_method,
+            'employee_id_random_digits': company.employee_id_random_digits,
+            'employee_id_sequence': company.employee_id_sequence.id,
+        }
 
-    def set_employee_id_sequence (self, cr, uid, ids, context=None):
-        config_parameters = self.pool.get("ir.config_parameter")
-        for record in self.browse(cr, uid, ids, context=context):
-            config_parameters.set_param(cr, uid, "hr_employee_id.employee_id_sequence", record.employee_id_sequence.id  or '', context=context)
+    @api.one
+    def set_employee_id_values(self):
+        company = self.env.user.company_id
+        company.employee_id_gen_method = self.employee_id_gen_method
+        company.employee_id_random_digits = self.employee_id_random_digits
+        company.employee_id_sequence = self.employee_id_sequence
+
+    @api.onchange('employee_id_gen_method')
+    def onchange_employee_id_gen_method(self):
+        if self.employee_id_gen_method == 'sequence':
+            self.employee_id_sequence = self._default_id_sequence()
