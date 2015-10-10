d2807476823cefadb34156ebd438317cf967fe89
Salton <smassally@idtlabs.sl>
2015-10-10 18:02:43 +0000

    fix pep8

--- hr_payroll_report_contrib_register/report/report_contribution_register.py
+++ hr_payroll_report_contrib_register/report/report_contribution_register.py
@@ -1,4 +1,22 @@
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2016 Salton Massally (<smassally@idtlabs.sl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 import time
 from datetime import datetime
 from dateutil import relativedelta
@@ -8,17 +26,29 @@ from openerp.report import report_sxw
 
 class contribution_register_report(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
-        super(contribution_register_report, self).__init__(cr, uid, name, context)
+        super(contribution_register_report, self).__init__(
+            cr, uid, name, context)
         self.localcontext.update({
             'get_payslip_lines': self._get_payslip_lines,
             'sum_total': self.sum_total,
         })
 
     def set_context(self, objects, data, ids, report_type=None):
-        self.date_from = data['form'].get('date_from', time.strftime('%Y-%m-%d'))
-        self.date_to = data['form'].get('date_to', str(datetime.now() + relativedelta.relativedelta(months=+1, day=1, days=-1))[:10])
+        self.date_from = data['form'].get(
+            'date_from',
+            time.strftime('%Y-%m-%d')
+        )
+        self.date_to = data['form'].get(
+            'date_to',
+            str(
+                datetime.now()
+                + relativedelta.relativedelta(months=+1, day=1, days=-1)
+            )[:10]
+        )
         self.slip_ids = data['form'].get('slip_ids', [])
-        return super(contribution_register_report, self).set_context(objects, data, ids, report_type=report_type)
+        return super(contribution_register_report, self).set_context(
+            objects, data, ids, report_type=report_type
+        )
 
     def sum_total(self):
         return self.regi_total
@@ -28,14 +58,15 @@ class contribution_register_report(report_sxw.rml_parse):
         payslip_lines = []
         res = []
         self.regi_total = 0.0
-        self.cr.execute("SELECT pl.id from hr_payslip_line as pl "\
-                        "LEFT JOIN hr_payslip AS hp on (pl.slip_id = hp.id) "\
-                        "WHERE (hp.date_from >= %s) AND (hp.date_to <= %s) "\
-                        "AND pl.register_id = %s "\
-                        "AND hp.state = 'done' "\
-                        "AND hp.id in %s"\
+        self.cr.execute("SELECT pl.id from hr_payslip_line as pl "
+                        "LEFT JOIN hr_payslip AS hp on (pl.slip_id = hp.id) "
+                        "WHERE (hp.date_from >= %s) AND (hp.date_to <= %s) "
+                        "AND pl.register_id = %s "
+                        "AND hp.state = 'done' "
+                        "AND hp.id in %s"
                         "ORDER BY pl.slip_id, pl.sequence",
-                        (self.date_from, self.date_to, obj.id, tuple(self.slip_ids)))
+                        (self.date_from, self.date_to, obj.id,
+                            tuple(self.slip_ids)))
         payslip_lines = [x[0] for x in self.cr.fetchall()]
         for line in payslip_line.browse(self.cr, self.uid, payslip_lines):
             res.append({
@@ -51,7 +82,8 @@ class contribution_register_report(report_sxw.rml_parse):
 
 
 class wrapped_report_contribution_register(osv.AbstractModel):
-    _name = 'report.hr_payroll_report_contrib_register.report_contributionregister'
+    _name = \
+        'report.hr_payroll_report_contrib_register.report_contributionregister'
     _inherit = 'report.abstract_report'
     _template = 'hr_payroll.report_contributionregister'
     _wrapped_report_class = contribution_register_report
--- hr_payroll_report_contrib_register/wizard/print_contribution_register.py
+++ hr_payroll_report_contrib_register/wizard/print_contribution_register.py
@@ -1,5 +1,23 @@
-# -*- coding:utf-8 -*-
-from openerp import models, fields, api, _
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2016 Salton Massally (<smassally@idtlabs.sl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from openerp import models, fields, api
 
 
 class print_run_contrib_register(models.TransientModel):
@@ -7,10 +25,10 @@ class print_run_contrib_register(models.TransientModel):
     _description = 'Print Contribution Register'
 
     contrib_register_ids = fields.Many2many(
-                                            'hr.contribution.register',
-                                            string="Contribution Registers"
-                                            )
-    
+        'hr.contribution.register',
+        string="Contribution Registers"
+    )
+
     @api.multi
     def print_contrib_register(self):
         self.ensure_one()
@@ -18,16 +36,19 @@ class print_run_contrib_register(models.TransientModel):
         run = self.env['hr.payslip.run'].browse(run_id)
 
         datas = {
-             'model': 'hr.contribution.register', 
-             'ids'  : self.contrib_register_ids.ids,
-             'form' : {
-                       'date_from': run.date_start,
-                       'date_to'  : run.date_end,
-                       'slip_ids' : run.slip_ids.ids,
-                       }
-         }
-        report_obj = self.env['report'].with_context(active_ids=self.contrib_register_ids.ids,
-                                                     active_model='hr.contribution.register')
-        return report_obj.get_action(self.contrib_register_ids, 'hr_payroll_report_contrib_register.report_contributionregister',
-                                     data=datas)
- 
+            'model': 'hr.contribution.register',
+            'ids': self.contrib_register_ids.ids,
+            'form': {
+                'date_from': run.date_start,
+                'date_to': run.date_end,
+                'slip_ids': run.slip_ids.ids,
+            }
+        }
+        report_obj = self.env['report'].with_context(
+            active_ids=self.contrib_register_ids.ids,
+            active_model='hr.contribution.register')
+        return report_obj.get_action(
+            self.contrib_register_ids,
+            'hr_payroll_report_contrib_register.report_contributionregister',
+            data=datas
+        )
