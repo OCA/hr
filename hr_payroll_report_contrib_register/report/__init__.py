927a8d88b48ca45eff2ef1973797e36679b2fd3c
Salton <smassally@idtlabs.sl>
2015-10-14 18:15:49 +0000

    [IMP] replace relative import

--- hr_payroll_report_contrib_register/report/__init__.py
+++ hr_payroll_report_contrib_register/report/__init__.py
@@ -17,4 +17,4 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-import report_contribution_register
+from . import report_contribution_register
