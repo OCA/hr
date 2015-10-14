4fc4be667048dc1e3be2510dae86abb64ea42fc5
Salton <smassally@idtlabs.sl>
2015-10-14 18:08:58 +0000

    ensure that default salary struc can be selected from company

--- hr_payroll_default_salary_structure/__openerp__.py
+++ hr_payroll_default_salary_structure/__openerp__.py
@@ -31,8 +31,7 @@
     ],
     "data": [
         'views/res_config.xml',
+        'views/res_company.xml',
     ],
     'installable': True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
