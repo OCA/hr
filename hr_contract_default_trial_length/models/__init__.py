24557cef87cb3e8251e15fc7046025c035273a98
Salton <smassally@idtlabs.sl>
2015-10-15 10:30:31 +0000

    [IMP] Trial length should depend onc ontract types

--- hr_contract_default_trial_length/models/__init__.py
+++ hr_contract_default_trial_length/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#    Copyright (C) 2016 Salton Massally (<smassally@idtlabs.sl>).
+#    Copyright (C) 2015 Salton Massally (<smassally@idtlabs.sl>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -17,6 +17,5 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from . import res_config
-from . import res_company
+from . import hr_contract_type
 from . import hr_contract
