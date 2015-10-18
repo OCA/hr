85b3438e5daab020bbf870cf83a76934cf63f980
Salton <smassally@idtlabs.sl>
2015-10-18 18:08:20 +0000

    amendment and slip category added

--- hr_payslip_amendment/__openerp__.py
+++ hr_payslip_amendment/__openerp__.py
@@ -1,9 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': 'Pay Slip Amendment',
     'version': '1.0',
     'category': 'Human Resources',
     'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>, "
-              "Salton Massally<smassally@idtlabs.sl>, "
               "Odoo Community Association (OCA)",
     'summary': "Add Amendments to Current and Future Pay Slips",
     'website': 'http://miketelahun.wordpress.com',
@@ -12,8 +30,10 @@
         'hr_payroll',
     ],
     'data': [
+        'views/hr_payslip_amendment_category_view.xml',
         'views/hr_payslip_amendment_view.xml',
         'views/hr_payslip_amendment_workflow.xml',
+        'security/ir.model.access.csv'
     ],
     'installable': True,
 }
