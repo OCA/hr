7d67ce3b12ac4fec70b47ad7e1e2c04aa0dd04ec
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-10-19 12:01:34 +0200

    [ADD] Add hr_holidays_previous_type module

---
+++ hr_holidays_previous_type/tests/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import test_hr_holidays_previous_type
---
+++ hr_holidays_previous_type/tests/test_hr_holidays_previous_type.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#     This file is part of hr_holidays_previous_type,
+#     an Odoo module.
+#
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#
+#     hr_holidays_previous_type is free software:
+#     you can redistribute it and/or modify it under the terms of the GNU
+#     Affero General Public License as published by the Free Software
+#     Foundation,either version 3 of the License, or (at your option) any
+#     later version.
+#
+#     hr_holidays_previous_type is distributed
+#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with hr_holidays_previous_type.
+#     If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.tests import common
+from openerp import exceptions
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from datetime import datetime
+
+
+def convert_datetime(str_date):
+    date_dt = datetime.strptime(str_date, '%Y-%m-%d')
+    return date_dt.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+
+class TestHrHolidaysPreviousType(common.TransactionCase):
+
+    def setUp(self):
+        super(TestHrHolidaysPreviousType, self).setUp()
+        self.holidays_obj = self.env['hr.holidays']
+        self.type01 = self.env.ref('hr_holidays.holiday_status_cl')
+        self.type02 = self.type01.copy()
+        self.employee01 = self.env.ref('hr.employee')
+        self.year = datetime.now().strftime('%Y')
+        self.yearp1 = str(int(self.year) + 1)
+        self.yearp2 = str(int(self.year) + 2)
+
+    def _prepare_types(self):
+        self.type01.use_validity_dates = True
+        date_start = convert_datetime('%s-01-01' % self.year)
+        date_end = convert_datetime('%s-03-31' % self.yearp1)
+        self.type01.date_start = date_start
+        self.type01.date_end = date_end
+
+        self.type02.use_validity_dates = True
+        date_start = convert_datetime('%s-01-01' % self.yearp1)
+        date_end = convert_datetime('%s-03-31' % self.yearp2)
+        self.type02.date_start = date_start
+        self.type02.date_end = date_end
+        self.type02.previous_type_id = self.type01
+
+    def _create_allocation(self):
+        allocation_vals = {
+            'name': 'Test',
+            'type': 'add',
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type01.id,
+            'number_of_days_temp': 10,
+        }
+        self.holidays_obj.create(allocation_vals)
+        allocation_vals = {
+            'name': 'Test',
+            'type': 'add',
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type02.id,
+            'number_of_days_temp': 10,
+        }
+        self.holidays_obj.create(allocation_vals)
+
+    def test_without_previous_type(self):
+        self._create_allocation()
+        date_start = convert_datetime('%s-01-01' % self.year)
+        date_end = convert_datetime('%s-01-05' % self.year)
+        leave_vals = {
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type01.id,
+            'name': 'test',
+            'date_from': date_start,
+            'date_to': date_end,
+            'number_of_days_temp': 5.0
+        }
+        self.holidays_obj.create(leave_vals)
+        date_start = convert_datetime('%s-03-31' % self.yearp1)
+        date_end = convert_datetime('%s-04-01' % self.yearp1)
+        leave_vals = {
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type02.id,
+            'name': 'test',
+            'date_from': date_start,
+            'date_to': date_end,
+            'number_of_days_temp': 2.0
+        }
+        self.holidays_obj.create(leave_vals)
+
+    def test_previous_type(self):
+        self._prepare_types()
+        self._create_allocation()
+        date_start = convert_datetime('%s-01-01' % self.year)
+        date_end = convert_datetime('%s-01-05' % self.year)
+        leave_vals01 = {
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type01.id,
+            'name': 'test',
+            'date_from': date_start,
+            'date_to': date_end,
+            'number_of_days_temp': 5.0
+        }
+        self.holidays_obj.create(leave_vals01)
+        date_start = convert_datetime('%s-03-31' % self.yearp1)
+        date_end = convert_datetime('%s-04-01' % self.yearp1)
+        leave_vals = {
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type02.id,
+            'name': 'test',
+            'date_from': date_start,
+            'date_to': date_end,
+            'number_of_days_temp': 2.0
+        }
+        # I test with a date from before the previous type end date
+        with self.assertRaises(exceptions.ValidationError),\
+                self.cr.savepoint():
+            self.holidays_obj.create(leave_vals)
+        # I test before the validity date of the previous type
+        leave_vals['date_from'] = convert_datetime('%s-03-30' % self.yearp1)
+        leave_vals['date_to'] = convert_datetime('%s-03-31' % self.yearp1)
+        with self.assertRaises(exceptions.ValidationError),\
+                self.cr.savepoint():
+            self.holidays_obj.create(leave_vals)
+        # I test after the validity date of the previous type
+        leave_vals['date_from'] = convert_datetime('%s-04-01' % self.yearp1)
+        leave_vals['date_to'] = convert_datetime('%s-04-02' % self.yearp1)
+        self.holidays_obj.create(leave_vals)
+        # I test with no validity dates onthe previous type.
+        # An exception should be raised
+        self.type01.use_validity_dates = False
+        leave_vals['date_from'] = convert_datetime('%s-04-03' % self.yearp1)
+        leave_vals['date_to'] = convert_datetime('%s-04-04' % self.yearp1)
+        with self.assertRaises(exceptions.ValidationError),\
+                self.cr.savepoint():
+            self.holidays_obj.create(leave_vals)
+        self.type01.use_validity_dates = True
+        # I take remaining leaves on the first type
+        leave_vals01['date_from'] = convert_datetime('%s-01-01' % self.yearp1)
+        leave_vals01['date_to'] = convert_datetime('%s-01-05' % self.yearp1)
+        self.holidays_obj.create(leave_vals01)
+        # Test with no remaining leaves on the first type
+        leave_vals['date_from'] = convert_datetime('%s-03-30' % self.yearp1)
+        leave_vals['date_to'] = convert_datetime('%s-03-31' % self.yearp1)
+        self.holidays_obj.create(leave_vals)
