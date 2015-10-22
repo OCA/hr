aa486e9ce68a78e51cc51eb64b4d7ad2ae0d55c8
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-22 11:15:00 +0500

    bump 8.0.1.1.0

--- hr_skill/__openerp__.py
+++ hr_skill/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Skill Management",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Human Resources",
     "license": "AGPL-3",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
