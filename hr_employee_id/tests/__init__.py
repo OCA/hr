499ca021c0d47b6ced6c12588e8500a3ff265892
Salton <smassally@idtlabs.sl>
2015-10-24 12:52:28 +0000

    rebase on 8.0

--- hr_employee_id/tests/__init__.py
+++ hr_employee_id/tests/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+###############################################################################
 #
-#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
+#    Copyright (C) 2015 Salton Massally (<smassally@idtlabs.sl>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -10,11 +10,12 @@
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-from . import models
+###############################################################################
+
+from . import test_employee_id
