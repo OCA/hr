78fb707b3f6b5b563185a73f163c9b22cd3ac681
Fekete Mihai <mihai.fekete@forbiom.eu>
2015-11-09 22:16:56 -0800

    Update version number.

--- hr_employee_firstname/__openerp__.py
+++ hr_employee_firstname/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'HR Employee First Name, Last Name',
-    'version': '8.0.0.0.1',
+    'version': '8.0.0.0.2',
     'author': "Savoir-faire Linux, "
               "Fekete Mihai (Forest and Biomass Services Romania), "
               "Odoo Community Association (OCA)",
