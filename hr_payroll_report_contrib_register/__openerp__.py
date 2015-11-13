c6887df13ca95aef46a1c044cc3f7461876d5fb7
Salton <smassally@idtlabs.sl>
2015-11-13 14:47:25 +0000

    Cosmetic changes adjustversion number add try me on runbot button add the bug tracker paragraph with predefined template.

--- hr_payroll_report_contrib_register/__openerp__.py
+++ hr_payroll_report_contrib_register/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#    Copyright (C) 2016 Salton Massally (<smassally@idtlabs.sl>).
+#    Copyright (C) 2015 Salton Massally (<smassally@idtlabs.sl>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,7 +20,8 @@
 
 {
     'name': 'Payroll Contribution Register Report',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
     'category': 'Human Resources',
     'summary': "Print Contribution register from payroll run",
     'author': "Salton Massally <smassally@idtlabs.sl>, "
