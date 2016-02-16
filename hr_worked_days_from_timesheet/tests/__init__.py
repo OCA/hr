e4a729d37a0d245ec13a40b3fb2007b9b5453baa
Andhitia Rama <andhitia.r@gmail.com>
2016-02-17 04:15:15 +0700

    1. Add unittest 2. Add header on test .py 3. Change Warning into UserError
    Fix header. Fix duplicate author
    
    Fix unittest
    
    Fix PEP8

---
+++ hr_worked_days_from_timesheet/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia <https://opensynergy-indonesia.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_worked_days_from_timesheet
