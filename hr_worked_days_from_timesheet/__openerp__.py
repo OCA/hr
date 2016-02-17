76f9711d36115ed03a2cff1dd35d8484e0b0e1db
Andhitia Rama <andhitia.r@gmail.com>
2016-02-17 07:58:38 +0700

    Shorten .py header

--- hr_worked_days_from_timesheet/__openerp__.py
+++ hr_worked_days_from_timesheet/__openerp__.py
@@ -1,32 +1,14 @@
-# -*- coding:utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Odoo Canada. All Rights Reserved.
-#    Copyright (C) 2015 Acysos S.L. All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2012 - 2014 Odoo Canada
+# © 2015 Acysos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     'name': 'Worked Days From Timesheet',
     'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Generic Modules/Human Resources',
-    'author': "Savoir-faire Linux, Acysos SL, \
-    Acysos S.L., \
-    Odoo Community Association (OCA)",
+    'author': "Savoir-faire Linux, Acysos SL,Acysos S.L.,"
+    "Odoo Community Association (OCA)",
     'website': 'https://www.savoirfairelinux.com/',
     'depends': [
         'hr_payroll',
