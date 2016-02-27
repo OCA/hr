4addc17ce5f417bf79de85979777349ebba2885c
Andhitia Rama <andhitia.r@gmail.com>
2016-02-27 18:26:12 +0700

    [MIG] hr_employee_id: Migration to 8.0
    * Change .py header according OCA simple header
    * Add parameter name on field declaration
    * Greenify travis
    * Modify README.rst. Removing tabs from xml
    * Remove space from xml files
    * Make module installable

--- hr_employee_id/__openerp__.py
+++ hr_employee_id/__openerp__.py
@@ -1,31 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011, 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Employee ID',
     'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Generic Modules/Human Resources',
     'author': 'Michael Telahun Makonnen, '
+              'OpenSynergy Indonesia,'
               'Odoo Community Association (OCA)',
     'website': 'http://miketelahun.wordpress.com',
-    'license': 'AGPL-3',
     'depends': [
         'hr',
     ],
@@ -35,5 +20,5 @@
         'views/hr_employee_views.xml',
         'data/hr_employee_sequence.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
--- hr_employee_id/models/res_company.py
+++ hr_employee_id/models/res_company.py
@@ -1,22 +1,7 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#    Copyright (C) 2015 Salton Massally (<smassally@idtlabs.sl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# © 2015 Salton Massally <smassally@idtlabs.sl>
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
 
@@ -25,7 +10,7 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     employee_id_gen_method = fields.Selection(
-        [
+        selection=[
             ('random', 'Random'),
             ('sequence', 'Sequence'),
         ],
@@ -33,10 +18,12 @@ class ResCompany(models.Model):
         default='random'
     )
     employee_id_random_digits = fields.Integer(
-        '# of Digits', default=5,
+        string='# of Digits',
+        default=5,
         help="Number of digits making up the ID"
     )
     employee_id_sequence = fields.Many2one(
-        'ir.sequence', 'Sequence',
+        comodel_name='ir.sequence',
+        string='Sequence',
         help="Pattern to be used for used for ID Generation",
     )
