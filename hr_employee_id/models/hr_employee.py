20878d46207dae32a72b28227c47faf3e00e4770
Andhitia Rama <andhitia.r@gmail.com>
2016-02-27 18:26:12 +0700

    [MIG] hr_employee_id: Migration to 8.0
    * Change .py header according OCA simple header
    * Add parameter name on field declaration
    * Greenify travis
    * Modify README.rst. Removing tabs from xml
    * Remove space from xml files
    * Make module installable

--- hr_employee_id/models/hr_employee.py
+++ hr_employee_id/models/hr_employee.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011, 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 import random
 import string
 from openerp import models, fields, api, _
@@ -29,7 +15,7 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     identification_id = fields.Char(
-        'Identification No',
+        string='Identification No',
         copy=False
     )
 
@@ -66,7 +52,6 @@ class HrEmployee(models.Model):
         return employee_id
 
     @api.model
-    @api.returns('self', lambda value: value.id)
     def create(self, vals):
         eid = self._generate_identification_id()
         vals['identification_id'] = eid
