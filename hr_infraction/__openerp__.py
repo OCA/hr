d2a9a7de3aea1ce7a714933155344161714cab79
Andhitia Rama <andhitia.r@gmail.com>
2016-03-07 17:41:54 +0700

    Adjusting manifest file according OCA guidelines. Make modul instllable

--- hr_infraction/__openerp__.py
+++ hr_infraction/__openerp__.py
@@ -1,40 +1,17 @@
-# -*- coding:utf-8 -*-
-#
-#
-#    Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#    All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# -*- coding: utf-8 -*-
+# © 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Employee Infraction Management',
-    'version': '1.0',
+    "version": "8.0.1.0.0",
     'category': 'Generic Modules/Human Resources',
-    'description': """
-Warning/Disciplinary Action Management
-========================================
-    """,
-    'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>,Odoo Community Association (OCA)",
+    'author': "OpenSynergy Indonesia,Michael Telahun Makonnen <mmakonnen@gmail.com>,"
+                   "Odoo Community Association (OCA)",
     'website': 'http://miketelahun.wordpress.com',
     'license': 'AGPL-3',
     'depends': [
         'hr',
-        'hr_employee_state',
-        'hr_security',
-        'hr_transfer',
     ],
     'data': [
         'security/ir.model.access.csv',
@@ -43,7 +20,5 @@ Warning/Disciplinary Action Management
         'hr_infraction_view.xml',
         'hr_infraction_workflow.xml',
     ],
-    'test': [
-    ],
-    'installable': False,
+    'installable': True,
 }
