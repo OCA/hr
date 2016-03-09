ac1ffd91b21759ebb610e36dea3b4cc4a4bab6bd
Andhitia Rama <andhitia.r@gmail.com>
2016-03-09 18:56:57 +0700

    Fixing copyright on py files

--- hr_infraction/models/hr_employee.py
+++ hr_infraction/models/hr_employee.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import models, fields
 
 
--- hr_infraction/tests/test_infraction_flow.py
+++ hr_infraction/tests/test_infraction_flow.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © <YEAR(S)> <AUTHOR(S)>
+# © 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
