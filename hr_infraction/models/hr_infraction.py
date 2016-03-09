65fc2bcc1fecc4ef88dc7f51c3facfad2fb64c07
Andhitia Rama <andhitia.r@gmail.com>
2016-03-09 11:18:30 +0700

    Add translate property to some of the fields

--- hr_infraction/models/hr_infraction.py
+++ hr_infraction/models/hr_infraction.py
@@ -14,6 +14,7 @@ class HrInfractionCatagory(models.Model):
     name = fields.Char(
         string='Name',
         required=True,
+        translate=True,
     )
     code = fields.Char(
         string='Code',
@@ -28,6 +29,7 @@ class HrInfractionWarning(models.Model):
     name = fields.Char(
         string='Warning',
         required=True,
+        translate=True,
         )
 
     code = fields.Char(
