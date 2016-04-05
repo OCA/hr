ad712fe38aada2683822acd62d7feabadbd05a78
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2016-04-05 10:04:06 +0300

    Update Hr public holidays.
    [MIG] hr_public_holidays

---
+++ hr_public_holidays/models/hr_public_holidays_line.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# ©  2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# ©  2014 initOS GmbH & Co. KG <http://www.initos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models, api
+from openerp.exceptions import Warning as UserError
+
+
+class HrPublicHolidaysLine(models.Model):
+    _name = 'hr.holidays.public.line'
+    _description = 'Public Holidays Lines'
+    _order = "date, name desc"
+
+    name = fields.Char(
+        'Name',
+        required=True,
+    )
+    date = fields.Date(
+        'Date',
+        required=True
+    )
+    year_id = fields.Many2one(
+        'hr.holidays.public',
+        'Calendar Year',
+        required=True,
+    )
+    variable = fields.Boolean('Date may change')
+    state_ids = fields.Many2many(
+        'res.country.state',
+        'hr_holiday_public_state_rel',
+        'line_id',
+        'state_id',
+        'Related States'
+    )
+
+    @api.one
+    @api.constrains('date', 'state_ids')
+    def _check_date_state(self):
+        if fields.Date.from_string(self.date).year != self.year_id.year:
+            raise UserError(
+                'Dates of holidays should be the same year '
+                'as the calendar year they are being assigned to'
+            )
+        if self.state_ids:
+            domain = [('date', '=', self.date),
+                      ('year_id', '=', self.year_id.id),
+                      ('state_ids', '!=', False),
+                      ('id', '!=', self.id)]
+            holidays = self.search(domain)
+            for holiday in holidays:
+                if self.state_ids & holiday.state_ids:
+                    raise UserError('You can\'t create duplicate public '
+                                    'holiday per date %s and one of the '
+                                    'country states.' % self.date)
+        domain = [('date', '=', self.date),
+                  ('year_id', '=', self.year_id.id),
+                  ('state_ids', '=', False)]
+        if self.search_count(domain) > 1:
+            raise UserError('You can\'t create duplicate public holiday '
+                            'per date %s.' % self.date)
+        return True
