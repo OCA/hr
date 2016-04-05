3647d024e4dc38ecd01c393e707dcdc88416130f
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2016-04-05 10:04:06 +0300

    Update Hr public holidays.
    [MIG] hr_public_holidays

--- hr_public_holidays/tests/__init__.py
+++ hr_public_holidays/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# ©  2015 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# ©  2015 Salton Massally <smassally@idtlabs.sl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import test_public_holidays
