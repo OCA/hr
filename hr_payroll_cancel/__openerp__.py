39a3006d1894d937f12962c1b36fb1b28137306b
Luis Escobar <lescobar@vauxoo.com>
2016-04-14 00:45:09 +0000

    [FIX] Wrong manifest version format

--- hr_payroll_cancel/__openerp__.py
+++ hr_payroll_cancel/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "Hr Payroll Cancel",
-    "version": "1.6",
+    "version": "8.0.0.1.6",
     "author": "Vauxoo",
     "category": "Localization/Mexico",
     "website": "http://www.vauxoo.com/",
