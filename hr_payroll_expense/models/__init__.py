f73f125e3930fa4cf00762b8e0eb283cdead406d
Coninckx David <dco@open-net.ch>
2016-05-18 11:57:11 +0200

    Fix hr_payroll_expense - Fix headers - Fix blank line on README.rst

--- hr_payroll_expense/models/__init__.py
+++ hr_payroll_expense/models/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-#
-#  File: models/__init__.py
-#  Module: hr_payroll_expenses
-#
-#  Created by dco@open-net.ch
-#  See __openerp__.py for full copyright and licensing details
-##############################################################################
+# © 2016 Coninckx David (Open Net Sarl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import hr_contract
 from . import hr_expenses
