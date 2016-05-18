4f745b62c46230d7b2fea6c5b4e4219ef654bd67
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2016-05-18 16:20:13 +0300

    Update Hr holidays compute days (#210)
    * Squash commits into a single one.
    
    * Update tests with old onchange methods.
    
    * Fix variable name in tests..
    
    * Update tests.
    
    * Update flake8.
    
    * Update security rules so that employee can read his schedule and contract, and hr manager to have the same rigths as defined in hr_contract.
    
    * Improve onchange employee, add tests for onchange.
    
    * Fix flake, whitespaces.

---
+++ hr_holidays_compute_days/models/hr_employee.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# ©  2015 iDT LABS (http://www.@idtlabs.sl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from datetime import datetime, time
+from openerp import models, api
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    @api.multi
+    def work_scheduled_on_day(self, date_dt, public_holiday=True,
+                              schedule=True):
+        ''''
+        returns true or false depending on if employee was scheduled to work
+        on a particular day. It does this by both checking if it is a public
+        holiday and the resource calendar of the contract
+        @param date_dt: date for which to check
+        @param public_holiday: optional, whether to consider public holidays,
+                               default=True
+        @param schedule: optional, whether to consider the contract's resource
+                         calendar. default=True
+        '''
+        self.ensure_one()
+        if public_holiday and self.env['hr.holidays.public'].is_public_holiday(
+                date_dt, employee_id=self.id):
+            return False
+        elif schedule and self.contract_id and self.contract_id.working_hours:
+            hours = self.contract_id.working_hours.get_working_hours_of_date(
+                datetime.combine(date_dt, time.min))[0]
+            if not hours:
+                return False
+        elif schedule and (not self.contract_id or (
+                self.contract_id and not self.contract_id.working_hours)):
+            return date_dt.weekday() not in (5, 6)
+        return True
