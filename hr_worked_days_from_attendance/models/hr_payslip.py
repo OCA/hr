e6187537966320d77f6a06d775757b3841b577af
Andhitia Rama <andhitia.r@gmail.com>
2016-07-02 00:05:04 +0700

    -

--- hr_worked_days_from_attendance/models/hr_payslip.py
+++ hr_worked_days_from_attendance/models/hr_payslip.py
@@ -2,7 +2,8 @@
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, _
+from openerp import models, api, _, exceptions
+from datetime import datetime, timedelta
 
 
 class HrPayslip(models.Model):
@@ -48,3 +49,79 @@ class HrPayslip(models.Model):
                 res["number_of_hours"] += day.total_attendance
 
         wd_obj.create(res)
+
+    @api.model
+    def get_worked_day_lines(self, contract_ids, 
+            date_from, date_to):
+        def was_on_leave(employee_id, datetime_day):
+            res = False
+            day = datetime_day.strftime("%Y-%m-%d")
+            obj_holiday = self.env["hr.holidays"]
+            holidays = obj_holiday.search([
+                ("state","=","validate"),
+                ("employee_id","=",employee_id),
+                ("type","=","remove"),
+                ("date_from","<=",day),
+                ("date_to",">=",day)])
+            if holidays:
+                res = holidays[0].holiday_status_id.name
+            return res
+
+        res = []
+        obj_contract = self.env["hr.contract"]
+        for contract in obj_contract.browse(contract_ids):
+            # raise exceptions.Warning("c")
+            if not contract.working_hours:
+                #fill only if the contract as a working schedule linked
+                continue
+            attendances = {
+                 'name': _("Normal Working Days paid at 100%"),
+                 'sequence': 1,
+                 'code': 'WORK100',
+                 'number_of_days': 0.0,
+                 'number_of_hours': 0.0,
+                 'contract_id': contract.id,
+            }
+            leaves = {}
+            day_from = datetime.strptime(date_from,"%Y-%m-%d")
+            day_to = datetime.strptime(date_to,"%Y-%m-%d")
+            nb_of_days = (day_to - day_from).days + 1
+            obj_calendar = self.env["resource.calendar"]
+            for day in range(0, nb_of_days):
+                working_hours_on_day = obj_calendar.working_hours_on_day(
+                        contract.working_hours, 
+                        day_from + timedelta(days=day))
+                if working_hours_on_day:
+                    #the employee had to work
+                    leave_type = was_on_leave(
+                            contract.employee_id.id, 
+                            day_from + timedelta(days=day))
+                    if leave_type:
+                        #if he was on leave, fill the leaves dict
+                        if leave_type in leaves:
+                            leaves[leave_type]['number_of_days'] += 1.0
+                            leaves[leave_type]['number_of_hours'] += working_hours_on_day
+                        else:
+                            leaves[leave_type] = {
+                                'name': leave_type,
+                                'sequence': 5,
+                                'code': leave_type,
+                                'number_of_days': 1.0,
+                                'number_of_hours': working_hours_on_day,
+                                'contract_id': contract.id,
+                            }
+                    else:
+                        #add the input vals to tmp (increment if existing)
+                        obj_day = self.env["hr_timesheet_sheet.sheet.day"]
+                        date_attn = (day_from + timedelta(days=day)).strftime("%Y-%m-%d")
+                        criteria2 = [
+                            ("name", "=", date_attn),
+                            ("sheet_id.employee_id", "=", contract.employee_id.id),
+                        ]
+                        attn = obj_day.search(criteria2)
+                        if attn:
+                            attendances['number_of_days'] += 1.0
+                            attendances['number_of_hours'] += attn[0].total_attendance
+            leaves = [value for key,value in leaves.items()]
+            res += [attendances] + leaves
+        return res
