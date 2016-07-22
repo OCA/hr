6fd1c004faa6c4d6c7ef47eba874a4d53723e21e
aheficent <ahenriquez@eficent.com>
2016-07-22 12:09:29 +0200

    [FIX] Version number was changed by mistake in previous commit

--- hr_payroll_cancel/__openerp__.py
+++ hr_payroll_cancel/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Hr Payroll Cancel",
-    "version": "8.0.0.1",
+    "version": "8.0.0.1.6",
     "author": "Vauxoo,"
               "Odoo Community Association (OCA)",
     "category": "Localization/Mexico",
