f8a3dd2fa10b3f7f20c55348d43c9adca95877ee
aheficent <ahenriquez@eficent.com>
2016-07-22 18:34:49 +0200

    [FIX] Comments at https://github.com/Eficent/eficent-odoo-addons/pull/18

--- hr_payslip_change_state/__openerp__.py
+++ hr_payslip_change_state/__openerp__.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Aaron Henriquez (Eficent)
-#    Copyright 2016 Eficent
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2016 - Eficent http://www.eficent.com/
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Change the state of payslip in tree view',
     'version': '1.0',
