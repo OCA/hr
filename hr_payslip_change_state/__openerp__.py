4211e5f70f3bf47cf41fb6867089824e7bb6c806
aheficent <ahenriquez@eficent.com>
2016-08-01 10:21:52 +0200

    [FIX] travis comments for  commit e0138a5

--- hr_payslip_change_state/__openerp__.py
+++ hr_payslip_change_state/__openerp__.py
@@ -2,17 +2,16 @@
 # © 2016 - Eficent http://www.eficent.com/
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Change the state of many payslips at a time',
-    'version': '8.0.0.0.0',
-    'category': 'Human Resources',
-    'author': "Eficent",
-    'website': 'http://www.eficent.com',
-    'depends': ['hr_payroll_cancel'],
+    "name": "Change the state of many payslips at a time",
+    "version": "8.0.0.0.0",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "depends": ['hr_payroll_cancel'],
     "demo": [],
     "data": [
         "wizard/hr_payslip_change_state_view.xml",
     ],
     "installable": True,
     "auto_install": False,
-    "active": False
 }
