f905df157167bbaa7b3445902ffaf7649d150639
Andhitia Rama <andhitia.r@gmail.com>
2016-08-17 05:25:50 +0700

    Split model into different py file

--- hr_expense_product_policy/models/__init__.py
+++ hr_expense_product_policy/models/__init__.py
@@ -2,5 +2,7 @@
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import hr_job
+from . import hr_department
 from . import hr_employee
 from . import hr_expense
