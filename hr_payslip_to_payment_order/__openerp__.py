86744ce64c33ed8cd04b822b4f81fdf5cac131e8
Andhitia Rama <andhitia.r@gmail.com>
2016-10-30 12:42:12 +0700

    Module structure

---
+++ hr_payslip_to_payment_order/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payslip to Payment Order",
+    "version": "8.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://opensynergy-indonesia/",
+    "author": "OpenSynergy Indonesia, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_payroll_account",
+        "account_payment",
+    ],
+    "data": [
+    ],
+}
