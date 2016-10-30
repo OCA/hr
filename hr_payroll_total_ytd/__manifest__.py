5a3ae93352ad681a4bd6e60894529edfb5049a84
Bassirou Ndaw <b.ndaw@ergobit.org>
2016-10-30 22:46:44 +0100

    [ADD] hr_payroll_total_ytd

---
+++ hr_payroll_total_ytd/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Ergobit Consulting (https://www.ergobit.org)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'HR Payroll Total YTD',
+    'version': '1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    'author': 'Ergobit Consulting, Odoo Community Association (OCA)',
+    'website': 'https://ergobit.org',
+    'depends': [
+        'hr_payroll',
+    ],
+    'data': [
+        'report/report_payslip.xml',
+        'views/hr_payslip.xml',
+    ],
+    'installable': True,
+}
