448d30f476ad3ad24674a6364f3b5fea59eb5ac6
Andhitia Rama <andhitia.r@gmail.com>
2016-10-30 07:13:45 +0700

    Inherit hr.contract and add hr.contract.input_type

--- hr_payslip_input_policy/models/__init__.py
+++ hr_payslip_input_policy/models/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import hr_payslip_input_type
+from . import hr_payslip_input_type, hr_contract
