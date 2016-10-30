e038f3c6dc9ad579b1ebd64632c4ae41adaa88f4
Bassirou Ndaw <b.ndaw@ergobit.org>
2016-10-30 21:35:03 +0100

    [ADD] new module hr_payroll_total_ytd

---
+++ hr_payroll_total_ytd/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Ergobit Consulting (https://www.ergobit.org)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import hr_payslip
+from . import hr_payslip_line
