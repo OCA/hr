15777bdc01c0d55f991316e35a6b879697b006eb
Bassirou Ndaw <b.ndaw@ergobit.org>
2016-10-31 01:23:32 +0100

    [ADD] hr_payroll_total_ytd

---
+++ hr_payroll_total_ytd/models/hr_payslip_line.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 Ergobit Consulting (https://www.ergobit.org)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models, api, _
+from odoo.addons import decimal_precision as dp
+
+
+class HrPayslipLine(models.Model):
+
+    _inherit = 'hr.payslip.line'
+
+    amount_ytd = fields.Float(
+        'Total YTD',
+        digits_compute=dp.get_precision('Payroll'),
+    )
