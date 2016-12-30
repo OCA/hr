7415cacc27f7f455146953480bd9b003a24ab03b
Julien Laloux <julien.laloux@logicasoft.eu>
2016-12-30 17:43:16 +0100

    [MIG] 10.0 Porting hr_experience

--- hr_experience/models/hr_employee.py
+++ hr_experience/models/hr_employee.py
@@ -2,10 +2,10 @@
 # © 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
-class hr_employee(models.Model):
+class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     academic_ids = fields.One2many('hr.academic',
