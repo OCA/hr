2438bfa9057fbc26414658afa9e329cdeaecbf08
Julien Laloux <julien.laloux@logicasoft.eu>
2016-12-30 17:43:16 +0100

    [MIG] 10.0 Porting hr_experience

--- hr_experience/__init__.py
+++ hr_experience/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
--- hr_experience/__manifest__.py
+++ hr_experience/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# © 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Experience Management",
-    "version": "8.0.1.1.0",
+    "version": "10.0.1.0.0",
     "author": "Savoir-faire Linux,"
               "OpenSynergy Indonesia,"
               "Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
     "category": "Human Resources",
-    "depends": ["hr", ],
+    "depends": ["hr"],
     "data": [
         "security/ir.model.access.csv",
         "security/hr_security.xml",
@@ -20,5 +20,5 @@
         "views/hr_professional_view.xml",
         "views/hr_certification_view.xml",
     ],
-    'installable': False
+    'installable': True
 }
