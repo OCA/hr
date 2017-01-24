4ad16a42f6d6beefca4928272682581ed2bc3781
Antonio Esposito <espo-tony@users.noreply.github.com>
2017-01-24 14:27:03 +0100

    [MIG] hr_employee_firstname: Migrated to 10.0 (#276)

--- hr_employee_firstname/models/hr_employee.py
+++ hr_employee_firstname/models/hr_employee.py
@@ -1,29 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2010 - 2014 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# ©  2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, SUPERUSER_ID
+from odoo import models, fields, api
 
-UPDATE_PARTNER_FIELDS = set(['firstname', 'lastname', 'user_id',
-                             'address_home_id'])
+UPDATE_PARTNER_FIELDS = ['firstname', 'lastname', 'user_id', 'address_home_id']
 
 
 class HrEmployee(models.Model):
@@ -33,11 +14,6 @@ class HrEmployee(models.Model):
     def split_name(self, name):
         clean_name = u" ".join(name.split(None)) if name else name
         return self.env['res.partner']._get_inverse_name(clean_name)
-
-    @api.cr_context
-    def _auto_init(self, cr, context=None):
-        super(HrEmployee, self)._auto_init(cr, context=context)
-        self._update_employee_names(cr, SUPERUSER_ID, context=context)
 
     @api.model
     def _update_employee_names(self):
@@ -64,11 +40,13 @@ class HrEmployee(models.Model):
     def _get_name(self, lastname, firstname):
         return self.env['res.partner']._get_computed_name(lastname, firstname)
 
-    @api.one
+    @api.multi
     @api.onchange('firstname', 'lastname')
     def get_name(self):
-        if self.firstname and self.lastname:
-            self.name = self._get_name(self.lastname, self.firstname)
+        for employee in self:
+            if employee.firstname and employee.lastname:
+                employee.name = self._get_name(
+                    employee.lastname, employee.firstname)
 
     def _firstname_default(self):
         return ' ' if self.env.context.get('module') else False
