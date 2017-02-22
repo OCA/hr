4c2fa0a0be6821ffec1fe2235ff27b85062b79f0
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-22 15:00:04 +0100

    improve contact tree view with new fields

--- hr_emergency_contact/__init__.py
+++ hr_emergency_contact/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2011 Michael Telahun Makonnen <mmakonnen@gmail.com>
-# © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
--- hr_emergency_contact/models/hr_employee.py
+++ hr_emergency_contact/models/hr_employee.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2011 Michael Telahun Makonnen <mmakonnen@gmail.com>
-# © 2016 OpenSynergy Indonesia
+# copyright 2011 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
@@ -13,6 +13,7 @@ class HrEmployee(models.Model):
     emergency_contact_ids = fields.Many2many(
         comodel_name='res.partner',
         string='Emergency Contacts',
+        relation='rel_employee_emergency_contact',
         domain=[
             ('is_company', '=', False),
         ]
