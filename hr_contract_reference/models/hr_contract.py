fe3cb7256ce0d4e6665c10123d5076e36a3d5c4c
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-22 15:26:21 +0100

    [MIG] hr_contract_reference: Migrated to 10.0

--- hr_contract_reference/models/hr_contract.py
+++ hr_contract_reference/models/hr_contract.py
@@ -1,28 +1,11 @@
-# -*- coding:utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#    All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# copyright 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
-class hr_contract(models.Model):
+class HrContract(models.Model):
     _inherit = 'hr.contract'
 
     name = fields.Char('Contract Reference', required=False,
@@ -32,4 +15,4 @@ class hr_contract(models.Model):
     def create(self, vals):
         if vals.get('number', '/') == '/':
             vals['name'] = self.env['ir.sequence'].next_by_code('contract.ref')
-        return super(hr_contract, self).create(vals)
+        return super(HrContract, self).create(vals)
