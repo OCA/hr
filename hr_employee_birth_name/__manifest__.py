76866332becb693621db84d27f69c2e5f3beb763
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-22 17:40:13 +0100

    Add 10.0 hr_employee_birth_name

---
+++ hr_employee_birth_name/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# copyright  2017 Denis Leemann, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Employee Birth Name',
+    'version': '10.0.1.0.0',
+    'category': 'Human Resources',
+    'author': "Camptocamp SA,Odoo Community Association (OCA)",
+    'website': 'http://www.camptocamp.com',
+    'license': 'AGPL-3',
+    'depends': ['hr', ],
+    'data': [
+        'views/hr_employee_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
