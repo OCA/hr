5fe469345e6759e27a1f28e80976716e1e4cebf9
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-22 15:26:21 +0100

    hr_contract_reference: Migrated to 10.0

--- hr_contract_reference/__manifest__.py
+++ hr_contract_reference/__manifest__.py
@@ -1,26 +1,9 @@
-# -*- coding:utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#    All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# copyright 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "HR Contract Reference",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Generic Modules/Human Resources",
     "author": 'Michael Telahun Makonnen, '
               'Fekete Mihai (Forest and Biomass Services Romania), '
@@ -32,5 +15,5 @@
         'views/hr_contract_view.xml',
         'data/hr_contract_sequence.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
