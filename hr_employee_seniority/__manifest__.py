32a34ea029f6076c25e24d951259fc9cc5cad193
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-23 10:04:32 +0100

    [MIG] hr_employee_seniority: Migrated to 10.0

--- hr_employee_seniority/__manifest__.py
+++ hr_employee_seniority/__manifest__.py
@@ -1,46 +1,30 @@
-# -*- coding:utf-8 -*-
-#
-#
-#    Copyright (C) 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>.
-#    All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# -*- coding: utf-8 -*-
+# copyright 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# copyright 2017 Denis Leemann, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 
 {
     'name': 'Employee Seniority',
-    'version': '1.0',
-    'category': 'Generic Modules/Human Resources',
-    'description': """
-Keep Track of Length of Employment
-==================================
-    """,
-    'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>,Odoo Community Association (OCA)",
+    'summary': 'Keep Track of Length of Employment',
+    'version': '10.0.1.0.0',
+    'category': 'Human Resources',
+    'author': "Michael Telahun Makonnen <mmakonnen@gmail.com>, "
+              "Camptocamp SA, "
+              "Odoo Community Association (OCA)",
     'website': 'http://miketelahun.wordpress.com',
     'license': 'AGPL-3',
     'depends': [
         'hr',
-        'hr_security',
+        'hr_contract',
     ],
     "external_dependencies": {
         'python': ['dateutil'],
     },
     'data': [
-        'hr_view.xml',
+        'views/employee_views.xml',
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
 }
