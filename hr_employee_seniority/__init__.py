2f276eb1064534c224551c52bb49b9124ed8367f
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-23 17:19:59 +0100

    update computation

--- hr_employee_seniority/__init__.py
+++ hr_employee_seniority/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# copyright 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
-# copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
