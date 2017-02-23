6f851a75c5fbd5e3941cb656dc0a7e3743441b9a
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2017-02-23 09:50:22 -0300

    FIX

--- hr_skill/__openerp__.py
+++ hr_skill/__openerp__.py
@@ -17,7 +17,6 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-
 {
     "name": "Skill Management",
     "version": "9.0.1.0.0",
