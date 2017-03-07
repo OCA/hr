84405e623eb7378453084ebf1c2d3bdf184a84b9
Julien Laloux <julien.laloux@logicasoft.eu>
2017-03-07 13:43:56 +0100

    Refactor code for pep-8

--- hr_skill/tests/test_hr_skill.py
+++ hr_skill/tests/test_hr_skill.py
@@ -10,6 +10,11 @@ class TestHrSkill(TransactionCase):
     def test_name_get(self):
         hr_skill_model = self.env['hr.skill']
         soft_skill_id = hr_skill_model.create({'name': 'Soft skill'})
-        leadership_skill_id = hr_skill_model.create({'name': 'Leadership', 'parent_id': soft_skill_id.id})
+        leadership_skill_id = hr_skill_model.create({
+            'name': 'Leadership',
+            'parent_id': soft_skill_id.id
+        })
 
-        self.assertEqual(leadership_skill_id.name_get(), [(leadership_skill_id.id, u'Soft skill / Leadership')])
+        self.assertEqual(
+            leadership_skill_id.name_get(),
+            [(leadership_skill_id.id, u'Soft skill / Leadership')])
