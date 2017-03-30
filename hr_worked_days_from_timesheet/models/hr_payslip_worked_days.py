e512dcb855936367a989af5bcc697c2f863a8c8f
lreficent <lois.rilo@eficent.com>
2017-03-30 10:49:02 +0200

    hr_worked_days_from_timesheet

--- hr_worked_days_from_timesheet/models/hr_payslip_worked_days.py
+++ hr_worked_days_from_timesheet/models/hr_payslip_worked_days.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # © 2012 Odoo Canada
 # © 2015 Acysos S.L.
+# © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import models, fields
+from openerp import fields, models
 
 
 class HrPayslipWorkedDays(models.Model):
