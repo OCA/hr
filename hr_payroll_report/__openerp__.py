3e2c772d5de314424ef90f8fba702d5178824bc4
Eric @ Elico Corp <eric.caudal@elico-corp.com>
2017-04-25 15:11:35 +0800

    Update __openerp__.py

--- hr_payroll_report/__openerp__.py
+++ hr_payroll_report/__openerp__.py
@@ -2,11 +2,12 @@
 # Copyright 2017 Elico Corp (www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Payroll Report",
-    "version": "9.0.0.1.0",
+    "name": "Payslip Lines BI report",
+    "version": "9.0.1.0.0",
+    "summary": "Analyze your payroll with the Payslip Lines BI report"
     "category": "Generic Modules/Human Resources",
-    "website": "https://odoo-community.org/",
-    "author": "Elico Corp (www.elico-corp.com), "
+    "website": "https://www.elico-corp.com/",
+    "author": "Elico Corp, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
