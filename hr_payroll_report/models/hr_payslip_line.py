9042fc5f8b4c13a448e48ddc72ba973748f2fd76
Xudong Yang <fanderous@gmail.com>
2017-05-02 18:23:47 +0800

    [UPD] update to new api

--- hr_payroll_report/models/hr_payslip_line.py
+++ hr_payroll_report/models/hr_payslip_line.py
@@ -2,15 +2,14 @@
 # Copyright 2017 Elico Corp (www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import fields, osv
+from openerp import models, fields
 
 
-class HrPayrollReport(osv.osv):
+class HrPayrollReport(models.Model):
     _inherit = 'hr.payslip.line'
 
-    _columns = {
-        'date_from': fields.date('Date From', related='slip_id.date_from'),
-        'date_to': fields.date('Date To', related='slip_id.date_to'),
-        'payslip_run_id': fields.char('Payslip Batches',
-                                      related='slip_id.payslip_run_id.name'),
-    }
+    date_from = fields.Date(related='slip_id.date_from', store=True)
+    date_to = fields.Date(related='slip_id.date_to', store=True)
+    payslip_run_id = fields.Char('Payslip Batches',
+                                 related='slip_id.payslip_run_id.name',
+                                 store=True)
