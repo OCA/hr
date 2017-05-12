edba01245b0f4141fe934d1fcd770d16386c901a
Andrea <a.stirpe@onestein.nl>
2017-05-12 12:14:03 +0200

    Fix message notification + code review

--- hr_holidays_leave_auto_approve/__manifest__.py
+++ hr_holidays_leave_auto_approve/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -7,7 +7,7 @@
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'summary': '''Leave type for auto approval of Leaves''',
-    'author': 'ONESTEiN BV, Odoo Community Association (OCA)',
+    'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'http://www.onestein.eu',
     'category': 'Human Resources',
     'depends': [
