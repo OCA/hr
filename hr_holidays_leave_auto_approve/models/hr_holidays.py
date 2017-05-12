dc744afac74959aee6e30a4b17217e7bda46259d
Andrea <a.stirpe@onestein.nl>
2017-05-12 12:14:03 +0200

    Fix message notification + code review

--- hr_holidays_leave_auto_approve/models/hr_holidays.py
+++ hr_holidays_leave_auto_approve/models/hr_holidays.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api
@@ -16,9 +16,21 @@ class HrHolidays(models.Model):
 
     @api.model
     def create(self, values):
-        res = super(HrHolidays, self).create(values)
-        if self.env.user.sudo().has_group(
+        auto_approve = self._get_auto_approve_on_creation(values)
+        res = super(HrHolidays, self.with_context(
+            tracking_disable=auto_approve)
+        ).create(values)
+        if self.sudo().env.user.has_group(
                 'hr_holidays.group_hr_holidays_user'):
             if res.holiday_status_id and res.holiday_status_id.auto_approve:
                 res.sudo().action_approve()
         return res
+
+    @api.model
+    def _get_auto_approve_on_creation(self, values):
+        auto_approve = False
+        if values.get('holiday_status_id'):
+            auto_approve = self.env['hr.holidays.status'].browse(
+                values.get('holiday_status_id')
+            ).auto_approve
+        return auto_approve
