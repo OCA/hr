e30f5ece1f202394f1ef2313f333b23a8d509897
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-14 18:08:14 +0530

    [MIG] hr_period v10

---
+++ hr_period/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# Copyright 2015 Savoir-faire Linux. All Rights Reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_hr_fiscalyear
