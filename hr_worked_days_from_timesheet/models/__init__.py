7eba805021adc1ff283f4278e619f8b62e0938aa
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-08 13:28:39 +0530

    hr_worked_days_from_timesheet v10

--- hr_worked_days_from_timesheet/models/__init__.py
+++ hr_worked_days_from_timesheet/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2012 Odoo Canada
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import hr_payslip_worked_days
--- hr_worked_days_from_timesheet/tests/__init__.py
+++ hr_worked_days_from_timesheet/tests/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 OpenSynergy Indonesia <https://opensynergy-indonesia.com>
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_worked_days_from_timesheet
