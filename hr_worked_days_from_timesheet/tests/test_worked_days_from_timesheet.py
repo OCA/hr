73d08cac7dde6eea20a8318e26053369faed6f3d
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-08 13:28:39 +0530

    [ADD] hr_worked_days_from_timesheet v10

--- hr_worked_days_from_timesheet/tests/test_worked_days_from_timesheet.py
+++ hr_worked_days_from_timesheet/tests/test_worked_days_from_timesheet.py
@@ -2,9 +2,11 @@
 # © 2012 Odoo Canada
 # © 2015 Acysos S.L.
 # © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
+import time
 
 
 class TestComputeWorkdays(TransactionCase):
@@ -14,7 +16,10 @@ class TestComputeWorkdays(TransactionCase):
 
         self.company = self.env.ref('base.main_company')
         self.user_admin = self.env.ref('base.partner_root')
+        self.timesheet_sheet = self.env['hr_timesheet_sheet.sheet']
+        self.project_2 = self.env.ref('project.project_project_2')
 
+        # create user
         user_dict = {
             'name': 'User 1',
             'login': 'tua@example.com',
@@ -22,6 +27,7 @@ class TestComputeWorkdays(TransactionCase):
         }
         self.user_test = self.env['res.users'].create(user_dict)
 
+        # create Employee
         employee_dict = {
             'name': 'Employee 1',
             'user_id': self.user_test.id,
@@ -29,130 +35,44 @@ class TestComputeWorkdays(TransactionCase):
         }
         self.employee = self.env['hr.employee'].create(employee_dict)
 
+        # create Contract
         contract_dict = {
             'name': 'Contract 1',
             'employee_id': self.employee.id,
-            'wage': 0.0,
+            'wage': 10.0,
         }
         self.contract = self.env['hr.contract'].create(contract_dict)
 
-        analytic_dict = {
-            'name': 'Account 1',
-            'type': 'normal',
-            'use_timesheets': True,
-        }
-
-        self.analytic = self.env['account.analytic.account'].create(
-            analytic_dict)
-
-        ts1_dict = {
+        self.timesheet_sheet = self.timesheet_sheet.create({
+            'date_from': time.strftime('%Y-%m-11'),
+            'date_to': time.strftime('%Y-%m-17'),
+            'name': 'Employee 1',
+            'state': 'new',
+            'user_id': self.user_test.id,
             'employee_id': self.employee.id,
-            'date_from': '2014-01-01',
-            'date_to': '2014-01-01',
-        }
-        self.ts1 = self.env['hr_timesheet_sheet.sheet'].create(ts1_dict)
+        })
 
-        ts2_dict = {
-            'employee_id': self.employee.id,
-            'date_from': '2014-01-02',
-            'date_to': '2014-01-04',
-        }
-        self.ts2 = self.env['hr_timesheet_sheet.sheet'].create(ts2_dict)
-
-        ts3_dict = {
-            'employee_id': self.employee.id,
-            'date_from': '2014-01-05',
-            'date_to': '2014-01-15',
-        }
-        self.ts3 = self.env['hr_timesheet_sheet.sheet'].create(ts3_dict)
-
-        ts4_dict = {
-            'employee_id': self.employee.id,
-            'date_from': '2014-01-16',
-            'date_to': '2014-01-16',
-        }
-        self.ts4 = self.env['hr_timesheet_sheet.sheet'].create(ts4_dict)
-
-        analytic_ts1_dict = {
-            'date': '2014-01-01',
+        # I add 5 hours of work timesheet
+        self.timesheet_sheet.write({'timesheet_ids': [(0, 0, {
+            'project_id': self.project_2.id,
+            'date': time.strftime('%Y-%m-11'),
+            'name': 'Develop UT for hr module(1)',
             'user_id': self.user_test.id,
-            'name': 'ddd',
-            'account_id': self.analytic.id,
-            'unit_amount': 3.0,
-            'is_timesheet': True
-        }
-        self.analytic_ts1 = self.env['account.analytic.line'].with_context(
-            user_id=self.user_test.id).create(
-                analytic_ts1_dict)
+            'unit_amount': 5.00,
+        })]})
 
-        analytic_ts2_dict = {
-            'date': '2014-01-02',
-            'user_id': self.user_test.id,
-            'name': 'ddd',
-            'account_id': self.analytic.id,
-            'unit_amount': 5.0,
-            'is_timesheet': True,
-            'sheet_id': self.ts2.id,
-        }
-        self.analytic_ts2 = self.env['account.analytic.line'].with_context(
-            user_id=self.user_test.id).create(
-                analytic_ts2_dict)
-
-        analytic_ts3_dict = {
-            'date': '2014-01-04',
-            'user_id': self.user_test.id,
-            'sheet_id': self.ts2.id,
-            'name': 'ddd',
-            'account_id': self.analytic.id,
-            'unit_amount': 7.0,
-            'is_timesheet': True
-        }
-        self.analytic_ts3 = self.env['account.analytic.line'].with_context(
-            user_id=self.user_test.id).create(
-                analytic_ts3_dict)
-
-        analytic_ts4_dict = {
-            'date': '2014-01-15',
-            'user_id': self.user_test.id,
-            'sheet_id': self.ts4.id,
-            'name': 'ddd',
-            'account_id': self.analytic.id,
-            'unit_amount': 11.5,
-            'is_timesheet': True
-        }
-        self.analytic_ts4 = self.env['account.analytic.line'].with_context(
-            user_id=self.user_test.id).create(
-                analytic_ts4_dict)
-
-        analytic_ts5_dict = {
-            'date': '2014-01-16',
-            'user_id': self.user_test.id,
-            'sheet_id': self.ts1.id,
-            'name': 'ddd',
-            'account_id': self.analytic.id,
-            'unit_amount': 13,
-            'is_timesheet': True
-        }
-        self.analytic_ts5 = self.env['account.analytic.line'].with_context(
-            user_id=self.user_test.id).create(
-                analytic_ts5_dict)
-
-        ts = self.ts1 + self.ts2 + self.ts3 + self.ts4
-        ts.button_confirm()
-        ts.signal_workflow('done')
+        self.timesheet_sheet.action_timesheet_confirm()
+        self.timesheet_sheet.action_timesheet_done()
 
     def test_timesheet_import(self):
         payslip_dict = {
             'employee_id': self.employee.id,
             'contract_id': self.contract.id,
-            'date_from': '2014-01-02',
-            'date_to': '2014-01-15',
+            'date_from': time.strftime('%Y-%m-01'),
+            'date_to': time.strftime('%Y-%m-21'),
         }
         payslip = self.env['hr.payslip'].create(
             payslip_dict)
-        payslip.import_worked_days()
-        for wd in payslip.worked_days_line_ids:
-            self.assertIn(wd.number_of_hours, [11.5, 12])
 
         payslip.import_worked_days()
-        self.assertEqual(len(payslip.worked_days_line_ids), 2)
+        self.assertEqual(payslip.worked_days_line_ids.number_of_hours, 5.0)
