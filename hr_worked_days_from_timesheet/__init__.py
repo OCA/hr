22fb5e98783711449ef0b12c622b558289b7809f
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-08 13:28:39 +0530

    [ADD] hr_worked_days_from_timesheet v10

--- hr_worked_days_from_timesheet/__init__.py
+++ hr_worked_days_from_timesheet/__init__.py
@@ -2,6 +2,7 @@
 # © 2012 Odoo Canada
 # © 2015 Acysos S.L.
 # © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import models
