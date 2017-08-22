3ce1e6a64d2a364aa4b74f6b5d2d90c568e2156e
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-22 15:30:50 +0530

    [IMP] Improved code

--- hr_worked_days_from_timesheet/models/hr_payslip.py
+++ hr_worked_days_from_timesheet/models/hr_payslip.py
@@ -26,8 +26,7 @@ class HrPayslip(models.Model):
             number_of_hours = 0
             for ts in ts_sheet.timesheet_ids:
                 if date_from <= ts.date <= date_to:
-                    unit_amount = ts.unit_amount
-                    number_of_hours += unit_amount
+                    number_of_hours += ts.unit_amount
             if number_of_hours > 0:
                 self.env['hr.payslip.worked_days'].create({
                     'name': _('Timesheet %s') % date_from_formated,
