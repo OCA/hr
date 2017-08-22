edd0e3c67e04f2562a426409e001741945a3ffcd
mreficent <miquel.raich@eficent.com>
2017-08-22 13:51:42 +0200

    hr_holidays_notify_employee_manager: Migration to 10.0

---
+++ hr_holidays_notify_employee_manager/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import hr_holidays
+from . import hr_holidays_config_settings
+from . import res_company
