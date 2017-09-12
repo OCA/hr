dc1c2a023992a6cb1ee8b944c5acc871431223ae
Simone Orsi <simahawk@users.noreply.github.com>
2017-09-12 07:09:33 +0200

    [fix] `hr_employee_id` use deprecated `sequence.get_id()` (#358)

--- hr_employee_id/__manifest__.py
+++ hr_employee_id/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Employee ID',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'license': 'AGPL-3',
     'category': 'Generic Modules/Human Resources',
     'author': 'Michael Telahun Makonnen, '
