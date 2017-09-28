3e76567c53c6e143bb63d82f2efdc5bbf960fa6b
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-28 12:51:26 +0530

    [MIG] hr_period v10

--- hr_period/models/hr_contract.py
+++ hr_period/models/hr_contract.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 # Copyright 2015 Savoir-faire Linux. All Rights Reserved.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import models, fields
 from .hr_fiscal_year import get_schedules
 
 
