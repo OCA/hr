edd4b4469fc9bb6ebac37fa73d0bbaac01a8e7e1
Vo Minh Bao Hieu <hieu.vmb@komit-consulting.com>
2017-10-04 15:07:46 +0700

    [MIG]-Migrate module hr_holidays_compute_days

---
+++ hr_holidays_compute_days/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# ©  2015 iDT LABS (http://www.@idtlabs.sl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import hr_employee
+from . import hr_holidays_status
+from . import hr_holidays
