440bed44b8d4af7cc6400c8d9a4d3cc135968181
Denis Leemann <denis.leemann@camptocamp.com>
2017-10-09 16:34:14 +0200

    Fix imports & lost things in pull/300

--- hr_skill/models/hr_employee.py
+++ hr_skill/models/hr_employee.py
@@ -2,7 +2,7 @@
 # Copyright 2013 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class Employee(models.Model):
