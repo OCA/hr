06c6c38c924f683f2760b3599eac949deb19c218
Duc Dao <ducdd@komit-consulting.com>
2017-11-08 12:34:51 +0700

    [MIG] hr_skill: Migration to 11.0

--- hr_skill/models/__init__.py
+++ hr_skill/models/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import hr_skill
 from . import hr_employee
+from . import hr_skill
--- hr_skill/models/hr_employee.py
+++ hr_skill/models/hr_employee.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import fields, models
 
 
@@ -9,10 +7,10 @@ class Employee(models.Model):
     _inherit = 'hr.employee'
 
     skill_ids = fields.Many2many(
-        'hr.skill',
-        'skill_employee_rel',
-        'employee_id',
-        'skill_id',
-        'Skills',
+        comodel_name='hr.skill',
+        relation='skill_employee_rel',
+        column1='employee_id',
+        column2='skill_id',
+        string='Skills',
         domain="[('child_ids', '=', False)]",
     )
