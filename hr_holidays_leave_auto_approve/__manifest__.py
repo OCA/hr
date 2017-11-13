19c0f7a189ea3083dc4e646f8273d90258e14f46
Fekete Mihai <mihai.fekete@forbiom.eu>
2017-11-13 01:04:56 -0800

    Migrate hr_holidays_leave_auto_approve to version 11.0.
    Alphabetical imports.

--- hr_holidays_leave_auto_approve/__manifest__.py
+++ hr_holidays_leave_auto_approve/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Auto Approve Leaves',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'summary': '''Leave type for auto approval of Leaves''',
     'author': 'Onestein, Odoo Community Association (OCA)',
