21eb4f62daa7e3d9ea35326b36e37e642db8d41b
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-12-26 16:54:09 +0100

    [IMP] hr_public_holidays: Improve README

--- hr_public_holidays/models/__init__.py
+++ hr_public_holidays/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# ©  2015 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import hr_public_holidays_line
