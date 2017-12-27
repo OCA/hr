905cedc007225827018638de3030f0cc15f8ea28
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-12-27 19:27:15 +0100

    [RFR] hr_holidays_compute_days: General refactoring and improvement
    * To not depend on hr_contract. Working times are set also in employee,
      and v11 rely on this data, not in contract working time. This simplifies
      also the code.
    * To not include all the checks while selecting start or end leave date.
      At the end, it's the same for you: what counts is the number of leave
      days. As the compute algorithm already handles overlapping leaves,
      rest days and public holidays, there's no problem with this.
    * It handles fraction of days!! New algorithm put the part of the working
      day that you are leaving. There's a new option at leave type level
      for keeping the behavior of including full days. This option is checked
      by default also for keeping backwards compatibility. This code has been
      made similar to v11.
    * It includes UI helps for being better to select dates in the form view,
      allowing to select only dates instead of date + time.
    * The number of days is not manually modifiable by leave users, but to
      leave managers. If this behavior is not desirable, you can manually
      add other groups to the view, as this won't be overwritten with module
      update.

--- hr_holidays_compute_days/models/hr_employee.py
+++ hr_holidays_compute_days/models/hr_employee.py
@@ -1,37 +1,78 @@
 # -*- coding: utf-8 -*-
-# ©  2015 iDT LABS (http://www.@idtlabs.sl)
+# Copyright 2015 iDT LABS (http://www.@idtlabs.sl)
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from datetime import datetime, time
-from openerp import models, api
+from datetime import timedelta
+from dateutil import rrule
+from odoo import fields, models
+from odoo.tools.float_utils import float_is_zero
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    @api.multi
-    def work_scheduled_on_day(self, date_dt, public_holiday=True,
-                              schedule=True):
-        ''''
-        returns true or false depending on if employee was scheduled to work
-        on a particular day. It does this by both checking if it is a public
-        holiday and the resource calendar of the contract
-        @param date_dt: date for which to check
-        @param public_holiday: optional, whether to consider public holidays,
-                               default=True
-        @param schedule: optional, whether to consider the contract's resource
-                         calendar. default=True
-        '''
+    def _get_public_holidays_leaves(self, start_dt, end_dt):
+        """Get the public holidays for the current employee and given dates in
+        the format expected by resource methods.
+
+        :param: start_dt: Initial datetime.
+        :param: end_dt: End datetime.
+        :return: List of tuples with (start_date, end_date) as elements.
+        """
         self.ensure_one()
-        if public_holiday and self.env['hr.holidays.public'].is_public_holiday(
-                date_dt, employee_id=self.id):
-            return False
-        elif schedule and self.contract_id and self.contract_id.working_hours:
-            hours = self.contract_id.working_hours.get_working_hours_of_date(
-                datetime.combine(date_dt, time.min))
-            if not hours:
-                return False
-        elif schedule and (not self.contract_id or (
-                self.contract_id and not self.contract_id.working_hours)):
-            return date_dt.weekday() not in (5, 6)
-        return True
+        leaves = []
+        for day in rrule.rrule(rrule.YEARLY, dtstart=start_dt, until=end_dt):
+            lines = self.env['hr.holidays.public'].get_holidays_list(
+                day.year, employee_id=self.id,
+            )
+            for line in lines:
+                date = fields.Datetime.from_string(line.date)
+                leaves.append((
+                    date.replace(hour=0, minute=0, second=0, microsecond=0),
+                    date.replace(
+                        hour=23, minute=59, second=59, microsecond=999999,
+                    ),
+                ))
+        return leaves
+
+    def get_work_days_count(self, from_datetime, to_datetime, calendar=None):
+        """Get the number or fraction of days that corresponds to this
+        employee according its working time, leaves and public holidays.
+        """
+        self.ensure_one()
+        days_count = 0.0
+        calendar = calendar or self.calendar_id
+        if not calendar:
+            return days_count
+        leaves = calendar.get_leave_intervals(resource_id=self.resource_id.id)
+        # Add public holidays to leaves
+        if self.env.context.get('exclude_public_holidays'):
+            leaves += self._get_public_holidays_leaves(
+                from_datetime, to_datetime,
+            )
+        for day_intervals in calendar._iter_work_intervals(
+                from_datetime, to_datetime, leaves=leaves):
+            if self.env.context.get('compute_full_days'):
+                days_count += 1
+                continue
+            theoric_hours = calendar.get_working_hours_of_date(
+                start_dt=day_intervals[0][0].replace(
+                    hour=0, minute=0, second=0, microsecond=0,
+                ),
+                leaves=leaves,
+                compute_leaves=False,
+                resource_id=self.resource_id,
+            )
+            if float_is_zero(theoric_hours, 2):
+                # This is because a rest day doesn't contain theoric hours,
+                # but if it's included in the loop, it's because we want to
+                # compute them, and they can only fully count.
+                days_count += 1
+                continue
+            work_time = sum(
+                (interval[1] - interval[0] for interval in day_intervals),
+                timedelta(),
+            )
+            days_count += work_time.total_seconds() / 3600 / theoric_hours
+        return round(days_count, 2)
---
+++ hr_holidays_compute_days/readme/USAGE.rst
@@ -0,0 +1,10 @@
+#. Go to *Leaves > Dashboard*.
+#. Select dragging on the calendar the days you want to be on leave, or go
+   to the form view for selecting start and end dates.
+#. On this form, you have 2 new checks for selecting full days instead of
+   days with time. If you deselect one of these checks, you will be able to
+   select also the starting/ending time of the leave.
+#. Select the proper "Leave Type".
+#. The number of days will be computed and shown according options on
+   leave type. If no leave type yet specified, then default configuration is
+   to exclude both rest days and public holidays.
