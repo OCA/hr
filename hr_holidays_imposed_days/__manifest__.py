58c33bb8e68a1627d27859316e80b5b0ee8bec25
Andrea <a.stirpe@onestein.nl>
2018-01-09 12:56:43 +0100

    [MIG] Port hr_holidays_imposed_days to V11 + refactoring

--- hr_holidays_imposed_days/__manifest__.py
+++ hr_holidays_imposed_days/__manifest__.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
 # Author: Damien Crier
-# Copyright 2016 Camptocamp SA
+# Copyright 2016-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Imposed holidays days',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Human Resources',
     'license': 'AGPL-3',
     'author': 'Camptocamp SA, '
               'Odoo Community Association (OCA)',
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://github.com/OCA/hr',
     'depends': ['hr_holidays'],
-    'data': ['views/imposed_days.xml',
-             'security/ir.model.access.csv',
-             ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_holidays_imposed_view.xml',
+    ],
     'installable': True,
 }
--- hr_holidays_imposed_days/tests/__init__.py
+++ hr_holidays_imposed_days/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import test_holidays_imposed_days
+from . import test_hr_holidays_imposed
