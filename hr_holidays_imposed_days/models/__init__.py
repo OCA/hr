212835ef631439acbb2f84152820eaff0a092fcf
Andrea <a.stirpe@onestein.nl>
2018-01-09 12:56:43 +0100

    [MIG] Port hr_holidays_imposed_days to V11 + refactoring

--- hr_holidays_imposed_days/models/__init__.py
+++ hr_holidays_imposed_days/models/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import hr_imposed_holidays
+from . import hr_holidays_imposed
 from . import hr_employee
