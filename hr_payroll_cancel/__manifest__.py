82361858e65a8220ce851c2cf3a28cb90493a79f
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-01-22 15:50:49 +0530

    [MIG] hr_payroll_cancel v10

--- hr_payroll_cancel/__manifest__.py
+++ hr_payroll_cancel/__manifest__.py
@@ -2,23 +2,24 @@
 # Copyright 2014 - Vauxoo http://www.vauxoo.com/
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Hr Payroll Cancel",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Vauxoo, Eficent, "
+              "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     "category": "HR",
     "website": "http://www.vauxoo.com/",
     "license": "AGPL-3",
     "depends": [
-        "hr_payroll"
+        "hr_payroll_account",
+        "account_cancel"
     ],
     "data": [
         "views/hr_payslip_view.xml",
-        "hr_payslip_workflow.xml",
     ],
     "installable": True,
-    "auto_install": False,
-    "post_init_hook": 'post_init_hook',
 }
