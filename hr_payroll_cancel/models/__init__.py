dc4abdecbecaa4a25d9d190448645ad72b4cefaa
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-01-22 15:50:49 +0530

    [MIG] hr_payroll_cancel v10

--- hr_payroll_cancel/models/__init__.py
+++ hr_payroll_cancel/models/__init__.py
@@ -5,4 +5,4 @@
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 #   (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
+from . import hr_payroll
