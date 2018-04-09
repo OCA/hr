0fc18c9ea61f2b5cf351f6788a4a5dd5ca8574dd
David Dufresne <dufresne.david.2@gmail.com>
2018-04-09 12:34:50 -0400

    [11.0][MIG] Migrate hr_experience

--- hr_experience/__manifest__.py
+++ hr_experience/__manifest__.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Experience Management",
-    "version": "10.0.1.0.0",
+    "version": "1.0.0",
     "author": "Savoir-faire Linux,"
               "OpenSynergy Indonesia,"
+              "Numigi,"
               "Odoo Community Association (OCA)",
-    "website": "http://www.savoirfairelinux.com",
+    "website": "https://github.com/oca/hr",
     "license": "AGPL-3",
     "category": "Human Resources",
     "depends": ["hr"],
