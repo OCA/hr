ca78ed31e0d4234c5aea558659496bd6702519fe
tarteo <d.sluijk@onestein.nl>
2018-05-16 16:27:23 +0200

    [MIG] hr_holidays_settings: Migration to 11.0

--- hr_holidays_settings/__manifest__.py
+++ hr_holidays_settings/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "HR Holidays Settings",
     "summary": "Enables Settings Form for HR Holidays.",
-    "version": "10.0.1.1.0",
+    "version": "11.0.1.1.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "application": False,
-    "installable": True,
     "depends": ["hr_holidays"],
-    "data": ['views/hr_holidays_config_settings.xml'],
+    "data": [
+        "views/res_config_settings.xml"
+    ],
+    "installable": True,
 }
