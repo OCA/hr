49d14a2bb45a65d42a1cd6ac057539d085863af7
Vacha Trivedi <vacha.trivedi@serpentcs.com>
2018-06-21 17:29:58 +0530

    [MIG][11.0]hr_employee_phone_extension:Migration to 11.0

--- hr_employee_phone_extension/__manifest__.py
+++ hr_employee_phone_extension/__manifest__.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 # copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Employee Phone Extension',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Human Resources',
     'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
-    'website': 'http://www.savoirfairelinux.com',
+    'website': "https://github.com/oca/hr",
     'license': 'AGPL-3',
-    'depends': ['hr', ],
+    'depends': ['hr'],
     'data': [
         'views/hr_employee_view.xml',
     ],
-    'demo': [],
-    'test': [],
     'installable': True,
     'auto_install': False,
 }
