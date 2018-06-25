179c752a9dc3093078412cd0368ffeca046e13e2
Vacha Trivedi <vacha.trivedi@serpentcs.com>
2018-06-25 15:40:11 +0530

    [MIG][11.0]hr_contract_multi_jobs:Migrated from 10.0 to 11.0.

---
+++ hr_contract_multi_job/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import hr_job
+from . import hr_contract
+from . import hr_contract_job
