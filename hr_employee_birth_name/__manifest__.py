42be5c4ee20fa8d0bc0d2149c92e167e2079023d
Vacha Trivedi <vacha.trivedi@serpentcs.com>
2018-07-03 18:29:33 +0530

    [MIG][11.0]hr_employee_birth_name:Migration to 11.0.
    [UPD] Update hr_employee_birth_name.pot
    
    Translated using Weblate (Portuguese (Brazil))
    
    Currently translated at 100,0% (2 of 2 strings)
    
    Translation: hr-11.0/hr-11.0-hr_employee_birth_name
    Translate-URL: https://translation.odoo-community.org/projects/hr-11-0/hr-11-0-hr_employee_birth_name/pt_BR/

--- hr_employee_birth_name/__manifest__.py
+++ hr_employee_birth_name/__manifest__.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 # copyright  2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Employee Birth Name',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Human Resources',
     'author': "Camptocamp SA,Odoo Community Association (OCA)",
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://github.com/OCA/hr',
     'license': 'AGPL-3',
     'depends': ['hr', ],
     'data': [
         'views/hr_employee_view.xml',
     ],
-    'demo': [],
-    'test': [],
     'installable': True,
     'auto_install': False,
 }
--- hr_employee_birth_name/models/hr_employee.py
+++ hr_employee_birth_name/models/hr_employee.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # copyright  2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
