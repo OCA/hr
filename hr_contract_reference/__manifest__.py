45f07f450904a7b5141f78bc8d9f2db0d79e9462
Vacha Trivedi <vacha.trivedi@serpentcs.com>
2018-07-03 12:42:47 +0530

    [Mig][11.0]hr_contract_reference:Migration to 11.0.

--- hr_contract_reference/__manifest__.py
+++ hr_contract_reference/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # copyright 2011,2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "HR Contract Reference",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Generic Modules/Human Resources",
     "author": 'Michael Telahun Makonnen, '
               'Fekete Mihai (Forest and Biomass Services Romania), '
               'Odoo Community Association (OCA)',
-    "website": "http://miketelahun.wordpress.com",
+    "website": "https://github.com/OCA/hr",
     "license": "AGPL-3",
     "depends": ["hr_contract"],
     "data": [
