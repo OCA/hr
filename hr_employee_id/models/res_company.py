fc55429e0b05fa52cabf141424cff65bfd477339
Vacha Trivedi <vacha.trivedi@serpentcs.com>
2018-07-04 14:51:51 +0530

    [MIG][11.0]hr_employee_id:Migration to 11.0.

--- hr_employee_id/models/res_company.py
+++ hr_employee_id/models/res_company.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2015 Salton Massally <smassally@idtlabs.sl>
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
