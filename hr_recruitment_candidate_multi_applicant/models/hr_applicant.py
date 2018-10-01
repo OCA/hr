f3b9f2bad5418cfc3011bc131d7f7ff03a736257
Denis Leemann <denis.leemann@camptocamp.com>
2018-10-01 11:30:07 +0200

    fixup! Add hr_recruitment_candidate_multi_applicant

--- hr_recruitment_candidate_multi_applicant/models/hr_applicant.py
+++ hr_recruitment_candidate_multi_applicant/models/hr_applicant.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HrApplicant(models.Model):
