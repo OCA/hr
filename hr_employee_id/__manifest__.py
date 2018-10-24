f3ba6835ae84cc8f6622efc5e35ef6649af19ff3
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-10-24 21:49:15 +0300

    [MIG] hr_employee_id: Migration to 12.0

--- hr_employee_id/__manifest__.py
+++ hr_employee_id/__manifest__.py
@@ -1,21 +1,26 @@
-# © 2011, 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
-# © 2016 OpenSynergy Indonesia
+# Copyright 2011, 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Employee ID',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Generic Modules/Human Resources',
-    'author': 'Michael Telahun Makonnen, '
-              'OpenSynergy Indonesia,'
-              'Camptocamp,'
-              'Odoo Community Association (OCA)',
+    'author':
+        'Brainbean Apps, '
+        'Michael Telahun Makonnen, '
+        'OpenSynergy Indonesia, '
+        'Camptocamp, '
+        'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/hr',
-    'depends': ['hr'],
+    'depends': [
+        'hr'
+    ],
     'data': [
-        'views/res_config_views.xml',
-        'views/hr_employee_views.xml',
         'data/hr_employee_sequence.xml',
+        'views/hr_employee_views.xml',
+        'views/res_config_settings_views.xml',
     ],
     'installable': True,
 }
