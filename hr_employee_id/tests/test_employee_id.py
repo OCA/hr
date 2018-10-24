8c2a9b9c1e3b109a570d92d328f22096adac3998
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-10-24 21:49:15 +0300

    [MIG] hr_employee_id: Migration to 12.0

--- hr_employee_id/tests/test_employee_id.py
+++ hr_employee_id/tests/test_employee_id.py
@@ -1,5 +1,6 @@
-# © 2015 Salton Massally <smassally@idtlabs.sl>
-# © 2016 OpenSynergy Indonesia
+# Copyright 2015 Salton Massally <smassally@idtlabs.sl>
+# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
@@ -11,7 +12,7 @@ class TestEmployeeID(common.TransactionCase):
         super(TestEmployeeID, self).setUp()
         self.employee_model = self.env['hr.employee']
         self.company = self.env.ref('base.main_company')
-        self.sequence = self.env.ref('hr_employee_id.seq_employeeid_ref')
+        self.sequence = self.env.ref('hr_employee_id.seq_hr_employee_id')
 
     def test_random_id_generation(self):
         # test ID generation for random
@@ -28,17 +29,28 @@ class TestEmployeeID(common.TransactionCase):
 
     def test_sequences_id_generation(self):
         # test ID generation for a provided sequence
-        self.company.write({'employee_id_gen_method': 'sequence',
-                            'employee_id_sequence': self.sequence.id})
+        self.company.write({
+            'employee_id_gen_method': 'sequence',
+            'employee_id_sequence': self.sequence.id
+        })
         employee = self.employee_model.create({'name': 'Employee'})
 
         self.assertTrue(len(employee.identification_id))
 
+    def test_no_sequences_id_generation(self):
+        # test ID generation for a provided sequence
+        self.company.write({'employee_id_gen_method': 'sequence'})
+        employee = self.employee_model.create({'name': 'Employee'})
+
+        self.assertEqual(employee.identification_id, False)
+
     def test_custom_id(self):
         # if we pass the ID no generation occurs.
         # Let's set a sequence and check that is not used at all
-        self.company.write({'employee_id_gen_method': 'sequence',
-                            'employee_id_sequence': self.sequence.id})
+        self.company.write({
+            'employee_id_gen_method': 'sequence',
+            'employee_id_sequence': self.sequence.id
+        })
         number = self.sequence.number_next
         employee = self.employee_model.create({
             'name': 'Employee', 'identification_id': 'THERE_YOU_GO'
@@ -48,10 +60,12 @@ class TestEmployeeID(common.TransactionCase):
 
     def test_configuration_default_values(self):
         # test loading of default configuration values
-        self.company.write({'employee_id_gen_method': None,
-                            'employee_id_random_digits': None,
-                            'employee_id_sequence': None})
-        config_model = self.env['hr.employeeid.config.settings']
+        self.company.write(self.company.default_get([
+            'employee_id_gen_method',
+            'employee_id_random_digits',
+            'employee_id_sequence'
+        ]))
+        config_model = self.env['res.config.settings']
         config = config_model.create({})
         self.assertTrue(config.company_id.id == self.company.id)
         self.assertTrue(config.employee_id_gen_method == 'random')
