3d889c8b8f511cc020dad05b94eceb552ca50e81
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-10-28 14:57:16 +0200

    [12.0][ADD] hr_employee_document: various documents attached to the employee profile
    [UPD] Update hr_employee_document.pot
    
    [ADD] icon.png

---
+++ hr_employee_document/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'HR Employee Document',
+    'version': '12.0.1.0.0',
+    'category': 'Human Resources',
+    'website': 'https://github.com/OCA/hr',
+    'author':
+        'Brainbean Apps, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'Documents attached to the employee profile',
+    'depends': [
+        'hr',
+        'document',
+    ],
+    'data': [
+        'views/hr_employee.xml',
+    ],
+}
