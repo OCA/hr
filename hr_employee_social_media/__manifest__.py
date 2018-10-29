c857fc0116539878d22e2a6ebaa0f5ab50352c0d
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-10-29 14:33:22 +0200

    [MIG] hr_employee_social_media: Migration to 12.0

--- hr_employee_social_media/__manifest__.py
+++ hr_employee_social_media/__manifest__.py
@@ -1,26 +1,26 @@
-# -*- coding: utf-8 -*-
-# ©  2017 Denis Leemann, Camptocamp SA
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Employee Social Media',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Human Resources',
-    'author': "Camptocamp SA, Odoo Community Association (OCA)",
-    'website': 'http://www.camptocamp.com',
+    'author':
+        'Brainbean Apps, '
+        'Camptocamp SA, '
+        'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/hr',
     'license': 'AGPL-3',
-    'depends': ['hr', ],
+    'depends': [
+        'hr',
+    ],
     'data': [
-        # data
-        'data/social_media.xml',
-        # security
         'security/ir.model.access.csv',
-        # views
+        'data/social_media.xml',
         'views/hr_employee_view.xml',
         'views/hr_social_media.xml',
     ],
-    'demo': [],
-    'test': [],
     'installable': True,
     'auto_install': False,
 }
---
+++ hr_employee_social_media/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module add the following to Employee:
+
+* Menu entry in settings for creating new social-media
+* View Tree in employee's form to list/add new social-media accounts
