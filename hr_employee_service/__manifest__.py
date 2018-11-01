bc4121e60dba637bf9bc48cf68e797055a570360
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-01 09:05:25 +0200

    [12.0][ADD] hr_employee_service: employee service information & duration

---
+++ hr_employee_service/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2018 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'HR Employee Service',
+    'version': '12.0.1.0.0',
+    'category': 'Human Resources',
+    'website': 'https://github.com/OCA/hr',
+    'author':
+        'Brainbean Apps, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'Employee service information & duration',
+    'depends': [
+        'hr',
+    ],
+    'external_dependencies': {
+        'python': [
+            'dateutil',
+        ],
+    },
+    'data': [
+        'views/hr_employee.xml',
+    ],
+}
