2cc933facda822495363cde9807665ec3ec8aa5d
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-11-01 17:05:35 +0100

    Bump version of hr_holidays_leave_auto_approve

--- hr_holidays_leave_auto_approve/__manifest__.py
+++ hr_holidays_leave_auto_approve/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Auto Approve Leaves',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'license': 'AGPL-3',
     'summary': '''Leave type for auto approval of Leaves''',
     'author': 'Onestein, Odoo Community Association (OCA)',
