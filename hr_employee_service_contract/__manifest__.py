9f23237c6f95cf766103c64be5d585aaa597e10a
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-08 06:11:57 +0200

    [12.0][ADD] hr_employee_service_contract: calculate service length based on contracts

---
+++ hr_employee_service_contract/__manifest__.py
@@ -0,0 +1,32 @@
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'HR Employee Service from Contracts',
+    'version': '12.0.1.0.0',
+    'category': 'Human Resources',
+    'website': 'https://github.com/OCA/hr',
+    'author':
+        'Brainbean Apps, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': (
+        'Employee service information & duration based on employee\'s'
+        ' contracts'
+    ),
+    'depends': [
+        'hr',
+        'hr_contract',
+        'hr_employee_service',
+    ],
+    'external_dependencies': {
+        'python': [
+            'dateutil',
+        ],
+    },
+    'data': [
+        'views/hr_employee.xml',
+    ],
+}
