7939520fe0cff99565b0d129123e2b9f6aab4f14
Anand Kansagra <kansagraanand@hotmail.com>
2018-11-28 17:35:26 +0530

    [MIG] hr_experience: Migration to 12.0.

--- hr_experience/models/hr_certification.py
+++ hr_experience/models/hr_certification.py
@@ -5,6 +5,8 @@ from odoo import models, fields
 
 
 class HrCertification(models.Model):
+    """Added the details of the certification."""
+
     _name = 'hr.certification'
     _inherit = 'hr.curriculum'
 
