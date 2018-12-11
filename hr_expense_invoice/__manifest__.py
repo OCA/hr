144ffddd213ac7a78f50eb3e49a769137e0b8492
cubells <vicent@vcubells.net>
2018-12-11 12:09:33 +0100

    [MIG] hr_expense_invoice: Migration to 11.0

--- hr_expense_invoice/__manifest__.py
+++ hr_expense_invoice/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Supplier invoices on HR expenses',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Human Resources',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'website': 'https://www.tecnativa.com',
+    'website': 'https://github.com/OCA/hr',
     'depends': [
         'hr_expense',
     ],
---
+++ hr_expense_invoice/readme/USAGE.rst
@@ -0,0 +1,4 @@
+* Create an expense sheet.
+* Add an expense line to sheet with an invoice_id selected or create one new.
+* Process expense sheet.
+* On paying expense sheet, you are reconciling supplier invoice too.
