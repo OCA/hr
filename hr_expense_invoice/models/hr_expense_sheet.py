b36caa17bbfe4e9daae83d132b410dc059e242e5
cubells <vicent@vcubells.net>
2018-12-11 12:09:33 +0100

    [MIG] hr_expense_invoice: Migration to 11.0

--- hr_expense_invoice/models/hr_expense_sheet.py
+++ hr_expense_invoice/models/hr_expense_sheet.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, exceptions, models
+from odoo import _, api, models
+from odoo.exceptions import UserError
+from odoo.tools import float_compare
 
 
 class HrExpenseSheet(models.Model):
@@ -11,6 +12,7 @@ class HrExpenseSheet(models.Model):
 
     @api.multi
     def action_sheet_move_create(self):
+        precision = self.env['decimal.precision'].precision_get('Account')
         expense_line_ids = self.mapped('expense_line_ids').filtered(
             lambda r: r.invoice_id,
         )
@@ -28,15 +30,16 @@ class HrExpenseSheet(models.Model):
             )
             if len(c_move_lines) > 1:
                 c_move_lines = c_move_lines[0]
+            residual = line.invoice_id.residual
             c_move_lines |= line.invoice_id.move_id.line_ids.filtered(
                 lambda x:
                 x.account_id == line.invoice_id.account_id and
-                x.credit == line.invoice_id.residual)
+                float_compare(x.credit, residual, precision) == 0)
             if len(c_move_lines) != 2:
-                raise exceptions.Warning(
+                raise UserError(
                     _('Cannot reconcile supplier invoice payable with '
                       'generated line. Please check amounts and see '
                       'if the invoice is already added or paid. '
-                      'Invoice: %s') % line.invoice.name)
+                      'Invoice: %s') % line.invoice_id.number)
             c_move_lines.reconcile()
         return res
