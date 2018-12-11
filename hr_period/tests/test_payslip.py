279a04d89c8f53f568f4eda2c1da4158c79a6bb0
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-12-11 09:45:56 +0100

    [12.0][MIG] Migrate module hr_period to v12

--- hr_period/tests/test_payslip.py
+++ hr_period/tests/test_payslip.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Savoir-faire Linux. All Rights Reserved.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_hr_fiscalyear
 from odoo.exceptions import UserError, ValidationError
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 
 
 class PayslipCase(test_hr_fiscalyear.TestHrFiscalyear):
@@ -152,7 +152,8 @@ class PayslipCase(test_hr_fiscalyear.TestHrFiscalyear):
             'hr_period_id': periods2[0].id,
             'company_id': self.company2.id})
         run.onchange_company_id()
-        self.assertEqual(run.hr_period_id.date_start, '2016-01-01')
+        self.assertEqual(
+            run.hr_period_id.date_start.strftime(DF), '2016-01-01')
 
     def test_contract(self):
         fy = self.create_fiscal_year(
