031e26f37b800ad960ec267eb9e1467347fc4ac1
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-12-22 13:39:56 +0530

    [MIG] hr_holidays_notify_employee_manager v12

--- hr_holidays_notify_employee_manager/models/__init__.py
+++ hr_holidays_notify_employee_manager/models/__init__.py
@@ -1,6 +1,5 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import hr_holidays
+from . import hr_leave
 from . import res_config_settings
 from . import res_company
---
+++ hr_holidays_notify_employee_manager/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module extends the functionality of the Leaves App to make possible
+to notify by mail the manager of the employee requesting the leave.
