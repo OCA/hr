c3e17024a00747554b82417821a0403d1ab8d06e
Beñat Jimenez <benat.jimenez@eficent.com>
2019-01-16 09:28:26 +0100

    [MIG] hr_payslip_change_state: Migration to version 12

--- hr_payslip_change_state/__manifest__.py
+++ hr_payslip_change_state/__manifest__.py
@@ -1,12 +1,13 @@
-# © 2016 - Eficent http://www.eficent.com/
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2019 - Eficent http://www.eficent.com/
+# Copyright 2019 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Human Resources Payslip Change State",
     "summary": "Change the state of many payslips at a time",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
     "author": "Eficent, Odoo Community Association (OCA)",
     "depends": [
         'hr_payroll_cancel'
