6b540b5d9aaa712def60da8b3d98079110acde64
eslam <esla.amer@gmail.com>
2019-02-13 15:44:43 +0200

    [MIG]hr_contract_multi_job:Migration to 12.0

--- hr_contract_multi_job/models/hr_contract.py
+++ hr_contract_multi_job/models/hr_contract.py
@@ -28,19 +28,10 @@ class HrContract(models.Model):
         contains one and only one record with field is_main_job == True
         """
         for contract in self:
-            main_job = contract.contract_job_ids.filtered('is_main_job').job_id
+            main_job = contract.contract_job_ids.filtered('is_main_job')
+            if len(main_job) > 1:
+                raise UserError(
+                    _("You must assign one and only one job position "
+                      "as main job position."))
+            main_job = main_job.job_id
             contract.job_id = main_job
-
-    @api.multi
-    @api.constrains('contract_job_ids')
-    def _check_one_main_job(self):
-        for contract in self:
-            # if the contract has no job assigned, a main job
-            # is not required. Otherwise, one main job assigned is
-            # required.
-            if contract.contract_job_ids:
-                main_jobs = contract.contract_job_ids.filtered('is_main_job')
-                if len(main_jobs) != 1:
-                    raise UserError(
-                        _("You must assign one and only one job position "
-                          "as main job position."))
--- hr_contract_multi_job/models/hr_contract_job.py
+++ hr_contract_multi_job/models/hr_contract_job.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HrContractJob(models.Model):
