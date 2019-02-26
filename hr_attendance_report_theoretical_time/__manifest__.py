5b142c220745de5aa756aed4d7a76e713768c13e
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-02-26 14:52:33 +0100

    [12.0] hr_attendance_report_theoretical_time: add my attendance as default filter

--- hr_attendance_report_theoretical_time/__manifest__.py
+++ hr_attendance_report_theoretical_time/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Theoretical vs Attended Time Analysis",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "Tecnativa, "
