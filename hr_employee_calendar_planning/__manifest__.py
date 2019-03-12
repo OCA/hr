5449d3af5950e218d70bc5e589949a3c1ac86fbc
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-03-13 00:47:18 +0100

    [ADD] hr_employee_calendar_planning: Employee Calendar Planning
    This module allows to manage employee working time with profiles by date
    intervals.
    
    The profiles are regular working time calendars, but they are treated as
    master ones here, allowing you to compose complexes working times by dates.
    
    Under the hook, a unique working time is created for each employee with the
    proper composition for not affecting the rest of the functionality linked to
    this model.
    
    Installation
    ============
    
    During the installation of the module, current working times are split by
    start/end dates for having consistent data, and the potential new composed
    calendar planning is saved instead on the employee.
    
    Configuration
    =============
    
    1. Go to *Employees > Employees*.
    2. Open or create a new one.
    3. On the "Public Information" page, fill the section "Calendar planning" with:
    
       * Starting date (optional).
       * Ending date (optional).
       * Working time to apply during that date interval.
    
    Known issues / Roadmap
    ======================
    
    * Add a wizard for generating next year calendar planning based on current one
      in batch.
    * Add constraint for avoiding planning lines overlapping.
    * Avoid the regeneration of whole private calendars each time a change is
      detected.
    
    [UPD] README.rst

---
+++ hr_employee_calendar_planning/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Employee Calendar Planning",
+    "version": "10.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_employee_views.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
