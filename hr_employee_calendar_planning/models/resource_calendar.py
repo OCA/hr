ed2a9b8f701cb329f47cf07a0e33a6c69dab0f0d
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-03-15 10:22:52 +0100

    [MIG] hr_employee_calendar_planning
    [UPD] README.rst

--- hr_employee_calendar_planning/models/resource_calendar.py
+++ hr_employee_calendar_planning/models/resource_calendar.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResourceCalendar(models.Model):
@@ -11,3 +10,14 @@ class ResourceCalendar(models.Model):
     active = fields.Boolean(
         default=True,
     )
+
+    @api.multi
+    def write(self, vals):
+        res = super(ResourceCalendar, self).write(vals)
+        for record in self.filtered('active'):
+            calendars = self.env['hr.employee.calendar'].search([
+                ('calendar_id', '=', record.id)
+            ])
+            for employee in calendars.mapped('employee_id'):
+                employee._regenerate_calendar()
+        return res
