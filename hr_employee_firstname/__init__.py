744545f35a2156441ddc30b3f2f1b0d80e3a2313
Andrea <a.stirpe@onestein.nl>
2019-03-28 15:53:44 +0100

    [12.0][MIG] hr_employee_firstname

--- hr_employee_firstname/__init__.py
+++ hr_employee_firstname/__init__.py
@@ -1,5 +1,4 @@
-# Copyright (C)  2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
 from .init_hook import post_init_hook
---
+++ hr_employee_firstname/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows you to add firstname and lastname in employee form,
+and concatenate both in name field.
