e0dbf846d78a1a415ecdcc8840685f13a73a147d
Andrea <a.stirpe@onestein.nl>
2019-04-08 08:36:05 +0200

    [FIX/IMP] Make required attributes dynamic

--- hr_employee_firstname/models/hr_employee.py
+++ hr_employee_firstname/models/hr_employee.py
@@ -2,7 +2,12 @@
 # Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
 
 UPDATE_PARTNER_FIELDS = ['firstname', 'lastname', 'user_id', 'address_home_id']
 
@@ -16,32 +21,37 @@ class HrEmployee(models.Model):
 
     @api.onchange('firstname', 'lastname')
     def _onchange_firstname_lastname(self):
-        if self.firstname and self.lastname:
+        if self.firstname or self.lastname:
             self.name = self._get_name(self.lastname, self.firstname)
 
-    def _firstname_default(self):
-        return ' ' if self.env.context.get('module') else False
-
-    firstname = fields.Char(default=_firstname_default)
-    lastname = fields.Char(required=True, default=_firstname_default)
+    firstname = fields.Char()
+    lastname = fields.Char()
 
     @api.model
     def create(self, vals):
-        if vals.get('firstname') and vals.get('lastname'):
-            vals['name'] = self._get_name(vals['lastname'], vals['firstname'])
-
+        if vals.get('firstname') or vals.get('lastname'):
+            vals['name'] = self._get_name(
+                vals.get('lastname'), vals.get('firstname'))
         elif vals.get('name'):
             vals['lastname'] = self.split_name(vals['name'])['lastname']
             vals['firstname'] = self.split_name(vals['name'])['firstname']
+        else:
+            raise ValidationError(_('No name set.'))
         res = super().create(vals)
         res._update_partner_firstname()
         return res
 
     @api.multi
     def write(self, vals):
-        if vals.get('firstname') or vals.get('lastname'):
-            lastname = vals.get('lastname') or self.lastname or ' '
-            firstname = vals.get('firstname') or self.firstname or ' '
+        if 'firstname' in vals or 'lastname' in vals:
+            if 'lastname' in vals:
+                lastname = vals.get('lastname')
+            else:
+                lastname = self.lastname
+            if 'firstname' in vals:
+                firstname = vals.get('firstname')
+            else:
+                firstname = self.firstname
             vals['name'] = self._get_name(lastname, firstname)
         elif vals.get('name'):
             vals['lastname'] = self.split_name(vals['name'])['lastname']
@@ -56,18 +66,29 @@ class HrEmployee(models.Model):
         clean_name = " ".join(name.split(None)) if name else name
         return self.env['res.partner']._get_inverse_name(clean_name)
 
+    @api.multi
+    def _inverse_name(self):
+        """Try to revert the effect of :meth:`._compute_name`."""
+        for record in self:
+            parts = self.env['res.partner']._get_inverse_name(record.name)
+            record.lastname = parts['lastname']
+            record.firstname = parts['firstname']
+
     @api.model
-    def _update_employee_names(self):
-        employees = self.search([
-            ('firstname', '=', ' '),
-            ('lastname', '=', ' ')
-        ])
-        for employee in employees:
-            split_name = self.split_name(employee.name)
-            employee.write({
-                'firstname': split_name['firstname'],
-                'lastname': split_name['lastname'],
-            })
+    def _install_employee_firstname(self):
+        """Save names correctly in the database.
+
+        Before installing the module, field ``name`` contains all full names.
+        When installing it, this method parses those names and saves them
+        correctly into the database. This can be called later too if needed.
+        """
+        # Find records with empty firstname and lastname
+        records = self.search([("firstname", "=", False),
+                               ("lastname", "=", False)])
+
+        # Force calculations there
+        records._inverse_name()
+        _logger.info("%d employees updated installing module.", len(records))
 
     def _update_partner_firstname(self):
         for employee in self:
@@ -77,3 +98,10 @@ class HrEmployee(models.Model):
                 'firstname': employee.firstname,
                 'lastname': employee.lastname,
             })
+
+    @api.constrains("firstname", "lastname")
+    def _check_name(self):
+        """Ensure at least one name is set."""
+        for record in self:
+            if not (record.firstname or record.lastname):
+                raise ValidationError(_('No name set.'))
