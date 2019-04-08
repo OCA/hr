1b84b988e528ee29929c322a0bac591e8f67cf23
Andrea <a.stirpe@onestein.nl>
2019-04-08 08:36:05 +0200

    [FIX/IMP] Make required attributes dynamic

--- hr_employee_firstname/__manifest__.py
+++ hr_employee_firstname/__manifest__.py
@@ -1,12 +1,13 @@
-# ©  2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2010-2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'HR Employee First Name, Last Name',
     'version': '12.0.1.0.0',
-    'author': "Savoir-faire Linux, "
-              "Fekete Mihai (Forest and Biomass Services Romania), "
-              "Odoo Community Association (OCA)",
+    'author': 'Savoir-faire Linux, '
+              'Fekete Mihai (Forest and Biomass Services Romania), '
+              'Onestein, '
+              'Odoo Community Association (OCA)',
     'maintainer': 'Savoir-faire Linux',
     'website': 'https://github.com/OCA/hr/',
     'license': 'AGPL-3',
@@ -19,6 +20,6 @@
     'data': [
         'views/hr_view.xml',
     ],
-    "post_init_hook": "post_init_hook",
+    'post_init_hook': 'post_init_hook',
     'installable': True,
 }
