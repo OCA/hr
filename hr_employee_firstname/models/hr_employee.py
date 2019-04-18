df178e19438fe78fb237755976ca67f11cfe9acc
Andrea <a.stirpe@onestein.nl>
2019-04-18 09:20:33 +0200

    [FIX/IMP] Make required attributes dynamic

--- hr_employee_firstname/models/hr_employee.py
+++ hr_employee_firstname/models/hr_employee.py
@@ -1,7 +1,9 @@
-# ©  2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2010-2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 UPDATE_PARTNER_FIELDS = ['firstname', 'lastname', 'user_id', 'address_home_id']
 
@@ -13,39 +15,39 @@ class HrEmployee(models.Model):
     def _get_name(self, lastname, firstname):
         return self.env['res.partner']._get_computed_name(lastname, firstname)
 
-    @api.multi
     @api.onchange('firstname', 'lastname')
-    def get_name(self):
-        for employee in self:
-            if employee.firstname and employee.lastname:
-                employee.name = self._get_name(
-                    employee.lastname, employee.firstname)
+    def _onchange_firstname_lastname(self):
+        if self.firstname or self.lastname:
+            self.name = self._get_name(self.lastname, self.firstname)
 
-    def _firstname_default(self):
-        return ' ' if self.env.context.get('module') else False
-
-    firstname = fields.Char(
-        "Firstname", default=_firstname_default)
-    lastname = fields.Char(
-        "Lastname", required=True, default=_firstname_default)
+    firstname = fields.Char()
+    lastname = fields.Char()
 
     @api.model
     def create(self, vals):
-        if vals.get('firstname') and vals.get('lastname'):
-            vals['name'] = self._get_name(vals['lastname'], vals['firstname'])
-
+        if vals.get('firstname') or vals.get('lastname'):
+            vals['name'] = self._get_name(
+                vals.get('lastname'), vals.get('firstname'))
         elif vals.get('name'):
             vals['lastname'] = self.split_name(vals['name'])['lastname']
             vals['firstname'] = self.split_name(vals['name'])['firstname']
+        else:
+            raise ValidationError(_('No name set.'))
         res = super(HrEmployee, self).create(vals)
         self._update_partner_firstname(res)
         return res
 
     @api.multi
     def write(self, vals):
-        if vals.get('firstname') or vals.get('lastname'):
-            lastname = vals.get('lastname') or self.lastname or ' '
-            firstname = vals.get('firstname') or self.firstname or ' '
+        if 'firstname' in vals or 'lastname' in vals:
+            if 'lastname' in vals:
+                lastname = vals.get('lastname')
+            else:
+                lastname = self.lastname
+            if 'firstname' in vals:
+                firstname = vals.get('firstname')
+            else:
+                firstname = self.firstname
             vals['name'] = self._get_name(lastname, firstname)
         elif vals.get('name'):
             vals['lastname'] = self.split_name(vals['name'])['lastname']
@@ -63,7 +65,7 @@ class HrEmployee(models.Model):
     @api.model
     def _update_employee_names(self):
         employees = self.search([
-            ('firstname', '=', ' '), ('lastname', '=', ' ')])
+            ('firstname', '=', False), ('lastname', '=', False)])
 
         for ee in employees:
             split_name = self.split_name(ee.name)
@@ -80,3 +82,10 @@ class HrEmployee(models.Model):
                 partners += partner
         partners.write({'firstname': employee.firstname,
                         'lastname': employee.lastname})
+
+    @api.constrains("firstname", "lastname")
+    def _check_name(self):
+        """Ensure at least one name is set."""
+        for record in self:
+            if not (record.firstname or record.lastname or record.name):
+                raise ValidationError(_('No name set.'))
