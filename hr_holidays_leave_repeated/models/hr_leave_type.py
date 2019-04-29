8e76d44ed64a41dc6f669b48e73d72e9970dd8d3
Andrea Stirpe <a.stirpe@onestein.nl>
2019-04-29 11:31:57 +0200

    [ADD] hr_holidays_leave_repeated (#581)

---
+++ hr_holidays_leave_repeated/models/hr_leave_type.py
@@ -0,0 +1,10 @@
+# Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class HrLeaveType(models.Model):
+    _inherit = 'hr.leave.type'
+
+    repeat = fields.Boolean()
