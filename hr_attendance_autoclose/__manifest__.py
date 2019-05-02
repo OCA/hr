c9b7e3f64cfc3ce484c251e0e8a2090d44ab7a63
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-05-02 10:59:21 +0200

    [MIG][11.0] hr_attendance_autoclose

--- hr_attendance_autoclose/__manifest__.py
+++ hr_attendance_autoclose/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'HR Attendance Auto Close',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Human Resources',
     'summary': 'Close stale Attendances',
     'website': 'https://github.com/OCA/hr',
@@ -12,7 +11,6 @@
     'license': 'AGPL-3',
     'installable': True,
     'depends': [
-        'hr_attendance',
         'hr_attendance_reason',
     ],
     'data': [
