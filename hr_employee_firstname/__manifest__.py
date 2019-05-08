451ca4025a0bd0b72c9648dbf1e177360cfa08c8
Andrea <a.stirpe@onestein.nl>
2019-05-08 09:32:09 +0200

    [FIX/IMP] Make required attributes dynamic

--- hr_employee_firstname/__manifest__.py
+++ hr_employee_firstname/__manifest__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 # ©  2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'HR Employee First Name, Last Name',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': "Savoir-faire Linux, "
               "Fekete Mihai (Forest and Biomass Services Romania), "
+              "Onestein, "
               "Odoo Community Association (OCA)",
     'maintainer': 'Savoir-faire Linux',
-    'website': 'http://www.savoirfairelinux.com',
+    'website': 'https://github.com/OCA/hr/',
     'license': 'AGPL-3',
     'category': 'Human Resources',
     'summary': 'Adds First Name to Employee',
