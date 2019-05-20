f991cb19c26cee0d9785a6d1e4de9f3c8925c903
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-05-20 13:05:21 +0200

    [MIG][11.0] hr_holidays_validity_date

--- hr_holidays_validity_date/tests/test_hr_holidays_validity_date.py
+++ hr_holidays_validity_date/tests/test_hr_holidays_validity_date.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #     This file is part of hr_holidays_validity_date,
@@ -24,10 +23,9 @@
 ##############################################################################
 
 from odoo.tests import common
-from odoo import exceptions
+from odoo.exceptions import ValidationError
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from datetime import datetime, timedelta
-from odoo.tools import mute_logger
 
 
 class TestHrHolidaysValidityDate(common.TransactionCase):
@@ -38,13 +36,17 @@ class TestHrHolidaysValidityDate(common.TransactionCase):
     def setUp(self):
         super(TestHrHolidaysValidityDate, self).setUp()
         self.holidays_obj = self.env['hr.holidays']
-        self.type01 = self.env.ref('hr_holidays.holiday_status_cl')
+        self.type01 = self.env['hr.holidays.status'].create({
+            'name': 'Status',
+            'limit': True
+        })
         self.employee01 = self.env.ref('hr.employee_mit')
 
     def test_holidays_without_validity(self):
         today = datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        tommorow = (datetime.now() + timedelta(days=1))\
-            .strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        tomorrow = (datetime.now() + timedelta(days=1)).strftime(
+            DEFAULT_SERVER_DATETIME_FORMAT
+        )
         allocation_vals = {
             'name': 'Test',
             'type': 'add',
@@ -58,18 +60,25 @@ class TestHrHolidaysValidityDate(common.TransactionCase):
             'name': 'test',
             'holiday_status_id': self.type01.id,
             'date_from': today,
-            'date_to': tommorow,
+            'date_to': tomorrow,
             'number_of_days_temp': 1,
         }
         self.holidays_obj.create(leave_vals)
 
     def test_holidays_validity(self):
-        yesterday = (datetime.now() - timedelta(days=1))\
-            .strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        today = datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        tommorow = (datetime.now() + timedelta(days=1))\
-            .strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        now = datetime.now()
+        yesterday = (now - timedelta(days=1)).strftime(
+            DEFAULT_SERVER_DATETIME_FORMAT
+        )
+        today = now.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        tomorrow = (now + timedelta(days=1)).strftime(
+            DEFAULT_SERVER_DATETIME_FORMAT
+        )
+        day_after_tomorrow = (now + timedelta(days=2)).strftime(
+            DEFAULT_SERVER_DATETIME_FORMAT
+        )
         self.type01.use_validity_dates = True
+        self.type01.restrict_dates = True
         self.type01.date_start = yesterday
         self.type01.date_end = today
         allocation_vals = {
@@ -79,16 +88,27 @@ class TestHrHolidaysValidityDate(common.TransactionCase):
             'holiday_status_id': self.type01.id,
             'number_of_days_temp': 10,
         }
-        self.holidays_obj.create(allocation_vals)
+        allocation = self.holidays_obj.create(allocation_vals)
+        allocation.action_validate()
         leave_vals = {
             'employee_id': self.employee01.id,
             'holiday_status_id': self.type01.id,
             'name': 'test',
             'date_from': today,
-            'date_to': tommorow,
+            'date_to': tomorrow,
             'number_of_days_temp': 2,
         }
-        with mute_logger('odoo.models'):
-            with self.assertRaises(exceptions.ValidationError),\
-                    self.cr.savepoint():
-                self.holidays_obj.create(leave_vals)
+        with self.assertRaises(ValidationError):
+            self.holidays_obj.create(leave_vals)
+
+        self.type01.restrict_dates = False
+        leave_vals = {
+            'employee_id': self.employee01.id,
+            'holiday_status_id': self.type01.id,
+            'name': 'test',
+            'date_from': day_after_tomorrow,
+            'date_to': day_after_tomorrow,
+            'number_of_days_temp': 1,
+        }
+        holidays = self.holidays_obj.create(leave_vals)
+        self.assertIn('Warning', holidays.warning)
