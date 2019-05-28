8ffb7025ab6704eaab6d70d4eb504e7ac4c43a2e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-05-28 09:50:43 +0200

    [12.0][MIG] hr_attendance_reason

--- hr_attendance_reason/__manifest__.py
+++ hr_attendance_reason/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'HR Attendance Reason',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Human Resources',
     'website': 'https://github.com/OCA/hr',
     'author': 'Odoo S.A.,'
