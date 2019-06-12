fbbd2e415930ed83a78120ec47ef2811f094132e
ps-tubtim <pimolnat.s@outlook.com>
2019-06-12 16:05:48 +0700

    [MIG] hr_expense_sequence: Migration to 12.0

--- hr_expense_sequence/__manifest__.py
+++ hr_expense_sequence/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+# Copyright 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'HR expense sequence',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Human Resources',
-    'author': "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
-              "Odoo Community Association (OCA)",
-    'website': 'http://www.serviciosbaeza.com',
+    'author': 'Serv. Tecnol. Avanzados - Pedro M. Baeza,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/hr',
     'depends': [
         'hr_expense',
     ],
@@ -20,5 +19,5 @@
         'report/report_expense_sheet.xml',
     ],
     'installable': True,
-    "post_init_hook": "assign_old_sequences",
+    'post_init_hook': 'assign_old_sequences',
 }
