01d8e48903dd871ebb158d0a7137e7dd871c3e15
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-16 20:11:25 +0200

    [FIX] hr_holidays_compute_days: Fix days saving in assignations for non holidays manager
    Due to the trick of using related fields for showing readonly info, there's a glitch
    saving the number of days on assignations, where all the employees should be able of
    putting number of days.
    
    We fix it showing the original field on assignations.

--- hr_holidays_compute_days/__manifest__.py
+++ hr_holidays_compute_days/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 iDT LABS (http://www.@idtlabs.sl)
-# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Employee Compute Leave Days',
-    'version': '10.0.3.0.0',
+    'version': '10.0.3.1.0',
     'category': 'Human Resources',
     'license': 'AGPL-3',
     'summary': 'Computes the actual leave days '
