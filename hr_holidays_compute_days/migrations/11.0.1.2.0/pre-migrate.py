966063cfa369f3021bbe72702f428f17f49ebe01
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-22 11:47:50 +0200

    [FIX] hr_holidays_compute_days: Be able to update existing modules
    If not, an error raised due to the way updates are performed.

---
+++ hr_holidays_compute_days/migrations/11.0.1.2.0/pre-migrate.py
@@ -0,0 +1,11 @@
+# Copyright 2019 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrat()
+def migrate(env, version):
+    openupgrade.delete_records_safely_by_xml_id(
+        env, ['hr_holidays_compute_days.edit_holiday_new_manager'],
+    )
