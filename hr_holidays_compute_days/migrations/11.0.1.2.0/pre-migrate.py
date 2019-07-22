0a4e73f9f61c4e0256fbfbceb56710f75b8e1518
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-22 11:51:44 +0200

    [FIX] hr_holidays_compute_days: Oversight of previous commit

--- hr_holidays_compute_days/migrations/11.0.1.2.0/pre-migrate.py
+++ hr_holidays_compute_days/migrations/11.0.1.2.0/pre-migrate.py
@@ -4,7 +4,7 @@
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrat()
+@openupgrade.migrate()
 def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(
         env, ['hr_holidays_compute_days.edit_holiday_new_manager'],
