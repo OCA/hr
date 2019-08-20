4d592fdc01b247c54a5684fc0dc45cc5523ab440
hveficent <hector.villarreal@eficent.com>
2019-08-20 16:22:11 +0200

    [12.0][FIX] hr_holidays_notify_employee_manager: Add readonly False on leave_notify_manager option

--- hr_holidays_notify_employee_manager/models/res_config_settings.py
+++ hr_holidays_notify_employee_manager/models/res_config_settings.py
@@ -8,7 +8,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     leave_notify_manager = fields.Boolean(
-        related='company_id.leave_notify_manager',
+        related='company_id.leave_notify_manager', readonly=False,
         string="Leave Requests notified to employee's manager",
         help="When a leave request is created the employee's manager "
              "will be added as follower and notified by email.")
