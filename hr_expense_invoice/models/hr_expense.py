b6390a431264802cc7d655a853b276781c589ba7
kongrattapong <rattapong.chok@gmail.com>
2019-10-17 15:23:57 +0700

    [IMP] hr_expense_invoice: black, isort

--- hr_expense_invoice/models/hr_expense.py
+++ hr_expense_invoice/models/hr_expense.py
@@ -6,16 +6,15 @@ from odoo import api, fields, models
 
 
 class HrExpense(models.Model):
-    _inherit = 'hr.expense'
+    _inherit = "hr.expense"
 
     invoice_id = fields.Many2one(
-        comodel_name='account.invoice',
-        string='Vendor Bill',
+        comodel_name="account.invoice",
+        string="Vendor Bill",
         domain="[('type', '=', 'in_invoice'), ('state', '=', 'open')]",
-        oldname='invoice',
     )
 
-    @api.onchange('invoice_id')
+    @api.onchange("invoice_id")
     def onchange_invoice_id(self):
         self.date = self.invoice_id.date_invoice
         self.name = self.invoice_id.number
@@ -27,26 +26,24 @@ class HrExpense(models.Model):
         self.description = self.invoice_id.reference
 
     def _check_vals(self, vals):
-        if vals.get('invoice_id'):
+        if vals.get("invoice_id"):
             # Rewrite values because readonly fields are not stored
-            invoice = self.env['account.invoice'].browse(vals['invoice_id'])
-            vals['date'] = invoice.date_invoice
-            vals['analytic_account_id'] = False
-            vals['unit_amount'] = invoice.residual
-            vals['total_amount'] = invoice.residual
-            vals['quantity'] = 1.0
+            invoice = self.env["account.invoice"].browse(vals["invoice_id"])
+            vals["date"] = invoice.date_invoice
+            vals["analytic_account_id"] = False
+            vals["unit_amount"] = invoice.residual
+            vals["total_amount"] = invoice.residual
+            vals["quantity"] = 1.0
 
     @api.model
     def create(self, vals):
         self._check_vals(vals)
         return super(HrExpense, self).create(vals)
 
-    @api.multi
     def write(self, vals):
         self._check_vals(vals)
         return super(HrExpense, self).write(vals)
 
-    @api.multi
     def _get_account_move_line_values(self):
         move_line_values_by_expense = super()._get_account_move_line_values()
         for expense_id, move_lines in move_line_values_by_expense.items():
@@ -54,8 +51,9 @@ class HrExpense(models.Model):
             if not expense.invoice_id:
                 return move_line_values_by_expense
             for move_line in move_lines:
-                if move_line['debit']:
-                    move_line['partner_id'] = \
-                        expense.invoice_id.partner_id.commercial_partner_id.id
-                    move_line['account_id'] = expense.invoice_id.account_id.id
+                if move_line["debit"]:
+                    move_line[
+                        "partner_id"
+                    ] = expense.invoice_id.partner_id.commercial_partner_id.id
+                    move_line["account_id"] = expense.invoice_id.account_id.id
         return move_line_values_by_expense
