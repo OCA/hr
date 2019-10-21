fd9fd8553b8c5d367dbfb1312070606293eff24d
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-10-21 10:01:51 +0200

    [FIX] hr_expense_cancel: Avoid singleton error in post_init_hook

--- hr_expense_cancel/hooks.py
+++ hr_expense_cancel/hooks.py
@@ -14,7 +14,7 @@ def post_init_hook(cr, registry):
             if any(exp.payment_mode == 'own_account' for exp in expense_lines):
                 amls = sheet.account_move_id.mapped('line_ids')
                 reconcile = amls.mapped('full_reconcile_id')
-                aml_payment = reconcile.reconciled_line_ids.filtered(
+                aml_payment = reconcile.mapped('reconciled_line_ids').filtered(
                     lambda r: r not in amls)
                 payment = aml_payment.mapped('payment_id')
                 payment.write({'expense_sheet_id': sheet.id})
