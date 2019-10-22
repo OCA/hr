8ba2f261f11a30dc1c270dde729ed63958c8c73a
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-10-22 13:37:12 +0200

    [12.0][MIG] hr_calendar_rest_time

--- resource_hook/hooks.py
+++ resource_hook/hooks.py
@@ -1,6 +1,8 @@
-from odoo.addons.resource.models.resource_mixin import ResourceMixin
+from odoo.addons.resource.models.resource_mixin import ResourceMixin, ROUNDING_FACTOR
 from datetime import timedelta
 from odoo.tools import float_utils
+from pytz import utc
+from collections import defaultdict
 
 
 def post_load_hook():
@@ -8,31 +10,63 @@ def post_load_hook():
         ResourceMixin.get_work_days_data_original = \
             ResourceMixin.get_work_days_data
 
-    def __new_get_work_days_data(self, from_datetime, to_datetime,
-                                 calendar=None):
-        if not hasattr(self, '_get_work_hours'):
-            return self.get_day_work_hours_count_original(
-                from_datetime, to_datetime, calendar=calendar)
-        days_count = 0.0
-        total_work_time = timedelta()
+    def __new_get_work_days_data(
+            self, from_datetime, to_datetime, compute_leaves=True,
+            calendar=None, domain=None
+    ):
+        """
+            By default the resource calendar is used, but it can be
+            changed using the `calendar` argument.
+
+            `domain` is used in order to recognise the leaves to take,
+            None means default value ('time_type', '=', 'leave')
+
+            Returns a dict {'days': n, 'hours': h} containing the
+            quantity of working time expressed as days and as hours.
+        """
+        resource = self.resource_id
         calendar = calendar or self.resource_calendar_id
-        calendar = calendar.with_context(
-            no_tz_convert=self.env.context.get('no_tz_convert', False))
-        for day_intervals in calendar._iter_work_intervals(
-                from_datetime, to_datetime, self.resource_id.id,
-                compute_leaves=True):
-            theoric_hours = self.get_day_work_hours_count(
-                day_intervals[0][0].date(), calendar=calendar)
-            # Here we introduce the hook method '_get_work_hours'.
-            work_time = sum((self._get_work_hours(interval)
-                             for interval in day_intervals), timedelta())
-            total_work_time += work_time
-            if theoric_hours:
-                days_count += float_utils.round(
-                    (work_time.total_seconds() / 3600 / theoric_hours) * 4) / 4
+
+        # naive datetimes are made explicit in UTC
+        if not from_datetime.tzinfo:
+            from_datetime = from_datetime.replace(tzinfo=utc)
+        if not to_datetime.tzinfo:
+            to_datetime = to_datetime.replace(tzinfo=utc)
+
+        # total hours per day: retrieve attendances with one extra day margin,
+        # in order to compute the total hours on the first and last days
+        from_full = from_datetime - timedelta(days=1)
+        to_full = to_datetime + timedelta(days=1)
+        intervals = calendar._attendance_intervals(
+            from_full, to_full, resource
+        )
+        day_total = defaultdict(float)
+        for start, stop, meta in intervals:
+            day_total[start.date()] += (stop - start).total_seconds() / 3600
+
+        # actual hours per day
+        if compute_leaves:
+            intervals = calendar._work_intervals(
+                from_datetime, to_datetime, resource, domain
+            )
+        else:
+            intervals = calendar._attendance_intervals(
+                from_datetime, to_datetime, resource
+            )
+        day_hours = defaultdict(float)
+        for start, stop, meta in intervals:
+            day_hours[start.date()] += self._get_work_hours(start, stop, meta)
+
+        # compute number of days as quarters
+        days = sum(
+            float_utils.round(
+                ROUNDING_FACTOR * day_hours[day] / day_total[day]
+            ) / ROUNDING_FACTOR
+            for day in day_hours
+        )
         return {
-            'days': days_count,
-            'hours': total_work_time.total_seconds() / 3600,
+            'days': days,
+            'hours': sum(day_hours.values()),
         }
 
     ResourceMixin.get_work_days_data = __new_get_work_days_data
--- resource_hook/models/resource_mixin.py
+++ resource_hook/models/resource_mixin.py
@@ -7,11 +7,11 @@ from odoo import models
 class ResourceMixin(models.AbstractModel):
     _inherit = 'resource.mixin'
 
-    def _get_work_hours(self, interval):
+    def _get_work_hours(self, start, stop, meta):
         """
         This method now returns the hours between the two ends of the
         interval. Extend this method if you want to alter the logic.
         :param interval:
         :return: float representing the time worked.
         """
-        return interval[1] - interval[0]
+        return (stop - start).total_seconds() / 3600
