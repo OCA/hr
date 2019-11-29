5dbbcff7ae9d6e1380897141e6c4da8871fce4e2
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-11-29 09:03:10 +0100

    [11.0][FIX] hr_employee_medical_examination: placeholder

--- hr_employee_medical_examination/__manifest__.py
+++ hr_employee_medical_examination/__manifest__.py
@@ -8,6 +8,7 @@
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'maintainers': ['jarroyomorales'],
     'website': 'https://github.com/OCA/hr',
     'depends': [
         'hr',
