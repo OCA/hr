394cec91795f1f5acb138cbfff2fc1fefba1d60e
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-09 10:49:12 +0100

    [IMP] hr_attendance_rfid: black, isort

--- hr_attendance_rfid/tests/test_hr_attendance_rfid_process.py
+++ hr_attendance_rfid/tests/test_hr_attendance_rfid_process.py
@@ -8,48 +8,47 @@ from odoo.tools.misc import mute_logger
 
 
 class TestHrAttendance(TransactionCase):
-
     def setUp(self):
         super(TestHrAttendance, self).setUp()
-        self.employee_model = self.env['hr.employee']
-        self.test_employee = self.browse_ref('hr.employee_al')
-        self.rfid_card_code = '5b3f5'
+        self.employee_model = self.env["hr.employee"]
+        self.test_employee = self.browse_ref("hr.employee_al")
+        self.rfid_card_code = "5b3f5"
         self.test_employee.rfid_card_code = self.rfid_card_code
 
     def test_valid_employee(self):
         """Valid employee"""
-        res = self.employee_model.register_attendance(
-            self.rfid_card_code)
-        self.assertTrue('action' in res and res['action'] == 'check_in')
-        self.assertTrue('logged' in res and res['logged'])
+        res = self.employee_model.register_attendance(self.rfid_card_code)
+        self.assertTrue("action" in res and res["action"] == "check_in")
+        self.assertTrue("logged" in res and res["logged"])
         self.assertTrue(
-            'rfid_card_code' in res and
-            res['rfid_card_code'] == self.rfid_card_code)
-        res = self.employee_model.register_attendance(
-            self.rfid_card_code)
-        self.assertTrue('action' in res and res['action'] == 'check_out')
-        self.assertTrue('logged' in res and res['logged'])
+            "rfid_card_code" in res and res["rfid_card_code"] == self.rfid_card_code
+        )
+        res = self.employee_model.register_attendance(self.rfid_card_code)
+        self.assertTrue("action" in res and res["action"] == "check_out")
+        self.assertTrue("logged" in res and res["logged"])
 
-    @mute_logger('odoo.addons.hr_attendance_rfid.models.hr_employee')
+    @mute_logger("odoo.addons.hr_attendance_rfid.models.hr_employee")
     def test_exception_code(self):
         """Checkout is created for a future datetime"""
-        self.env['hr.attendance'].create({
-            'employee_id': self.test_employee.id,
-            'check_in': fields.Date.today(),
-            'check_out': fields.Datetime.to_string(
-                datetime.today()+timedelta(hours=8))
-        })
-        self.test_employee.update({'attendance_state': 'checked_in'})
-        res = self.employee_model.register_attendance(
-            self.rfid_card_code)
-        self.assertNotEquals(res['error_message'], '')
+        self.env["hr.attendance"].create(
+            {
+                "employee_id": self.test_employee.id,
+                "check_in": fields.Date.today(),
+                "check_out": fields.Datetime.to_string(
+                    datetime.today() + timedelta(hours=8)
+                ),
+            }
+        )
+        self.test_employee.update({"attendance_state": "checked_in"})
+        res = self.employee_model.register_attendance(self.rfid_card_code)
+        self.assertNotEquals(res["error_message"], "")
 
     def test_invalid_code(self):
         """Invalid employee"""
-        invalid_code = '029238d'
+        invalid_code = "029238d"
         res = self.employee_model.register_attendance(invalid_code)
-        self.assertTrue('action' in res and res['action'] == 'FALSE')
-        self.assertTrue('logged' in res and not res['logged'])
+        self.assertTrue("action" in res and res["action"] == "FALSE")
+        self.assertTrue("logged" in res and not res["logged"])
         self.assertTrue(
-            'rfid_card_code' in res and
-            res['rfid_card_code'] == invalid_code)
+            "rfid_card_code" in res and res["rfid_card_code"] == invalid_code
+        )
