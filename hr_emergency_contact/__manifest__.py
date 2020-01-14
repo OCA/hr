c2b2a849ca07cb96623c03eba55798ecbaacd0a9
robin.keunen <robin@keunen.net>
2020-01-14 15:30:16 +0100

    [REF] hr_emergency_contact: blacken

--- hr_emergency_contact/__manifest__.py
+++ hr_emergency_contact/__manifest__.py
@@ -3,18 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'HR Emergency Contact',
-    'version': '11.0.1.0.0',
-    'category': 'Generic Modules/Human Resources',
-    'author': "OpenSynergy Indonesia,Michael Telahun Makonnen,"
-              "Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/hr",
-    'license': 'AGPL-3',
-    'depends': [
-        'hr',
-    ],
-    'data': [
-        'views/hr_employee_views.xml',
-    ],
-    'installable': True,
+    "name": "HR Emergency Contact",
+    "version": "11.0.1.0.0",
+    "category": "Generic Modules/Human Resources",
+    "author": "OpenSynergy Indonesia,Michael Telahun Makonnen,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "license": "AGPL-3",
+    "depends": ["hr"],
+    "data": ["views/hr_employee_views.xml"],
+    "installable": True,
 }
--- hr_emergency_contact/models/hr_employee.py
+++ hr_emergency_contact/models/hr_employee.py
@@ -7,13 +7,11 @@ from odoo import fields, models
 
 class HrEmployee(models.Model):
 
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     emergency_contact_ids = fields.Many2many(
-        comodel_name='res.partner',
-        string='Emergency Contacts',
-        relation='rel_employee_emergency_contact',
-        domain=[
-            ('is_company', '=', False),
-        ]
+        comodel_name="res.partner",
+        string="Emergency Contacts",
+        relation="rel_employee_emergency_contact",
+        domain=[("is_company", "=", False)],
     )
