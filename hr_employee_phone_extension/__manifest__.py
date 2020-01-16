fdf48ce954352157fb30aee749e350efff386518
Saran440 <saranl@ecosoft.co.th>
2020-01-16 11:34:28 +0700

    [IMP] hr_employee_phone_extension: black, isort

--- hr_employee_phone_extension/__manifest__.py
+++ hr_employee_phone_extension/__manifest__.py
@@ -2,15 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Employee Phone Extension',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
-    'website': "https://github.com/oca/hr",
-    'license': 'AGPL-3',
-    'depends': ['hr'],
-    'data': [
-        'views/hr_employee_view.xml',
-    ],
-    'installable': True,
+    "name": "Employee Phone Extension",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/hr",
+    "license": "AGPL-3",
+    "depends": ["hr"],
+    "data": ["views/hr_employee_view.xml"],
+    "installable": True,
 }
