63ec79bcdda39ac9e9d6a7456a5d723321d7c178
Saran440 <saranl@ecosoft.co.th>
2020-01-16 11:34:28 +0700

    [IMP] hr_employee_phone_extension: black, isort

--- hr_employee_phone_extension/models/hr_employee.py
+++ hr_employee_phone_extension/models/hr_employee.py
@@ -7,9 +7,7 @@ from odoo import fields, models
 class HrEmployee(models.Model):
     """Enhance the features of the employee using Number details."""
 
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
-    internal_number = fields.Char('Internal Number',
-                                  help='Internal phone number.')
-    short_number = fields.Char('Short Number',
-                               help='Short phone number.')
+    internal_number = fields.Char("Internal Number", help="Internal phone number.")
+    short_number = fields.Char("Short Number", help="Short phone number.")
