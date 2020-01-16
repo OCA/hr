48f93ff591e64ef2c6f7de51012a9e058b026674
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-01-16 16:53:07 +0100

    [REF] hr_employee_age: blacken

--- hr_employee_age/models/hr_employee.py
+++ hr_employee_age/models/hr_employee.py
@@ -6,21 +6,18 @@ from odoo import api, fields, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
-    age = fields.Integer(
-        string='Age',
-        readonly=True,
-        compute='_compute_age',
-    )
+    age = fields.Integer(string="Age", readonly=True, compute="_compute_age")
 
     @api.multi
-    @api.depends('birthday')
+    @api.depends("birthday")
     def _compute_age(self):
         for record in self:
             age = 0
             if record.birthday:
                 age = relativedelta(
                     fields.Date.from_string(fields.Date.today()),
-                    fields.Date.from_string(record.birthday)).years
+                    fields.Date.from_string(record.birthday),
+                ).years
             record.age = age
--- hr_employee_age/tests/test_hr_employee.py
+++ hr_employee_age/tests/test_hr_employee.py
@@ -8,14 +8,13 @@ from odoo.tests import common
 class TestHrEmployee(common.TransactionCase):
     def setUp(self):
         super(TestHrEmployee, self).setUp()
-        self.emp_root = self.env.ref('hr.employee_root')
-        self.emp_root.write({
-            'birthday': '1990-05-15'
-        })
+        self.emp_root = self.env.ref("hr.employee_root")
+        self.emp_root.write({"birthday": "1990-05-15"})
 
     def test_compute_age(self):
         self.emp_root._compute_age()
         age = relativedelta(
             fields.Date.from_string(fields.Date.today()),
-            fields.Date.from_string(self.emp_root.birthday)).years
+            fields.Date.from_string(self.emp_root.birthday),
+        ).years
         self.assertEqual(self.emp_root.age, age)
