f71166e868aa5277388ca727e52125b719761786
Saran440 <saranl@ecosoft.co.th>
2020-01-27 17:38:48 +0700

    [12.0][MIG] hr_employee_language

--- hr_language/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017-Today: Odoo Community Association (OCA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import hr_language
-from . import hr_employee
