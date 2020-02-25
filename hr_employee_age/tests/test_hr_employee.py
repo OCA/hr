c131864d87bb8623519fafbc885f2027f2d28c5e
Saran440 <saranl@ecosoft.co.th>
2020-02-25 10:08:17 +0700

    [IMP] hr_employee_age: black, isort

--- hr_employee_age/tests/test_hr_employee.py
+++ hr_employee_age/tests/test_hr_employee.py
@@ -1,6 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from dateutil.relativedelta import relativedelta
+
 from odoo import fields
 from odoo.tests import common
 
@@ -13,8 +14,5 @@ class TestHrEmployee(common.TransactionCase):
 
     def test_compute_age(self):
         self.employee_admin._compute_age()
-        age = relativedelta(
-            fields.Date.today(),
-            self.employee_admin.birthday,
-        ).years
+        age = relativedelta(fields.Date.today(), self.employee_admin.birthday).years
         self.assertEqual(self.employee_admin.age, age)
