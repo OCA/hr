ea062e9e160228889a699d5e900db6e39bbe0cec
robin.keunen <robin@keunen.net>
2020-03-09 14:35:31 +0100

    [12.0][FIX] hr_employee_document: google_drive_link invisible
    Replacing google drive link in view by nothing, that is deleting,
    breaks the view inheritance.
    Making it invisible instead.
    
    hr_employee_document 12.0.1.0.1

--- hr_employee_document/__manifest__.py
+++ hr_employee_document/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'HR Employee Document',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Human Resources',
     'website': 'https://github.com/OCA/hr',
     'author':
