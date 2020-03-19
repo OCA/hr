bd616c8e9bd45b07ff9d6473d2a8c16c52293f67
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-19 14:55:37 +0100

    [12.0][FIX] fix hr_calendar_rest_time

--- resource_hook/hooks.py
+++ resource_hook/hooks.py
@@ -1,4 +1,5 @@
 from odoo.addons.resource.models.resource_mixin import ResourceMixin, ROUNDING_FACTOR
+from odoo.addons.resource.models.resource import ResourceCalendar
 from datetime import timedelta
 from odoo.tools import float_utils
 from pytz import utc
@@ -9,6 +10,9 @@ def post_load_hook():
     if not hasattr(ResourceMixin, 'get_work_days_data_original'):
         ResourceMixin.get_work_days_data_original = \
             ResourceMixin.get_work_days_data
+    if not hasattr(ResourceCalendar, 'get_work_hours_count_original'):
+        ResourceCalendar.get_work_hours_count_original = \
+            ResourceCalendar.get_work_hours_count
 
     def __new_get_work_days_data(
             self, from_datetime, to_datetime, compute_leaves=True,
@@ -69,4 +73,33 @@ def post_load_hook():
             'hours': sum(day_hours.values()),
         }
 
+    def __new_get_work_hours_count(
+        self, start_dt, end_dt, compute_leaves=True, domain=None
+    ):
+        """
+            `compute_leaves` controls whether or not this method is taking into
+            account the global leaves.
+
+            `domain` controls the way leaves are recognized.
+            None means default value ('time_type', '=', 'leave')
+
+            Counts the number of work hours between two datetimes.
+        """
+        # Set timezone in UTC if no timezone is explicitly given
+        if not start_dt.tzinfo:
+            start_dt = start_dt.replace(tzinfo=utc)
+        if not end_dt.tzinfo:
+            end_dt = end_dt.replace(tzinfo=utc)
+
+        if compute_leaves:
+            intervals = self._work_intervals(start_dt, end_dt, domain=domain)
+        else:
+            intervals = self._attendance_intervals(start_dt, end_dt)
+
+        return sum(
+            self._get_work_hours(start, stop, meta)
+            for start, stop, meta in intervals
+        )
+
     ResourceMixin.get_work_days_data = __new_get_work_days_data
+    ResourceCalendar.get_work_hours_count = __new_get_work_hours_count
