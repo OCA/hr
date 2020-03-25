ce6c11db2486ecb3100470c01771744c81a2af52
mreficent <miquel.raich@eficent.com>
2020-03-25 11:51:07 +0100

    [IMP] hr_attendance_geolocation: black, isort

--- hr_attendance_geolocation/__manifest__.py
+++ hr_attendance_geolocation/__manifest__.py
@@ -2,22 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hr Attendance Geolocation',
-    'summary': """
+    "name": "Hr Attendance Geolocation",
+    "summary": """
         With this module the geolocation of the user is tracked at the
         check-in/check-out step""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Eficent Business and IT Consulting Services S.L.,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'decimal_precision',
-        'hr_attendance',
-    ],
-    'data': [
-        'views/assets.xml',
-        'views/hr_attendance_views.xml',
-        'data/location_data.xml',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Eficent Business and IT Consulting Services S.L.,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["decimal_precision", "hr_attendance"],
+    "data": [
+        "views/assets.xml",
+        "views/hr_attendance_views.xml",
+        "data/location_data.xml",
     ],
 }
