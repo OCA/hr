03e6376a79328466f75951aa5b470c7d1b897d9e
Saran440 <saranl@ecosoft.co.th>
2020-03-30 15:43:39 +0700

    [IMP] hr_employee_service: black, isort

--- hr_employee_service/tests/test_hr_employee_service.py
+++ hr_employee_service/tests/test_hr_employee_service.py
@@ -1,52 +1,46 @@
 # Copyright (C) 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+from mock import patch
+
 from odoo import fields
 from odoo.tests import common
 
-from dateutil.relativedelta import relativedelta
-from datetime import date
-from mock import patch
-
 
 class TestHrEmployeeService(common.TransactionCase):
-
     def setUp(self):
         super().setUp()
 
         self.today = fields.Date.today()
         self.now = fields.Datetime.now()
-        self.Employee = self.env['hr.employee']
+        self.Employee = self.env["hr.employee"]
         self.SudoEmployee = self.Employee.sudo()
 
     def test_1(self):
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #1',
-            'service_hire_date': (
-                self.today - relativedelta(years=3)
-            ),
-            'service_start_date': (
-                self.today - relativedelta(years=3)
-            ),
-            'service_termination_date': (
-                self.today - relativedelta(years=1)
-            ),
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #1",
+                "service_hire_date": (self.today - relativedelta(years=3)),
+                "service_start_date": (self.today - relativedelta(years=3)),
+                "service_termination_date": (self.today - relativedelta(years=1)),
+            }
+        )
 
         self.assertEqual(employee.service_duration_years, 2)
         self.assertEqual(employee.service_duration_months, 0)
         self.assertEqual(employee.service_duration_days, 0)
 
     def test_2(self):
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #2',
-            'service_hire_date': (
-                self.today - relativedelta(years=3)
-            ),
-            'service_start_date': (
-                self.today - relativedelta(years=3)
-            ),
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #2",
+                "service_hire_date": (self.today - relativedelta(years=3)),
+                "service_start_date": (self.today - relativedelta(years=3)),
+            }
+        )
 
         self.assertEqual(employee.service_duration_years, 3)
         self.assertEqual(employee.service_duration_months, 0)
@@ -54,54 +48,52 @@ class TestHrEmployeeService(common.TransactionCase):
 
     def test_3(self):
         mocked_today = date(2019, 8, 27)
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #3',
-            'service_hire_date': (
-                mocked_today - relativedelta(months=6)
-            ),
-            'service_start_date': (
-                mocked_today - relativedelta(months=6)
-            ),
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #3",
+                "service_hire_date": (mocked_today - relativedelta(months=6)),
+                "service_start_date": (mocked_today - relativedelta(months=6)),
+            }
+        )
 
-        with patch('odoo.fields.Date.today') as today:
+        with patch("odoo.fields.Date.today") as today:
             today.return_value = mocked_today
             self.assertEqual(employee.service_duration_years, 0)
             self.assertEqual(employee.service_duration_months, 6)
             self.assertEqual(employee.service_duration_days, 0)
 
     def test_4(self):
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #4',
-            'service_hire_date': (
-                self.today - relativedelta(months=6)
-            ),
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #4",
+                "service_hire_date": (self.today - relativedelta(months=6)),
+            }
+        )
 
         self.assertEqual(employee.service_duration_years, 0)
         self.assertEqual(employee.service_duration_months, 0)
         self.assertEqual(employee.service_duration_days, 0)
 
     def test_5(self):
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #5',
-            'service_hire_date': (
-                self.today - relativedelta(days=1)
-            ),
-            'service_start_date': (
-                self.today - relativedelta(days=1)
-            ),
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #5",
+                "service_hire_date": (self.today - relativedelta(days=1)),
+                "service_start_date": (self.today - relativedelta(days=1)),
+            }
+        )
 
         self.assertEqual(employee.service_duration, 1)
         self.assertEqual(employee.service_duration_days, 1)
 
     def test_6(self):
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #6',
-            'service_hire_date': self.today,
-            'service_start_date': self.today,
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #6",
+                "service_hire_date": self.today,
+                "service_start_date": self.today,
+            }
+        )
 
         employee.service_hire_date = None
         employee.service_start_date = None
@@ -111,12 +103,12 @@ class TestHrEmployeeService(common.TransactionCase):
         self.assertEqual(employee.service_duration_days, 0)
 
     def test_7(self):
-        employee = self.SudoEmployee.create({
-            'name': 'Employee #7',
-            'service_hire_date': (
-                self.today - relativedelta(days=1)
-            ),
-        })
+        employee = self.SudoEmployee.create(
+            {
+                "name": "Employee #7",
+                "service_hire_date": (self.today - relativedelta(days=1)),
+            }
+        )
 
         employee._onchange_service_hire_date()
 
