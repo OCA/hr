79be4fdc124742dff3e61722c94fbddb26a856b1
Saran440 <saranl@ecosoft.co.th>
2020-03-30 15:57:30 +0700

    [IMP] hr_employee_relative: black, isort

--- hr_employee_relative/models/hr_employee.py
+++ hr_employee_relative/models/hr_employee.py
@@ -5,10 +5,10 @@ from odoo import fields, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     relative_ids = fields.One2many(
-        string='Relatives',
-        comodel_name='hr.employee.relative',
-        inverse_name='employee_id',
+        string="Relatives",
+        comodel_name="hr.employee.relative",
+        inverse_name="employee_id",
     )
