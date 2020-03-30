197dfb260dab00ddb3d0fc45815a7a455d53abe9
Saran440 <saranl@ecosoft.co.th>
2020-03-30 15:43:39 +0700

    [IMP] hr_employee_service: black, isort

--- hr_employee_service/__manifest__.py
+++ hr_employee_service/__manifest__.py
@@ -2,26 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'HR Employee Service',
-    'version': '12.0.1.0.1',
-    'category': 'Human Resources',
-    'website': 'https://github.com/OCA/hr',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Employee service information & duration',
-    'depends': [
-        'hr',
-    ],
-    'external_dependencies': {
-        'python': [
-            'dateutil',
-        ],
-    },
-    'data': [
-        'views/hr_employee.xml',
-    ],
+    "name": "HR Employee Service",
+    "version": "12.0.1.0.1",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Employee service information & duration",
+    "depends": ["hr"],
+    "external_dependencies": {"python": ["dateutil"]},
+    "data": ["views/hr_employee.xml"],
 }
