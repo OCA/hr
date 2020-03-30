78037e796fcf27e6bcac74da5448399c6f89b532
Saran440 <saranl@ecosoft.co.th>
2020-03-30 15:59:45 +0700

    [13.0][MIG] hr_employee_relative

--- hr_employee_relative/__manifest__.py
+++ hr_employee_relative/__manifest__.py
@@ -3,10 +3,10 @@
 
 {
     "name": "HR Employee Relatives",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
-    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "author": "Brainbean Apps, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "application": False,
