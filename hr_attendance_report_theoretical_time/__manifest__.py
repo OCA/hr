94683d0c0c85809df7c1b9d745d7e6855eac4e48
Yoshi Tashiro <tashiro@quartile.co>
2020-04-07 16:45:18 +0800

    [10.0][FIX] hr_attendance_report_theoretical_time
    To fix the issue of menu path 'Attendaces > Reports' not being visible
    to 'Manual Attendance' group.

--- hr_attendance_report_theoretical_time/__manifest__.py
+++ hr_attendance_report_theoretical_time/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Theoretical vs Attended Time Analysis",
-    "version": "10.0.2.0.0",
+    "version": "10.0.2.0.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "Tecnativa, "
