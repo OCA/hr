eb457b45fb0145651b583d4a7f75e25be4128730
Letizia Freda <letizia.freda@netfarm.it>
2020-04-22 22:41:37 +0200

    Added hr_attendance_module

---
+++ hr_attendance_quagga/models/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from odoo import models, fields, api, _
+
+
+class Employee(models.Model):
+    _inherit = 'hr.employee'
+
+    @api.model
+    def attendance_scan(self, barcode):
+        """ Receive a barcode scanned from the Kiosk Mode and change the
+            attendances of corresponding employee.
+            Returns either an action or a warning.
+        """
+        employee = self.search([('barcode', '=', barcode)], limit=1)
+
+        # prevent duplicate
+        date_test = datetime.now() - timedelta(seconds=60)
+
+        attendances = self.env['hr.attendance'].search(
+            [('check_in', '>=', date_test.strftime('%Y/%m/%d, %H:%M:%S')),
+             ('employee_id', '=', employee.id)])
+        if len(attendances) > 0:
+            return employee and {'warning': _(
+                'You can scan a badge only one time per 60 seconds.')}
+        else:
+            # check for duplicates
+            attendances = self.env['hr.attendance'].search(
+                [('check_out', '>=', date_test.strftime('%Y/%m/%d, %H:%M:%S')),
+                 ('employee_id', '=', employee.id)])
+            if len(attendances) > 0:
+                return employee and {'warning': _(
+                    'You can scan a badge only one time per 60 seconds.')}
+            else:
+                return employee and employee.attendance_action(
+                    'hr_attendance.hr_attendance_action_kiosk_mode') or \
+                       {'warning': _(
+                           'No employee corresponding to barcode %s') % barcode}
---
+++ hr_attendance_quagga/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Gianluigi Tiesi <sherpya@netfarm.it>
+* Matteo Piciucchi <matteo.piciucchi@netfarm.it>
+* Letizia Freda <letizia.freda@netfarm.it>
