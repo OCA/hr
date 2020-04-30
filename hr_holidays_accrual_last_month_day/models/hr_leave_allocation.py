e7e1279fe87e72fe27fbcb3c60e1bd8e32a83bbb
Randall Castro - [Vauxoo] <randall@vauxoo.com>
2020-04-29 23:15:48 -0600

    [FIX] hr_leave_allocation.py: testing test_flake8

--- hr_holidays_accrual_last_month_day/models/hr_leave_allocation.py
+++ hr_holidays_accrual_last_month_day/models/hr_leave_allocation.py
@@ -8,17 +8,18 @@ class HolidaysAllocation(models.Model):
     _inherit = "hr.leave.allocation"
 
     force_last_month_day = fields.Boolean(
-        help='If its used this option, the nextcall will to take the last day of the month in the date provided.')
+        help='If its used this option, the nextcall will to '
+        'take the last day of the month in the date provided.')
 
     @api.model
     def _update_accrual(self):
         res = super(HolidaysAllocation, self)._update_accrual()
 
         holidays = self.search([
-            ('accrual', '=', True), ('employee_id.active', '=', True), ('state', '=', 'validate'),
-            ('holiday_type', '=', 'employee'), ('force_last_month_day', '=', True),
-            '|', ('date_to', '=', False), ('date_to', '>', fields.Datetime.now()),
-            ('nextcall', '!=', False)])
+            ('accrual', '=', True), ('employee_id.active', '=', True),
+            ('state', '=', 'validate'), ('holiday_type', '=', 'employee'),
+            ('force_last_month_day', '=', True), '|', ('date_to', '=', False),
+            ('date_to', '>', fields.Datetime.now()), ('nextcall', '!=', False)])
         for holiday in holidays:
             last_day_of_month = calendar.monthrange(holiday.nextcall.year, holiday.nextcall.month)[1]
             if last_day_of_month == holiday.nextcall.day:
