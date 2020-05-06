95929a964450364119b6fd152635818e4c09b3a8
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-05-06 14:19:02 +0200

    [IMP] hr_employee_calendar_planning: black, isort, prettier

--- hr_employee_calendar_planning/models/resource_calendar.py
+++ hr_employee_calendar_planning/models/resource_calendar.py
@@ -5,19 +5,17 @@ from odoo import api, fields, models
 
 
 class ResourceCalendar(models.Model):
-    _inherit = 'resource.calendar'
+    _inherit = "resource.calendar"
 
-    active = fields.Boolean(
-        default=True,
-    )
+    active = fields.Boolean(default=True,)
 
     @api.multi
     def write(self, vals):
         res = super(ResourceCalendar, self).write(vals)
-        for record in self.filtered('active'):
-            calendars = self.env['hr.employee.calendar'].search([
-                ('calendar_id', '=', record.id)
-            ])
-            for employee in calendars.mapped('employee_id'):
+        for record in self.filtered("active"):
+            calendars = self.env["hr.employee.calendar"].search(
+                [("calendar_id", "=", record.id)]
+            )
+            for employee in calendars.mapped("employee_id"):
                 employee._regenerate_calendar()
         return res
