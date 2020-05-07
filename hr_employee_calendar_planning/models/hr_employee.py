90c51913aa7cefb698922c50bc2d3efc02def731
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-05-07 10:49:28 +0200

    [13.0][IMP] support for two_week calendar

--- hr_employee_calendar_planning/models/hr_employee.py
+++ hr_employee_calendar_planning/models/hr_employee.py
@@ -1,7 +1,38 @@
 # Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
+
+SECTION_LINES = [
+    (
+        0,
+        0,
+        {
+            "name": "Even week",
+            "dayofweek": "0",
+            "sequence": "0",
+            "hour_from": 0,
+            "day_period": "morning",
+            "week_type": "0",
+            "hour_to": 0,
+            "display_type": "line_section",
+        },
+    ),
+    (
+        0,
+        0,
+        {
+            "name": "Odd week",
+            "dayofweek": "0",
+            "sequence": "25",
+            "hour_from": 0,
+            "day_period": "morning",
+            "week_type": "1",
+            "hour_to": 0,
+            "display_type": "line_section",
+        },
+    ),
+]
 
 
 class HrEmployee(models.Model):
@@ -15,6 +46,10 @@ class HrEmployee(models.Model):
 
     def _regenerate_calendar(self):
         self.ensure_one()
+        vals_list = []
+        two_weeks = bool(
+            self.calendar_ids.mapped("calendar_id").filtered("two_weeks_calendar")
+        )
         if not self.resource_calendar_id or self.resource_calendar_id.active:
             self.resource_calendar_id = (
                 self.env["resource.calendar"]
@@ -24,23 +59,50 @@ class HrEmployee(models.Model):
                         "name": _("Auto generated calendar for employee")
                         + " %s" % self.name,
                         "attendance_ids": [],
+                        "two_weeks_calendar": two_weeks,
                     }
                 )
                 .id
             )
         else:
             self.resource_calendar_id.attendance_ids.unlink()
-        vals_list = []
+            self.resource_calendar_id.two_weeks_calendar = two_weeks
+        seq = 0
         for line in self.calendar_ids:
             for attendance_line in line.calendar_id.attendance_ids:
+                if attendance_line.display_type == "line_section":
+                    continue
+                duplicate = two_weeks and not line.calendar_id.two_weeks_calendar
+                week_odd = attendance_line.week_type == "1"
                 data = attendance_line.copy_data(
                     {
                         "calendar_id": self.resource_calendar_id.id,
                         "date_from": line.date_start,
                         "date_to": line.date_end,
+                        "week_type": "0" if duplicate else attendance_line.week_type,
+                        "sequence": seq + 26 if week_odd else 24 - seq,
+                        # To make sure sequence of odd weeks is greater than 25
+                        # and sequence of even weeks is less than 25, as in
+                        # /resource/models/resource.py#L266
                     }
                 )[0]
+                seq += 1
                 vals_list.append((0, 0, data))
+                if duplicate:
+                    data = attendance_line.copy_data(
+                        {
+                            "calendar_id": self.resource_calendar_id.id,
+                            "date_from": line.date_start,
+                            "date_to": line.date_end,
+                            "week_type": "1",
+                            "sequence": seq + 26,
+                        },
+                    )[0]
+                    seq += 1
+                    vals_list += [(0, 0, data)]
+        if two_weeks:
+            SECTION_LINES[0][2]["sequence"] = -seq
+            vals_list = SECTION_LINES + vals_list
         self.resource_calendar_id.attendance_ids = vals_list
 
     def regenerate_calendar(self):
@@ -69,6 +131,7 @@ class HrEmployeeCalendar(models.Model):
         ),
     ]
 
+    @api.model_create_multi
     def create(self, vals):
         record = super(HrEmployeeCalendar, self).create(vals)
         record.employee_id._regenerate_calendar()
