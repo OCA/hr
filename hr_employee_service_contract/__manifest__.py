9117449645554ec562e6cae546e5782688da823c
Saran440 <saranl@ecosoft.co.th>
2020-06-16 10:10:29 +0700

    [IMP] hr_employee_service_contract: black, isort, prettier

--- hr_employee_service_contract/__manifest__.py
+++ hr_employee_service_contract/__manifest__.py
@@ -2,28 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'HR Employee Service from Contracts',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'website': 'https://github.com/OCA/hr',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': (
-        'Employee service information & duration based on employee\'s'
-        ' contracts'
+    "name": "HR Employee Service from Contracts",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": (
+        "Employee service information & duration based on employee's" " contracts"
     ),
-    'depends': [
-        'hr',
-        'hr_contract',
-        'hr_employee_service',
-    ],
-    'external_dependencies': {
-        'python': [
-            'dateutil',
-        ],
-    },
+    "depends": ["hr", "hr_contract", "hr_employee_service"],
+    "external_dependencies": {"python": ["dateutil"]},
 }
