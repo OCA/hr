cf7b1c54a2928b0350c1de9f0f46f16fccdf121e
kranokporn <kranokporn.td@gmail.com>
2020-06-18 10:19:12 +0700

    [IMP] hr_employee_service_contract: black, isort, prettier

--- hr_employee_service_contract/models/hr_employee.py
+++ hr_employee_service_contract/models/hr_employee.py
@@ -1,59 +1,51 @@
 # Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     first_contract_id = fields.Many2one(
-        'hr.contract',
-        compute='_compute_first_contract_id',
-        string='First Contract',
-        help='First contract of the employee',
+        "hr.contract",
+        compute="_compute_first_contract_id",
+        string="First Contract",
+        help="First contract of the employee",
     )
     last_contract_id = fields.Many2one(
-        'hr.contract',
-        compute='_compute_last_contract_id',
-        string='Last Contract',
-        help='Last contract of the employee',
+        "hr.contract",
+        compute="_compute_last_contract_id",
+        string="Last Contract",
+        help="Last contract of the employee",
     )
     service_start_date = fields.Date(
-        string='Start Date',
-        readonly=True,
-        related='first_contract_id.date_start',
+        string="Start Date", readonly=True, related="first_contract_id.date_start",
     )
     service_termination_date = fields.Date(
-        string='Termination Date',
-        readonly=True,
-        related='last_contract_id.date_end',
+        string="Termination Date", readonly=True, related="last_contract_id.date_end",
     )
 
     @api.multi
-    @api.depends('contract_ids')
+    @api.depends("contract_ids")
     def _compute_first_contract_id(self):
-        Contract = self.env['hr.contract']
+        Contract = self.env["hr.contract"]
         for employee in self:
             employee.first_contract_id = Contract.search(
-                employee._get_contract_filter(),
-                order='date_start asc',
-                limit=1
+                employee._get_contract_filter(), order="date_start asc", limit=1
             )
 
     @api.multi
-    @api.depends('contract_ids')
+    @api.depends("contract_ids")
     def _compute_last_contract_id(self):
-        Contract = self.env['hr.contract']
+        Contract = self.env["hr.contract"]
         for employee in self:
             employee.last_contract_id = Contract.search(
-                employee._get_contract_filter(),
-                order='date_end desc',
-                limit=1
+                employee._get_contract_filter(), order="date_end desc", limit=1
             )
 
     @api.multi
-    @api.onchange('service_hire_date')
+    @api.onchange("service_hire_date")
     def _onchange_service_hire_date(self):  # pragma: no cover
         # Do nothing
         pass
@@ -63,14 +55,14 @@ class HrEmployee(models.Model):
         self.ensure_one()
 
         return [
-            ('employee_id', '=', self.id),
-            ('state', 'in', self._get_service_contract_states()),
+            ("employee_id", "=", self.id),
+            ("state", "in", self._get_service_contract_states()),
         ]
 
     @api.model
     def _get_service_contract_states(self):
         return [
-            'open',
-            'pending',
-            'close',
+            "open",
+            "pending",
+            "close",
         ]
