9027b8185892367342a94432c997814f7aa24569
watcharaporn-git <watcha076@gmail.com>
2020-06-18 09:51:04 +0700

    [IMP] hr_holidays_credit: black, isort, prettier

--- hr_holidays_credit/__manifest__.py
+++ hr_holidays_credit/__manifest__.py
@@ -2,21 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Leave Credit',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'website': 'https://github.com/OCA/hr',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Enable negative leave balance for employees',
-    'depends': [
-        'hr_holidays',
-    ],
-    'data': [
-        'views/hr_leave_type.xml',
-    ],
+    "name": "Leave Credit",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Enable negative leave balance for employees",
+    "depends": ["hr_holidays"],
+    "data": ["views/hr_leave_type.xml"],
 }
--- hr_holidays_credit/models/hr_leave.py
+++ hr_holidays_credit/models/hr_leave.py
@@ -1,13 +1,13 @@
 # Copyright (C) 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class HrLeave(models.Model):
-    _inherit = 'hr.leave'
+    _inherit = "hr.leave"
 
-    @api.constrains('state', 'number_of_days', 'holiday_status_id')
+    @api.constrains("state", "number_of_days", "holiday_status_id")
     def _check_holidays(self):
         uncreditable_requests = self.filtered(
             lambda holiday: not holiday._is_holiday_credit_allowed()
@@ -28,17 +28,17 @@ class HrLeave(models.Model):
             return True
 
         if self.employee_id in (
-                leave_type.creditable_employee_category_ids.mapped(
-                    'employee_ids'
-                )):
+            leave_type.creditable_employee_category_ids.mapped("employee_ids")
+        ):
             return True
 
         if self.employee_id in (
-                leave_type.creditable_department_ids.mapped(
-                    'member_ids'
-                )):
+            leave_type.creditable_department_ids.mapped("member_ids")
+        ):
             return True
 
-        return not leave_type.creditable_employee_ids and \
-            not leave_type.creditable_employee_category_ids and \
-            not leave_type.creditable_department_ids
+        return (
+            not leave_type.creditable_employee_ids
+            and not leave_type.creditable_employee_category_ids
+            and not leave_type.creditable_department_ids
+        )
