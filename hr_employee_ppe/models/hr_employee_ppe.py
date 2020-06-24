233d6a7e48c63eaa3a3cb9ae48bf572acec03be5
Eduardo Aparicio <eduardo.caparica@gmail.com>
2020-06-24 00:17:54 -0300

    [12.0][UPD] Changes proposed by the Community for the first version implemented.

--- hr_employee_ppe/models/hr_employee_ppe.py
+++ hr_employee_ppe/models/hr_employee_ppe.py
@@ -1,7 +1,8 @@
 # Copyright 2020 Escodoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class HrEmployeePPE(models.Model):
@@ -11,32 +12,45 @@ class HrEmployeePPE(models.Model):
     _description = 'Personal Protective Equipments'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Many2one(
+    name = fields.Char(
+        required=True,
+    )
+    ppe_id = fields.Many2one(
         string='Equipment',
         required=True,
         comodel_name='hr.employee.ppe.equipment',
     )
     employee_id = fields.Many2one(
-        'hr.employee',
+        comodel_name='hr.employee',
         string='Employee',
         required=True
     )
     start_date = fields.Date(
-        'Start Date',
+        string='Start Date',
         default=fields.Date.today()
     )
-    end_date = fields.Date('End Date')
-    description = fields.Text('Description')
-    partner_id = fields.Many2one(
-        'res.partner',
-        'Partner',
-        help="Employer, School, University"
-        "Certification Authority"
+    end_date = fields.Date(
+        string='End Date'
     )
-    location = fields.Char('Location', help="Location")
-    expire = fields.Boolean('Expire', help="Expire", default=True)
+    description = fields.Text(
+        string='Description'
+    )
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Issued By',
+        help='Certification Authority'
+    )
+    location = fields.Char(
+        string='Location',
+        help='Location'
+    )
+    expire = fields.Boolean(
+        string='Expire',
+        help='True if the PPE expires',
+        default=True
+    )
     certification = fields.Char(
-        'Certification Number',
+        string='Certification Number',
         help='Certification Number'
     )
     status = fields.Selection(
@@ -46,6 +60,42 @@ class HrEmployeePPE(models.Model):
         help='Certification Number'
     )
 
-    @api.onchange('name')
+    @api.onchange('ppe_id', 'employee_id', 'end_date', 'start_date')
     def verify_expiracy(self):
-        self.expire = self.name.expirable
+        if self.ppe_id and self.employee_id:
+            self.name = self.ppe_id.name + _(' to ') + self.employee_id.name
+
+        self.expire = self.ppe_id.expirable
+
+        if not self.expire:
+            self.status = 'valid'
+
+        if self.expire and self.end_date:
+            if self.end_date < fields.Date.today():
+                self.status = 'expired'
+            else: 
+                self.status = 'valid'
+
+    @api.model
+    def cron_ppe_expiry_verification(self, date_ref=None):
+        if not date_ref:
+            date_ref = fields.Date.context_today(self)
+        domain = []
+        domain.extend([('end_date', '<', date_ref)])
+        ppes_to_check_expiry = self.search(domain)
+        for record in ppes_to_check_expiry:
+            record.status = 'expired'
+
+    @api.constrains('start_date', 'end_date')
+    def _check_dates(self):
+        for record in self:
+            if self.expire:
+                if not record.end_date or not record.start_date:
+                    raise ValidationError(
+                        _('You must inform start date and end date for expirable PPEs.')
+                    )
+                if record.end_date and record.start_date:
+                    if record.end_date < record.start_date:
+                        raise ValidationError(
+                            _('End date cannot occur earlier than the start date.')
+                        )
--- hr_employee_ppe/models/hr_employee_ppe_equipment.py
+++ hr_employee_ppe/models/hr_employee_ppe_equipment.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Escodoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HrEmployeePpeEquipment(models.Model):
@@ -10,5 +10,18 @@ class HrEmployeePpeEquipment(models.Model):
     _description = 'Personal Protective Equipments - Equipment List'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string='Equipment')
-    expirable = fields.Boolean()
+    name = fields.Char()
+    product_id = fields.Many2one(
+        required=True,
+        help="""Select the PPE from the product list. 
+            Please note that the PPE must be a consumable product.""",
+        comodel_name='product.product',
+        domain="[('type', '=', 'consu')]"
+    )
+    expirable = fields.Boolean(
+        help='Select this option if the PPE has expiry date.'
+    )
+
+    @api.onchange('product_id')
+    def update_name(self):
+        self.name = self.product_id.name
