7924195ddfc6dab02e806e01bd03ddbfd789de6b
Saran440 <saranl@ecosoft.co.th>
2020-06-26 10:29:07 +0700

    [MIG] hr_employee_ssn: Migration to 13.0
    [UPD] Update hr_employee_ssn.pot

--- hr_employee_ssn/__manifest__.py
+++ hr_employee_ssn/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR Employee SSN & SIN",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "Brainbean Apps, Odoo Community Association (OCA)",
