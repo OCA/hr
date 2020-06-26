be7485130b2a7ad3d8e2c5071b01bb364501ea76
Saran440 <saranl@ecosoft.co.th>
2020-06-26 10:22:29 +0700

    [IMP] hr_employee_ssn: black, isort, prettier

--- hr_employee_ssn/__manifest__.py
+++ hr_employee_ssn/__manifest__.py
@@ -2,21 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'HR Employee SSN & SIN',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'website': 'https://github.com/OCA/hr',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'View/edit employee\'s SSN & SIN fields',
-    'depends': [
-        'hr',
-    ],
-    'data': [
-        'views/hr_employee_views.xml',
-    ],
+    "name": "HR Employee SSN & SIN",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Brainbean Apps, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "View/edit employee's SSN & SIN fields",
+    "depends": ["hr"],
+    "data": ["views/hr_employee_views.xml"],
 }
