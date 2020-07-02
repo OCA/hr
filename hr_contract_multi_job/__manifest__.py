b23a753f576cc972c7f98ca96fce79cecef3e0e3
ps-tubtim <pimolnats@ecosoft.co.th>
2020-07-02 14:55:21 +0700

    [IMP] hr_contract_multi_job: black, isort, prettier

--- hr_contract_multi_job/__manifest__.py
+++ hr_contract_multi_job/__manifest__.py
@@ -3,20 +3,15 @@
 
 
 {
-    'name': 'HR Contract Multi Jobs',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Generic Modules/Human Resources',
-    'author': 'Savoir-faire Linux, '
-              'Fekete Mihai (Forest and Biomass Services Romania), '
-              'Odoo Community Association (OCA)',
-    'website': "https://github.com/OCA/hr",
-    'depends': [
-        'hr_contract'
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/hr_contract_view.xml',
-    ],
-    'installable': True,
+    "name": "HR Contract Multi Jobs",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Human Resources",
+    "author": "Savoir-faire Linux, "
+    "Fekete Mihai (Forest and Biomass Services Romania), "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr_contract"],
+    "data": ["security/ir.model.access.csv", "views/hr_contract_view.xml"],
+    "installable": True,
 }
--- hr_contract_multi_job/models/hr_contract_job.py
+++ hr_contract_multi_job/models/hr_contract_job.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class HrContractJob(models.Model):
@@ -12,17 +12,11 @@ class HrContractJob(models.Model):
     add fields to it, e.g. a salary class or a wage scale. These are fields
     that depend on both the contract and the job position.
     """
-    _name = 'hr.contract.job'
-    _description = 'Relational object between contract and job'
 
-    name = fields.Char(string='Job Name', related='job_id.name', index=True)
-    job_id = fields.Many2one(
-        'hr.job',
-        required=True,
-        ondelete='cascade')
-    contract_id = fields.Many2one(
-        'hr.contract',
-        required=True,
-        ondelete='cascade')
-    is_main_job = fields.Boolean(
-        string='Main Job Position')
+    _name = "hr.contract.job"
+    _description = "Relational object between contract and job"
+
+    name = fields.Char(string="Job Name", related="job_id.name", index=True)
+    job_id = fields.Many2one("hr.job", required=True, ondelete="cascade")
+    contract_id = fields.Many2one("hr.contract", required=True, ondelete="cascade")
+    is_main_job = fields.Boolean(string="Main Job Position")
