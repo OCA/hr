80ad227b646279931f902e6158186a731f638bfa
ps-tubtim <pimolnats@ecosoft.co.th>
2020-07-02 14:55:21 +0700

    [IMP] hr_contract_multi_job: black, isort, prettier

--- hr_contract_multi_job/models/hr_job.py
+++ hr_contract_multi_job/models/hr_job.py
@@ -1,13 +1,12 @@
 # Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class HrJob(models.Model):
-    _inherit = 'hr.job'
+    _inherit = "hr.job"
 
     contract_job_ids = fields.One2many(
-        'hr.contract.job',
-        'job_id',
-        string='Contract Jobs')
+        "hr.contract.job", "job_id", string="Contract Jobs"
+    )
--- hr_contract_multi_job/tests/test_hr_contract_multi_jobs.py
+++ hr_contract_multi_job/tests/test_hr_contract_multi_jobs.py
@@ -2,32 +2,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
-from odoo import exceptions
 
 
 class TestContractMultiJob(TransactionCase):
     def setUp(self):
         super(TestContractMultiJob, self).setUp()
-        self.employee_model = self.env['hr.employee']
-        self.user_model = self.env['res.users']
-        self.contract_model = self.env['hr.contract']
-        self.job_model = self.env['hr.job']
+        self.employee_model = self.env["hr.employee"]
+        self.user_model = self.env["res.users"]
+        self.contract_model = self.env["hr.contract"]
+        self.job_model = self.env["hr.job"]
 
         # Create an employee
-        self.employee_id = self.employee_model.create({'name': 'Employee 1'})
+        self.employee_id = self.employee_model.create({"name": "Employee 1"})
 
         # Create 2 jobs
-        self.job_id = self.job_model.create({'name': 'Job 1'})
+        self.job_id = self.job_model.create({"name": "Job 1"})
 
-        self.job_2_id = self.job_model.create({'name': 'Job 2'})
+        self.job_2_id = self.job_model.create({"name": "Job 2"})
 
         # Create a contract
         self.contract_id = self.contract_model.create(
-            {
-                'employee_id': self.employee_id.id,
-                'name': 'Contract 1',
-                'wage': 50000,
-            }
+            {"employee_id": self.employee_id.id, "name": "Contract 1", "wage": 50000}
         )
 
     def test_no_main_jobs(self):
@@ -36,7 +31,7 @@ class TestContractMultiJob(TransactionCase):
         when contract has no assigned job
         and check job_id is False.
         """
-        self.contract_id.write({'contract_job_ids': []})
+        self.contract_id.write({"contract_job_ids": []})
         self.assertFalse(self.contract_id.job_id is False)
 
     def test_one_main_jobs(self):
@@ -45,9 +40,13 @@ class TestContractMultiJob(TransactionCase):
         when contract has one assigned job
         and check is the job_id is set.
         """
-        self.contract_id.write({'contract_job_ids':
-                                [(0, 0, {'job_id': self.job_id.id,
-                                         'is_main_job': True})]})
+        self.contract_id.write(
+            {
+                "contract_job_ids": [
+                    (0, 0, {"job_id": self.job_id.id, "is_main_job": True})
+                ]
+            }
+        )
         self.assertEqual(self.contract_id.job_id.id, self.job_id.id)
 
     def test_two_contract_jobs_one_main_job(self):
@@ -56,11 +55,14 @@ class TestContractMultiJob(TransactionCase):
         when contract has two assigned jobs
         and check is the job_id is set as main job.
         """
-        self.contract_id.write({'contract_job_ids':
-                                [(0, 0, {'job_id': self.job_id.id,
-                                         'is_main_job': True}),
-                                 (0, 0, {'job_id': self.job_2_id.id,
-                                         'is_main_job': False})]})
+        self.contract_id.write(
+            {
+                "contract_job_ids": [
+                    (0, 0, {"job_id": self.job_id.id, "is_main_job": True}),
+                    (0, 0, {"job_id": self.job_2_id.id, "is_main_job": False}),
+                ]
+            }
+        )
         self.assertEqual(self.contract_id.job_id.id, self.job_id.id)
 
     def test_two_contract_jobs_two_main_job(self):
