748b6e712700b695fc5a19a019c7028f1a67a1b5
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-07-21 13:21:21 +0200

    [IMP] hr_employee_medical_examination: black, isort, prettier

--- hr_employee_medical_examination/models/hr_employee.py
+++ hr_employee_medical_examination/models/hr_employee.py
@@ -6,32 +6,29 @@ from odoo import api, fields, models
 
 class HrEmployee(models.Model):
 
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     medical_examination_ids = fields.One2many(
-        comodel_name='hr.employee.medical.examination',
-        inverse_name='employee_id',
+        comodel_name="hr.employee.medical.examination", inverse_name="employee_id",
     )
 
     medical_examination_count = fields.Integer(
-        compute='_compute_medical_examination_count',
+        compute="_compute_medical_examination_count",
     )
 
     can_see_examinations_button = fields.Boolean(
-        compute='_compute_can_see_examinations_button',
+        compute="_compute_can_see_examinations_button",
     )
 
-    @api.depends('medical_examination_ids')
+    @api.depends("medical_examination_ids")
     def _compute_medical_examination_count(self):
         for record in self:
-            record.medical_examination_count = len(
-                record.medical_examination_ids
-            )
+            record.medical_examination_count = len(record.medical_examination_ids)
 
     @api.multi
     def _compute_can_see_examinations_button(self):
         for record in self:
             record.can_see_examinations_button = (
-                self.env.uid == record.user_id.id or
-                self.env.user.has_group('hr.group_hr_manager')
+                self.env.uid == record.user_id.id
+                or self.env.user.has_group("hr.group_hr_manager")
             )
