0955895ec27c95c066bce6c5db556f938cf6b66e
Andrea <a.stirpe@onestein.nl>
2020-08-27 09:23:17 +0200

    [IMP] hr_employee_firstname: ease extension

--- hr_employee_firstname/models/hr_employee.py
+++ hr_employee_firstname/models/hr_employee.py
@@ -30,6 +30,20 @@ class HrEmployee(models.Model):
 
     @api.model
     def create(self, vals):
+        self._prepare_vals_on_create_firstname_lastname(vals)
+        res = super(HrEmployee, self).create(vals)
+        res._update_partner_firstname()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        self._prepare_vals_on_write_firstname_lastname(vals)
+        res = super(HrEmployee, self).write(vals)
+        if set(vals).intersection(UPDATE_PARTNER_FIELDS):
+            self._update_partner_firstname()
+        return res
+
+    def _prepare_vals_on_create_firstname_lastname(self, vals):
         if vals.get('firstname') or vals.get('lastname'):
             vals['name'] = self._get_name(vals['lastname'], vals['firstname'])
         elif vals.get('name'):
@@ -37,12 +51,8 @@ class HrEmployee(models.Model):
             vals['firstname'] = self.split_name(vals['name'])['firstname']
         else:
             raise UserError(_('No name set.'))
-        res = super(HrEmployee, self).create(vals)
-        res._update_partner_firstname()
-        return res
 
-    @api.multi
-    def write(self, vals):
+    def _prepare_vals_on_write_firstname_lastname(self, vals):
         if 'firstname' in vals or 'lastname' in vals:
             if 'lastname' in vals:
                 lastname = vals.get('lastname')
@@ -56,10 +66,6 @@ class HrEmployee(models.Model):
         elif vals.get('name'):
             vals['lastname'] = self.split_name(vals['name'])['lastname']
             vals['firstname'] = self.split_name(vals['name'])['firstname']
-        res = super(HrEmployee, self).write(vals)
-        if set(vals).intersection(UPDATE_PARTNER_FIELDS):
-            self._update_partner_firstname()
-        return res
 
     @api.model
     def split_name(self, name):
