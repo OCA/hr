fdfbb147a19e7a42480618a63fedfc88c962a7e6
Khalid <m.khalid@serpentcs.com>
2020-09-29 12:45:09 +0530

    ADD hr_expense_analytic_require

---
+++ hr_expense_analytic_require/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import company
+from . import res_config_settings
+from . import hr_expense
