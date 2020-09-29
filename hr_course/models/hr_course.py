65de3c4564b06cc40cb63b97c4e7024f9fa5d18e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-29 13:02:50 +0200

    [13.0][MIG] hr_course

--- hr_course/models/hr_course.py
+++ hr_course/models/hr_course.py
@@ -10,7 +10,7 @@ class HrCourse(models.Model):
     _description = "Course"
     _inherit = "mail.thread"
 
-    name = fields.Char(string="Name", required=True, track_visibility="onchange")
+    name = fields.Char(string="Name", required=True, tracking=True)
     category_id = fields.Many2one(
         "hr.course.category", string="Category", required=True,
     )
@@ -19,41 +19,43 @@ class HrCourse(models.Model):
         string="Start date",
         readonly=True,
         states={"draft": [("readonly", False)]},
-        track_visibility="onchange",
+        tracking=True,
     )
     end_date = fields.Date(
         string="End date",
         readonly=True,
         states={"draft": [("readonly", False)]},
-        track_visibility="onchange",
+        tracking=True,
     )
     currency_id = fields.Many2one(
         "res.currency",
         string="Currency",
         default=lambda self: self.env.user.company_id.currency_id,
     )
-    cost = fields.Monetary(
-        string="Course Cost", required=True, track_visibility="onchange"
-    )
+    cost = fields.Monetary(string="Course Cost", required=True, tracking=True)
     authorized_by = fields.Many2one(
         string="Authorized by",
         comodel_name="hr.employee",
         required=True,
         readonly=True,
         states={"draft": [("readonly", False)]},
-        track_visibility="onchange",
+        tracking=True,
     )
     permanence = fields.Boolean(
         string="Has Permanence",
         readonly=True,
         states={"draft": [("readonly", False)]},
-        track_visibility="onchange",
+        tracking=True,
+        help="Check if the participants of this course are restricted to"
+        " stay in the company for a certain period of time.",
     )
     permanence_time = fields.Char(
         string="Permanence time",
         readonly=True,
         states={"draft": [("readonly", False)]},
-        track_visibility="onchange",
+        tracking=True,
+        help="Amount of time the employee is restricted to stay in the"
+        " company by participating to this course.",
     )
     state = fields.Selection(
         [
@@ -67,7 +69,7 @@ class HrCourse(models.Model):
         required=True,
         readonly=True,
         default="draft",
-        track_visibility="onchange",
+        tracking=True,
     )
     attendant_ids = fields.Many2many(
         "hr.employee",
@@ -86,7 +88,7 @@ class HrCourse(models.Model):
         self.ensure_one()
         if self.start_date and self.end_date and (self.start_date > self.end_date):
             raise ValidationError(
-                _("The start date cannot" " be later than the end date.")
+                _("The start date cannot be later than the end date.")
             )
 
     def all_passed(self):
@@ -145,22 +147,18 @@ class HrCourse(models.Model):
             "state": "cancelled",
         }
 
-    @api.multi
     def draft2waiting(self):
         for record in self:
             record.write(record._draft2waiting_values())
 
-    @api.multi
     def waiting2inprogress(self):
         for record in self:
             record.write(record._waiting2inprogress_values())
 
-    @api.multi
     def inprogress2validation(self):
         for record in self:
             record.write(record._inprogress2validation_values())
 
-    @api.multi
     def validation2complete(self):
         for record in self:
             if self.course_attendee_ids.filtered(
@@ -172,12 +170,10 @@ class HrCourse(models.Model):
             else:
                 record.write(record._validation2complete_values())
 
-    @api.multi
     def back2draft(self):
         for record in self:
             record.write(record._back2draft_values())
 
-    @api.multi
     def cancel_course(self):
         for record in self:
             record.write(record._cancel_course_values())
