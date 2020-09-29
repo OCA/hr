068158bfe61772652b59bd0ab195167911c04bb7
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-29 12:54:51 +0200

    [IMP] hr_course: black, isort, prettier

--- hr_course/__manifest__.py
+++ hr_course/__manifest__.py
@@ -2,22 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hr Course',
-    'summary': """
+    "name": "Hr Course",
+    "summary": """
         This module allows your to manage employee's training courses""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr/',
-    'depends': [
-        'hr',
-        'mail',
-    ],
-    'data': [
-        'security/course_security.xml',
-        'security/ir.model.access.csv',
-        'views/hr_course_category_views.xml',
-        'views/hr_course_views.xml',
-        'views/hr_employee_views.xml',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr/",
+    "depends": ["hr", "mail"],
+    "data": [
+        "security/course_security.xml",
+        "security/ir.model.access.csv",
+        "views/hr_course_category_views.xml",
+        "views/hr_course_views.xml",
+        "views/hr_employee_views.xml",
     ],
 }
