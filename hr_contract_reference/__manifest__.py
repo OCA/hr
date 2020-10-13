350167e28364525feb1fc2c5b209d2571a982929
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-10-13 17:35:47 +0200

    [13.0][MIG] hr_contract_reference

--- hr_contract_reference/__manifest__.py
+++ hr_contract_reference/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "HR Contract Reference",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Generic Modules/Human Resources",
     "author": "Michael Telahun Makonnen, "
     "Fekete Mihai (Forest and Biomass Services Romania), "
