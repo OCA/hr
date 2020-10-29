c2581874739cf5c76d07482bbd360a8a7e304434
newtratip <tharathip.chaweewongphan@gmail.com>
2020-10-30 00:13:12 +0700

    [IMP] hr_employee_document: black, isort, prettier

--- hr_employee_document/__manifest__.py
+++ hr_employee_document/__manifest__.py
@@ -2,22 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'HR Employee Document',
-    'version': '12.0.1.1.0',
-    'category': 'Human Resources',
-    'website': 'https://github.com/OCA/hr',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Documents attached to the employee profile',
-    'depends': [
-        'hr',
-        'document',
-    ],
-    'data': [
-        'views/hr_employee.xml',
-    ],
+    "name": "HR Employee Document",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Brainbean Apps, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Documents attached to the employee profile",
+    "depends": ["hr", "document"],
+    "data": ["views/hr_employee.xml"],
 }
