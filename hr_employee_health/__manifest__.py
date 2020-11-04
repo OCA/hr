5e6b47b098625fa184bb37e9690714f0775cbf58
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-11-04 12:47:36 +0200

    [UPD] Brainbean Apps => CorporateHub

--- hr_employee_health/__manifest__.py
+++ hr_employee_health/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2018 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
@@ -7,7 +8,7 @@
     'category': 'Human Resources',
     'website': 'https://github.com/OCA/hr',
     'author':
-        'Brainbean Apps, '
+        'CorporateHub, '
         'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'installable': True,
