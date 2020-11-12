d35303a230e34938a000f7295c3cea398cbf5443
aphon61bank <aphon61bank@gmail.com>
2020-11-12 13:03:00 +0700

    [IMP] hr_employee_calendar_planning: black, isort, prettier

--- hr_employee_calendar_planning/__manifest__.py
+++ hr_employee_calendar_planning/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Employee Calendar Planning",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["hr"],
