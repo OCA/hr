43139c231215bf66ea8e055a3a2a106de759202c
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-12-10 12:21:28 +0100

    [13.0][IMP] hr_employee_relative: Add smart button

--- hr_employee_relative/models/hr_employee.py
+++ hr_employee_relative/models/hr_employee.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HrEmployee(models.Model):
@@ -12,3 +12,19 @@ class HrEmployee(models.Model):
         comodel_name="hr.employee.relative",
         inverse_name="employee_id",
     )
+
+    relatives_count = fields.Integer(compute="_compute_relatives_count")
+
+    @api.depends("relative_ids")
+    def _compute_relatives_count(self):
+        for record in self:
+            record.relatives_count = len(record.relative_ids)
+
+    def action_view_relatives(self):
+        self.ensure_one()
+        action = self.env.ref("hr_employee_relative.action_employee_relatives").read()[
+            0
+        ]
+        action["domain"] = [("employee_id", "=", self.id)]
+        action["context"] = {"default_employee_id": self.id}
+        return action
