fec4ea982c948aea144a61f1bca9293e2548ceb3
Daniel Reis <dreis@opensourceintegrators.com>
2020-12-14 22:33:53 +0000

    [FIX] hr_expense_sale_invoice: remove unrelated changes

--- hr_expense_sale_invoice/models/hr_expense.py
+++ hr_expense_sale_invoice/models/hr_expense.py
@@ -6,17 +6,6 @@ from odoo import api, fields, models
 class HrExpense(models.Model):
     _inherit = "hr.expense"
 
-    reference = fields.Char("Vendor")
     expense_invoice_id = fields.Many2one(
         "account.invoice", string="Invoice", readonly=True, copy=False
     )
-
-    @api.onchange("analytic_account_id")
-    def _onchange_analytic_account_id(self):
-        for rec in self:
-            if rec.analytic_account_id:
-                rec.sale_order_id = self.env["sale.order"].search(
-                    [("analytic_account_id", "=", rec.analytic_account_id.id)],
-                    limit=1,
-                    order="id desc",
-                )
