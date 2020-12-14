5982520645f21f7fbdced5fca5ee757521e03ea1
Khalid <m.khalid@serpentcs.com>
2020-12-14 21:23:38 +0530

    [IMP] change logic for expense calculation on invoice

--- hr_expense_sale_invoice/models/account_invoice.py
+++ hr_expense_sale_invoice/models/account_invoice.py
@@ -7,7 +7,8 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     expense_ids = fields.One2many(
-        "hr.expense", "expense_invoice_id", string="Expenses", readonly=True, copy=False
+        "hr.expense", "expense_invoice_id",
+        string="Expenses", readonly=True, copy=False
     )
     expense_count = fields.Integer(
         "Number of expenses", compute="_compute_expense_count"
@@ -36,12 +37,6 @@ class AccountInvoiceLine(models.Model):
 
     @api.model
     def create(self, values):
-        """ Link the timesheet from the SO lines to the corresponding draft invoice.
-            NOTE: only the timesheets linked to an Sale Line with a product
-            invoiced on delivered quantity are concerned, since in ordered
-            quantity, the timesheet quantity is not invoiced, but is simply
-            to compute the delivered one (for reporting).
-        """
         invoice_line = super(AccountInvoiceLine, self).create(values)
         if (
             invoice_line.invoice_id.type == "out_invoice"
@@ -50,20 +45,13 @@ class AccountInvoiceLine(models.Model):
             sale_line_delivery = invoice_line.sale_line_ids.filtered(
                 lambda sol: sol.product_id.invoice_policy == "delivery"
                 and sol.product_id.expense_policy == "cost"
+                and sol.product_id.type == 'service'
             )
             if sale_line_delivery:
-                for line in sale_line_delivery:
-                    if line.expense_id:
-                        line.expense_id.expense_invoice_id = invoice_line.invoice_id
+                expenses = self.env['hr.expense'].search(
+                    [('sale_order_id', '=', sale_line_delivery.order_id.id),
+                     ('expense_invoice_id', '=', False)])
+                expenses.write({
+                    'expense_invoice_id': invoice_line.invoice_id.id,
+                })
         return invoice_line
-
-    @api.model
-    def _expense_domain_get_invoiced_lines(self, sale_line_delivery):
-        """ Get the domain for the timesheet to link to the created invoice
-            :param sale_line_delivery: recordset of sale.order.line to invoice
-            :return a normalized domain
-        """
-        return [
-            ("so_line", "in", sale_line_delivery.ids),
-            ("expense_invoice_id", "=", False),
-        ]
