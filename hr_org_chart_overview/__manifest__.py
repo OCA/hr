1b3d4f959db51766f90606c8cb62278a3a7a2450
Irfani <irfani@irfani.web.id>
2020-12-24 23:36:33 +0000

    [14.0][MIG] hr_org_chart_overview : Add custom template to properly show chart control toolbar

---
+++ hr_org_chart_overview/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "HR Org Chart Overview",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Organizational Chart Overview",
+    "depends": ["hr"],
+    "data": ["views/hr_org_chart_overview_templates.xml", "views/hr_views.xml"],
+    "qweb": ["static/src/xml/hr_org_chart_overview.xml"],
+}
---
+++ hr_org_chart_overview/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Adria Gil Sorribes <adria.gil@forgeflow.com>
+* Lois Rilo Antelo <lois.rilo@forgeflow.com>
+* Irfani <irfani@irfani.web.id>
