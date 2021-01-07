e6e400d7d112b0f5c1da2c264960c1210e498b4b
Saran440 <saranl@ecosoft.co.th>
2021-01-07 16:43:09 +0700

    [FIX] rename fields residual

--- hr_expense_advance_clearing/tests/test_hr_expense_advance_clearing.py
+++ hr_expense_advance_clearing/tests/test_hr_expense_advance_clearing.py
@@ -142,7 +142,7 @@ class TestHrExpenseAdvanceClearing(common.SavepointCase):
         self.advance.action_submit_sheet()
         self.advance.approve_expense_sheets()
         self.advance.action_sheet_move_create()
-        self.assertEqual(self.advance.residual, 1000.0)
+        self.assertEqual(self.advance.clearing_residual, 1000.0)
         self._register_payment(self.advance)
         self.assertEqual(self.advance.state, 'done')
         # ------------------ Clearing --------------------------
@@ -165,7 +165,7 @@ class TestHrExpenseAdvanceClearing(common.SavepointCase):
         self.advance.action_submit_sheet()
         self.advance.approve_expense_sheets()
         self.advance.action_sheet_move_create()
-        self.assertEqual(self.advance.residual, 1000.0)
+        self.assertEqual(self.advance.clearing_residual, 1000.0)
         self._register_payment(self.advance)
         self.assertEqual(self.advance.state, 'done')
         # ------------------ Clearing --------------------------
@@ -187,7 +187,7 @@ class TestHrExpenseAdvanceClearing(common.SavepointCase):
         self.advance.action_submit_sheet()
         self.advance.approve_expense_sheets()
         self.advance.action_sheet_move_create()
-        self.assertEqual(self.advance.residual, 1000.0)
+        self.assertEqual(self.advance.clearing_residual, 1000.0)
         self._register_payment(self.advance)
         self.assertEqual(self.advance.state, 'done')
         # ------------------ Clearing --------------------------
@@ -200,6 +200,6 @@ class TestHrExpenseAdvanceClearing(common.SavepointCase):
         # Less amount, state set to done. Still remain 200 to be returned
         self.assertEqual(self.clearing_less.state, 'done')
         self.assertEqual(self.clearing_less.advance_sheet_residual, 200.0)
-        # Back to advance and do return advance, residual become 0.0
+        # Back to advance and do return advance, clearing residual become 0.0
         self._register_payment(self.advance, hr_return_advance=True)
-        self.assertEqual(self.advance.residual, 0.0)
+        self.assertEqual(self.advance.clearing_residual, 0.0)
