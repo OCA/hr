5bf741b16e3953965ef1aab364ff9bfb918decab
Reed Hayashikawa <rhayashikawa@opensourceintegrators.com>
2021-01-26 15:28:07 -0800

    [IMP] Expense Product - Attachment required issue for bulk import (#4719)

--- hr_expense_receipt_require/models/hr_expense.py
+++ hr_expense_receipt_require/models/hr_expense.py
@@ -1,17 +1,19 @@
 # Copyright (C) 2020 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, models, _
+from odoo import api, models, fields, _
 from odoo.exceptions import UserError
 
 
 class HrExpense(models.Model):
     _inherit = "hr.expense"
 
+    allow_without_attachment = fields.Boolean(default=False, string='Allow lines without attachment')
+
     @api.multi
     def action_submit_expenses(self):
         for rec in self:
-            if rec.product_id.expense_receipt_required and \
-                    not rec.attachment_number:
+            if (rec.product_id.expense_receipt_required and \
+                    not rec.attachment_number) and not rec.allow_without_attachment:
                 raise UserError(_('You need to provide a receipt'
                                   ' to submit this expense!'))
         return super(HrExpense, self).action_submit_expenses()
@@ -20,12 +22,14 @@ class HrExpense(models.Model):
 class HrExpenseSheet(models.Model):
     _inherit = "hr.expense.sheet"
 
+    allow_without_attachment = fields.Boolean(default=False, string='Allow lines without attachment')
+
     @api.multi
     def action_submit_sheet(self):
         for rec in self:
             for expense in rec.expense_line_ids:
-                if expense.product_id.expense_receipt_required and \
-                        not expense.attachment_number:
+                if (expense.product_id.expense_receipt_required and \
+                        not expense.attachment_number) and not rec.allow_without_attachment:
                     raise UserError(_('You need to provide a receipt to '
                                       'submit %s expense!') % expense.name)
 
