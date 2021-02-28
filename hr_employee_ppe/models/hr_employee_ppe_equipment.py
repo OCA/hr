9562352d7909c1809fa5437a55b942f1829064f0
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-28 14:58:46 -0300

    [IMP] hr_employee_ppe: black, isort, prettier

--- hr_employee_ppe/models/hr_employee_ppe_equipment.py
+++ hr_employee_ppe/models/hr_employee_ppe_equipment.py
@@ -6,22 +6,20 @@ from odoo import api, fields, models
 
 class HrEmployeePpeEquipment(models.Model):
 
-    _name = 'hr.employee.ppe.equipment'
-    _description = 'Personal Protective Equipments - Equipment List'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _rec_name = 'product_id'
+    _name = "hr.employee.ppe.equipment"
+    _description = "Personal Protective Equipments - Equipment List"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _rec_name = "product_id"
 
     product_id = fields.Many2one(
         required=True,
         help="""Select the PPE from the product list.
             Please note that the PPE must be a consumable product.""",
-        comodel_name='product.product',
-        domain="[('type', '=', 'consu')]"
+        comodel_name="product.product",
+        domain="[('type', '=', 'consu')]",
     )
-    expirable = fields.Boolean(
-        help='Select this option if the PPE has expiry date.'
-    )
+    expirable = fields.Boolean(help="Select this option if the PPE has expiry date.")
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def update_name(self):
         self.name = self.product_id.name
