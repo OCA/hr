8d30a1adeb54fe19243c1c9ec67da86122ae2528
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-28 14:58:46 -0300

    [IMP] hr_employee_ppe: black, isort, prettier

--- hr_employee_ppe/__manifest__.py
+++ hr_employee_ppe/__manifest__.py
@@ -3,27 +3,23 @@
 
 {
     "name": "Personal Protective Equipment (PPE) Management",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.1.0",
     "author": "Escodoo, Odoo Community Association (OCA)",
-    'maintainers': ['marcelsavegnago', 'eduaparicio'],
-    'images': ['static/description/banner.png'],
+    "maintainers": ["marcelsavegnago", "eduaparicio"],
+    "images": ["static/description/banner.png"],
     "website": "https://github.com/oca/hr",
     "license": "AGPL-3",
     "category": "Human Resources",
-    "depends": ["hr",
-                'mail',
-                'product'],
+    "depends": ["hr", "mail", "product"],
     "data": [
         "security/hr_security.xml",
-        'security/hr_employee_ppe_equipment.xml',
+        "security/hr_employee_ppe_equipment.xml",
         "security/ir.model.access.csv",
         "views/hr_employee_view.xml",
         "views/hr_employee_ppe_view.xml",
-        'views/hr_employee_ppe_equipment.xml',
-        'data/hr_employee_ppe_cron.xml',
+        "views/hr_employee_ppe_equipment.xml",
+        "data/hr_employee_ppe_cron.xml",
     ],
-    'demo': [
-        'demo/hr_employee_ppe_demo.xml',
-    ],
-    'installable': True
+    "demo": ["demo/hr_employee_ppe_demo.xml"],
+    "installable": True,
 }
