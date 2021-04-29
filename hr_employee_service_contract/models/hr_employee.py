c44c80deed66a930b3dc36a36deaceb4b30795ee
YurGur <jg.gural@gmail.com>
2021-04-29 11:36:24 -0500

    [12.0] [FIX] [hr_employee_service]
    Сorrect filling of the fields "service_start_date", "service_termination_date".
    It depends on the status change from "New" to "Running", and also if the "End date" field in one of the contracts is empty.

--- hr_employee_service_contract/models/hr_employee.py
+++ hr_employee_service_contract/models/hr_employee.py
@@ -31,7 +31,7 @@ class HrEmployee(models.Model):
     )
 
     @api.multi
-    @api.depends('contract_ids', 'contract_ids.date_start')
+    @api.depends('contract_ids', 'contract_ids.date_start', 'contract_ids.state')
     def _compute_first_contract_id(self):
         Contract = self.env['hr.contract']
         for employee in self:
@@ -42,22 +42,31 @@ class HrEmployee(models.Model):
             )
 
     @api.multi
-    @api.depends('contract_ids', 'contract_ids.date_end')
+    @api.depends('contract_ids', 'contract_ids.date_end', 'contract_ids.state')
     def _compute_last_contract_id(self):
         Contract = self.env['hr.contract']
         for employee in self:
-            employee.last_contract_id = Contract.search(
-                employee._get_contract_filter(),
-                order='date_end desc',
-                limit=1
-            )
-
+            employee.last_contract_id = self._get_last_contract_id(employee.id)
+    
     @api.multi
     @api.onchange('service_hire_date')
     def _onchange_service_hire_date(self):  # pragma: no cover
         # Do nothing
         pass
 
+    @api.multi
+    def _get_last_contract_id(self, employee_id):
+        cr = self.env.cr
+        query = '''SELECT c.id
+                FROM hr_contract c
+                WHERE c.employee_id = %s
+                    AND c.state IN %s
+                ORDER BY COALESCE(c.date_end, c.date_start) DESC
+                LIMIT 1'''
+        cr.execute(query, (employee_id, tuple(self._get_service_contract_states())))
+        contract_ids = cr.fetchone()
+        return self.env['hr.contract'].browse(contract_ids)
+    
     @api.multi
     def _get_contract_filter(self):
         self.ensure_one()
