e32a94bde5e8db9dd0d47034140129153b43583e
Chanakya Soni <csoni@opensourceintegrators.com>
2021-05-31 17:54:40 +0530

    [14.0][FIX] Travis.

--- hr_employee_birth_name/__manifest__.py
+++ hr_employee_birth_name/__manifest__.py
@@ -2,16 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Employee Birth Name',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'author': "Camptocamp SA,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/hr',
-    'license': 'AGPL-3',
-    'depends': ['hr', ],
-    'data': [
-        'views/hr_employee_view.xml',
+    "name": "Employee Birth Name",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "author": "Camptocamp SA,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "license": "AGPL-3",
+    "depends": [
+        "hr",
     ],
-    'installable': True,
-    'auto_install': False,
+    "data": [
+        "views/hr_employee_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
 }
--- hr_employee_birth_name/models/hr_employee.py
+++ hr_employee_birth_name/models/hr_employee.py
@@ -1,10 +1,10 @@
 # copyright  2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     birth_name = fields.Char()
