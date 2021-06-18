fba39c0ebbbd1977543a4cf7229c7f3d805914c7
Alba Riera <alba.riera@creublanca.es>
2021-06-18 08:41:14 +0200

    [ADD] hr_personal_equipment_variant_configurator

---
+++ hr_personal_equipment_variant_configurator/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Hr Personal Equipment Variant Configurator",
+    "summary": """
+        Manage variants of personal equipment""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr_personal_equipment_request", "product_variant_configurator"],
+    "data": ["views/hr_personal_equipment_request.xml"],
+}
