912c3d0b557cd0031be200f6401a9301db8bb43d
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-13 11:24:17 +0200

    [FIX] hr_employee_calendar_planning: Prevent disable resource.calendar when is related to some employee

--- hr_employee_calendar_planning/models/resource_calendar.py
+++ hr_employee_calendar_planning/models/resource_calendar.py
@@ -1,13 +1,27 @@
 # Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResourceCalendar(models.Model):
     _inherit = "resource.calendar"
 
     active = fields.Boolean(default=True)
+
+    @api.constrains("active")
+    def _check_active(self):
+        for item in self:
+            total_items = self.env["hr.employee.calendar"].search_count(
+                [("calendar_id", "=", item.id)]
+            )
+            if total_items:
+                raise ValidationError(
+                    _("%s is used in %s employee(s). You should change them first.")
+                    % (item.name, total_items)
+                )
 
     def write(self, vals):
         res = super(ResourceCalendar, self).write(vals)
