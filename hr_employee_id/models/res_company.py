9144bf4ddd04a5a5b808d2e6a40ec694c34cad39
Michael Telahun <mtm@trevi.et>
2021-07-14 18:18:10 +0300

    [IMP] hr_employee_id: black, isort, prettier

--- hr_employee_id/models/res_company.py
+++ hr_employee_id/models/res_company.py
@@ -7,23 +7,21 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     employee_id_gen_method = fields.Selection(
         selection=[
-            ('random', 'Random'),
-            ('sequence', 'Sequence'),
+            ("random", "Random"),
+            ("sequence", "Sequence"),
         ],
-        string='Generation Method',
-        default='random'
+        string="Generation Method",
+        default="random",
     )
     employee_id_random_digits = fields.Integer(
-        string='# of Digits',
-        default=5,
-        help='Number of digits in employee identifier'
+        string="# of Digits", default=5, help="Number of digits in employee identifier"
     )
     employee_id_sequence = fields.Many2one(
-        comodel_name='ir.sequence',
-        string='Identifier Sequence',
-        help='Pattern to be used for employee identifier generation'
+        comodel_name="ir.sequence",
+        string="Identifier Sequence",
+        help="Pattern to be used for employee identifier generation",
     )
