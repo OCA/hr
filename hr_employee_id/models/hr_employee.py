3ac55a3c7dbff21dac39e9f9fc551ae3a40bef13
Michael Telahun <mtm@trevi.et>
2021-07-14 18:18:10 +0300

    hr_employee_id: black, isort, prettier

--- hr_employee_id/models/hr_employee.py
+++ hr_employee_id/models/hr_employee.py
@@ -6,7 +6,8 @@
 import logging
 import random
 import string
-from odoo import api, fields, models, _
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
@@ -15,16 +16,16 @@ _logger = logging.getLogger(__name__)
 class HrEmployee(models.Model):
     """Implement company wide unique identification number."""
 
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
-    identification_id = fields.Char(
-        string='Identification No',
-        copy=False
-    )
+    identification_id = fields.Char(string="Identification No", copy=False)
 
     _sql_constraints = [
-        ('identification_id_uniq', 'unique(identification_id)',
-         'The Employee Number must be unique across the company(s).'),
+        (
+            "identification_id_uniq",
+            "unique(identification_id)",
+            "The Employee Number must be unique across the company(s).",
+        ),
     ]
 
     @api.model
@@ -34,32 +35,29 @@ class HrEmployee(models.Model):
 
         for retry in range(50):
             employee_id = False
-            if company.employee_id_gen_method == 'sequence':
+            if company.employee_id_gen_method == "sequence":
                 if not company.employee_id_sequence:
-                    _logger.warning(
-                        'No sequence configured for employee ID generation'
-                    )
+                    _logger.warning("No sequence configured for employee ID generation")
                     return employee_id
                 employee_id = company.employee_id_sequence.next_by_id()
-            elif company.employee_id_gen_method == 'random':
+            elif company.employee_id_gen_method == "random":
                 employee_id_random_digits = company.employee_id_random_digits
                 rnd = random.SystemRandom()
-                employee_id = ''.join(
-                    rnd.choice(string.digits)
-                    for x in range(employee_id_random_digits)
+                employee_id = "".join(
+                    rnd.choice(string.digits) for x in range(employee_id_random_digits)
                 )
 
-            if self.search_count([('identification_id', '=', employee_id)]):
+            if self.search_count([("identification_id", "=", employee_id)]):
                 continue
 
             return employee_id
 
         raise UserError(
-            _('Unable to generate unique Employee ID in %d steps.') % (retry, )
+            _("Unable to generate unique Employee ID in %d steps.") % (retry,)
         )
 
     @api.model
     def create(self, vals):
-        if not vals.get('identification_id'):
-            vals['identification_id'] = self._generate_identification_id()
+        if not vals.get("identification_id"):
+            vals["identification_id"] = self._generate_identification_id()
         return super(HrEmployee, self).create(vals)
