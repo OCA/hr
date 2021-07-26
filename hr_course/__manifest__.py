be80b153f40391cbcdcfc2462c8edee6d969e02a
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-07-26 15:30:43 +0200

    [IMP] : black, isort, prettier

--- hr_course/__manifest__.py
+++ hr_course/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/hr/",
+    "website": "https://github.com/OCA/hr",
     "depends": ["hr", "mail"],
     "data": [
         "security/course_security.xml",
