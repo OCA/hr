93a09d1685a798fa35043f7813fce545cfb5612a
newtratip <tharathip.chaweewongphan@gmail.com>
2021-08-12 02:23:00 +0700

    [14.0][ADD] hr_employee_digitized_signature

---
+++ hr_employee_digitized_signature/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Employee Digitized Signature",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "depends": ["hr"],
+    "data": ["views/hr_employee_views.xml"],
+    "installable": True,
+    "maintainers": ["newtratip"],
+}
