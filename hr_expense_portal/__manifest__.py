9d07ac81e0d2cc153e5871d4c761f3c571594774
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2021-10-01 01:35:31 +0530

    [IMP] hr_expense_portal: black, isort, prettier

--- hr_expense_portal/__manifest__.py
+++ hr_expense_portal/__manifest__.py
@@ -2,20 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR Expense Portal',
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'category': 'Generic Modules/Human Resources',
-    'summary': "Show current expense in a website.",
+    "name": "HR Expense Portal",
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Human Resources",
+    "summary": "Show current expense in a website.",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'sale_expense', 'website'
+    "website": "https://github.com/OCA/hr",
+    "depends": ["sale_expense", "website"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "views/expense_template.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/security.xml',
-        'views/expense_template.xml'
-    ],
-    'installable': True,
+    "installable": True,
 }
