3e0736fbb2c4d1019b982576209400f982c1af3e
FernandoRomera <fromera@astrohotel.es>
2021-10-20 14:52:35 +0200

    [RFC] training plan planning ISO 9001

---
+++ hr_course/migrations/13.0.2.0.0/post-migrations.py
@@ -0,0 +1,10 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "hr_course", "migrations/13.0.2.0.0/noupdate_changes.xml"
+    )
