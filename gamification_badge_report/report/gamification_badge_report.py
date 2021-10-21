c4d7a66c1344d1c543ddac568259064fd0442cc3
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2021-10-22 00:14:01 +0530

    [MIG] gamification_badge_report : migrated to v13.0.

--- gamification_badge_report/report/gamification_badge_report.py
+++ gamification_badge_report/report/gamification_badge_report.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2020 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, tools
+from odoo import fields, models, tools
 
 
 class GamificationBadgeReport(models.Model):
+
     _name = "gamification.badge.report"
     _description = "Gamification Badge Report"
     _auto = False
@@ -13,21 +14,16 @@ class GamificationBadgeReport(models.Model):
     employee_id = fields.Many2one("hr.employee", string="Employee")
     total = fields.Integer(string="Total")
 
-    def _query(self):
-        # LAST 5 BADGES
-        query = """
-                select he.id as id, he.id as employee_id , count(gbu) as total
-                from gamification_badge_user as gbu
-                left join hr_employee as he on he.id = gbu.employee_id
-                group by he.id
-                order by total desc
-            """
-        return query
-
-    @api.model_cr
     def init(self):
-        # self._table = sale_report
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute(
-            """CREATE or REPLACE VIEW {} as ({})""".format(self._table, self._query())
+            """
+                CREATE OR REPLACE VIEW gamification_badge_report AS (
+                    select he.id as id, he.id as employee_id , count(gbu) as total
+                    from gamification_badge_user as gbu
+                    left join hr_employee as he on he.id = gbu.employee_id
+                    group by he.id
+                    order by total desc
+                )
+            """
         )
