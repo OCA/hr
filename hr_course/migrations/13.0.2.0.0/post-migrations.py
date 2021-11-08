4fa5ef5eab73b323424b44c194733ddb9a8191ab
FernandoRomera <fromera@astrohotel.es>
2021-11-08 13:59:01 +0100

    [FIX] to be sure that no we have no problem with the identifier sequence

--- hr_course/migrations/13.0.2.0.0/post-migrations.py
+++ hr_course/migrations/13.0.2.0.0/post-migrations.py
@@ -8,3 +8,19 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "hr_course", "migrations/13.0.2.0.0/noupdate_changes.xml"
     )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        SELECT setval('hr_course_schedule_id_seq', MAX(id))
+        FROM hr_course_schedule
+        """,
+    )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        SELECT setval('hr_course_id_seq', MAX(id))
+        FROM hr_course
+        """,
+    )
