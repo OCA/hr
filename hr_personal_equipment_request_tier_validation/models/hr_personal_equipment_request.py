b6578f9df2874ec0a1296ce7a046233b59b8fcdb
Alba Riera <alba.riera@creublanca.es>
2021-11-10 18:11:17 +0100

    [ADD] hr_personal_equipment_request_tier_validation

---
+++ hr_personal_equipment_request_tier_validation/models/hr_personal_equipment_request.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class HrPersonalEquipmentRequest(models.Model):
+
+    _name = "hr.personal.equipment.request"
+    _inherit = ["hr.personal.equipment.request", "tier.validation"]
+    _tier_validation_manual_config = False
+
+    _state_to = ["accepted"]
+    _cancel_state = "cancelled"
