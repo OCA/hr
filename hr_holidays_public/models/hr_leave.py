019f23ff6de069770216d23e29cad86102d68748
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-12-02 18:50:42 +0100

    [FIX] hr_holidays_public: Being multi-record aware in _compute_number_of_hours_display
    backport of 887fd4a6 from hr-holidays repo

--- hr_holidays_public/models/hr_leave.py
+++ hr_holidays_public/models/hr_leave.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2021 Tecnativa - Pedro M. Baeza
 # Copyright 2018 Brainbean Apps
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -25,10 +25,18 @@ class HrLeave(models.Model):
 
     @api.depends("number_of_days")
     def _compute_number_of_hours_display(self):
-        if self.holiday_status_id.exclude_public_holidays or not self.holiday_status_id:
-            instance = self.with_context(
-                exclude_public_holidays=True, employee_id=self.employee_id.id
+        """If the leave is validated, no call to `_get_number_of_days` is done, so we
+        need to inject the context here for including the public holidays if applicable.
+
+        For such cases, we need to serialize the call to super in fragments.
+        """
+        to_serialize = self.filtered(
+            lambda x: x.state == "validate"
+            and x.holiday_status_id.exclude_public_holidays
+        )
+        for leave in to_serialize:
+            leave = leave.with_context(
+                exclude_public_holidays=True, employee_id=leave.employee_id.id
             )
-        else:
-            instance = self
-        return super(HrLeave, instance)._compute_number_of_hours_display()
+            super(HrLeave, leave)._compute_number_of_hours_display()
+        return super(HrLeave, self - to_serialize)._compute_number_of_hours_display()
