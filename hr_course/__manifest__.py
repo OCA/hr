dbd90e000ddab164454fe05a923746d931b62012
aromera <aromera@pmsastro.es>
2022-01-18 12:46:46 +0100

    [IMP] hr_course: Add improvements to 14

--- hr_course/__manifest__.py
+++ hr_course/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "HR Course",
     "summary": """
         This module allows your to manage employee's training courses""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
