aafb49cc9ece9aab8a37f34f083239154cccff55
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-02-11 14:31:39 +0100

    [OU-FIX] hr_employee_calendar_planning: Proper field name

--- hr_employee_calendar_planning/migrations/13.0.1.3.0/post-migration.py
+++ hr_employee_calendar_planning/migrations/13.0.1.3.0/post-migration.py
@@ -7,6 +7,6 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     env["hr.employee"].with_context(active_test=False).search([]).filtered(
-        lambda x: any(c.calendar_id.two_weeks for c in x.calendar_ids)
-        and any(not c.calendar_id.two_weeks for c in x.calendar_ids)
+        lambda x: any(c.calendar_id.two_weeks_calendar for c in x.calendar_ids)
+        and any(not c.calendar_id.two_weeks_calendar for c in x.calendar_ids)
     ).regenerate_calendar()
