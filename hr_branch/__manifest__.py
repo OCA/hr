c6d1af6bea2d7b6feb176769ed4a71cd6f48cb1c
Yaseen El-Tahir <eltahiryaseen@outlook.com>
2022-03-14 11:27:52 +0000

    init

---
+++ hr_branch/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "HR Branch",
+    "summary": "Allow define company branch for employee process",
+    "version": "14.0.1.0.0",
+    "development_status": "Mature",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "maintainers": ["luistorresm"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["hr"],
+    "data": ["views/hr_department_views.xml", "views/hr_employee_views.xml"],
+    "demo": [],
+    "qweb": [],
+}
---
+++ hr_contract_currency/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "HR Contract Currency",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "CorporateHub, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "summary": "Employee's contract currency",
+    "depends": ["hr_contract"],
+    "data": ["views/hr_contract.xml"],
+}
