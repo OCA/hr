f61ea225ea3f20701bc73381562d7cc7275d25c8
Olga Marco <olga.marco@creublanca.es>
2022-05-12 12:35:05 +0200

    [MIG] hr_personal_equipment_request: Migration to 15.0

--- hr_personal_equipment_request/__manifest__.py
+++ hr_personal_equipment_request/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Hr Personal Equipment Request",
     "summary": """
         This addon allows to manage employee personal equipment""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
