f1ce6e2db189218a4b0a184f5aefce92041c3539
Alba Riera <alba.riera@creublanca.es>
2022-05-13 13:09:28 +0200

    [MIG] hr_employee_service_contract: Migration to 14.0
    [UPD] Update hr_employee_service_contract.pot
    
    [UPD] README.rst

--- hr_employee_service_contract/__manifest__.py
+++ hr_employee_service_contract/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Employee Service from Contracts",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "CorporateHub, Odoo Community Association (OCA)",
