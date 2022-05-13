9a6037dcc5b8cea6877d799b1c02d9abc9a9a587
Alba Riera <alba.riera@creublanca.es>
2022-05-13 09:18:16 +0200

    [MIG] hr_employee_partner_external: Migration to 14.0

--- hr_employee_partner_external/__manifest__.py
+++ hr_employee_partner_external/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Employee external Partner",
     "summary": "Associate an external Partner to Employee",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "author": "Stefano Consolaro Associazione PNLUG - Gruppo Odoo, "
     "Odoo Community Association (OCA)",
