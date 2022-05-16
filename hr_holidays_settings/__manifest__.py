f8a075a6ebfa776ef0d017f0d7e608b0fce9029f
Juany Davila <juanyadavilap@gmail.com>
2022-05-16 15:24:45 -0500

    [MIG] hr_holidays_settings: Migration to 14.0

--- hr_holidays_settings/__manifest__.py
+++ hr_holidays_settings/__manifest__.py
@@ -3,10 +3,10 @@
 {
     "name": "HR Holidays Settings",
     "summary": "Enables Settings Form for HR Holidays.",
-    "version": "12.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["hr_holidays"],
     "data": ["views/res_config_settings.xml"],
