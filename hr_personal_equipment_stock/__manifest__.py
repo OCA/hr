ee6f460c588c55fb0d205ed4bfc51c296d542a01
Alba Riera <alba.riera@creublanca.es>
2022-05-17 12:27:51 +0200

    [MIG] hr_personal_equipment_stock: Migration to 14.0

--- hr_personal_equipment_stock/__manifest__.py
+++ hr_personal_equipment_stock/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Hr Personal Equipment Stock",
     "summary": """
         This addon allows to integrate hr_personal_equipment_request with stock""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
