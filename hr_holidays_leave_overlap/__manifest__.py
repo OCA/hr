a5b72934cd8fc4ca04b4dc326383ae865ed9b907
Sulivan Lominchar <slominchar@gmail.com>
2022-06-21 12:34:03 -0400

    [15.0] MIG hr_holidays_leave_overlap: Migration to 15.0

--- hr_holidays_leave_overlap/__manifest__.py
+++ hr_holidays_leave_overlap/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR - Holiday Leaves Overlap',
-    'summary': 'Handle leaves overlap',
-    'author': 'Onestein, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/hr/tree/11.0',
-    'category': 'Human Resources',
-    'version': '11.0.1.0.0',
-    'depends': [
-        'hr_holidays',
+    "name": "HR - Holiday Leaves Overlap",
+    "summary": "Handle leaves overlap",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/hr",
+    "category": "Human Resources",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "hr_holidays",
     ],
-    'installable': True,
+    "installable": True,
 }
