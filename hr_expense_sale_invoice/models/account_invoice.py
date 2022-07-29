6fb1b0bb3afece9ad94df0cb94611921f120aae5
Murtaza Mithaiwala <murtaza.m.serpentcs@gmail.com>
2022-07-29 15:17:03 +0530

    [15.0][FIX] Pre-commit

--- hr_expense_sale_invoice/models/account_invoice.py
+++ hr_expense_sale_invoice/models/account_invoice.py
@@ -7,8 +7,7 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     expense_ids = fields.One2many(
-        "hr.expense", "expense_invoice_id",
-        string="Expenses", readonly=True, copy=False
+        "hr.expense", "expense_invoice_id", string="Expenses", readonly=True, copy=False
     )
     expense_count = fields.Integer(
         "Number of expenses", compute="_compute_expense_count"
@@ -22,12 +21,10 @@ class AccountInvoice(models.Model):
             ["expense_invoice_id"],
             ["expense_invoice_id"],
         )
-        mapped_data = dict(
-            [
-                (t["expense_invoice_id"][0], t["expense_invoice_id_count"])
-                for t in expense_data
-            ]
-        )
+        mapped_data = {
+            t["expense_invoice_id"][0]: t["expense_invoice_id_count"]
+            for t in expense_data
+        }
         for invoice in self:
             invoice.expense_count = mapped_data.get(invoice.id, 0)
 
@@ -45,13 +42,18 @@ class AccountInvoiceLine(models.Model):
             sale_line_delivery = invoice_line.sale_line_ids.filtered(
                 lambda sol: sol.product_id.invoice_policy == "delivery"
                 and sol.product_id.expense_policy == "cost"
-                and sol.product_id.type == 'service'
+                and sol.product_id.type == "service"
             )
             if sale_line_delivery:
-                expenses = self.env['hr.expense'].search(
-                    [('sale_order_id', '=', sale_line_delivery.order_id.id),
-                     ('expense_invoice_id', '=', False)])
-                expenses.write({
-                    'expense_invoice_id': invoice_line.invoice_id.id,
-                })
+                expenses = self.env["hr.expense"].search(
+                    [
+                        ("sale_order_id", "=", sale_line_delivery.order_id.id),
+                        ("expense_invoice_id", "=", False),
+                    ]
+                )
+                expenses.write(
+                    {
+                        "expense_invoice_id": invoice_line.invoice_id.id,
+                    }
+                )
         return invoice_line
--- hr_expense_sale_invoice/models/hr_expense.py
+++ hr_expense_sale_invoice/models/hr_expense.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2020 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class HrExpense(models.Model):
