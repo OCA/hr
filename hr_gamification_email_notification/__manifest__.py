627e4481cafc6b8c3daddb2c2ba02c634cc4d0b3
Hardik-OSI <hsuthar@opensourceintegrators.com>
2022-08-10 22:14:56 +0530

    [15.0][MIG] hr_gamification_email_notification

--- hr_gamification_email_notification/__manifest__.py
+++ hr_gamification_email_notification/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 - TODAY, Open Source Integrators
-# Copyright (C) 2020 Serpent Consulting Services
+# Copyright (C) 2022 - TODAY, Open Source Integrators
+# Copyright (C) 2022 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "HR Gamification Email Notification",
-    "version": "12.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": """Open Source Integrators,
         Serpent Consulting Services,
@@ -12,17 +12,13 @@
     "summary": """Send Email to all the Employees with all the award given in
      the last week.""",
     "category": "Human Resources",
-    "maintainers": ["Khalid-SerpentCS"],
+    "maintainers": ["Hardik-OSI"],
     "website": "https://github.com/OCA/hr",
-    "depends": ["hr_gamification"],
-    "data": [
-        "data/ir_cron_data.xml",
-        "data/mail_template.xml",
-        "views/badge.xml"
-    ],
+    "depends": ["hr_gamification", "gamification"],
+    "data": ["data/ir_cron_data.xml", "data/mail_template.xml", "views/badge.xml"],
     "qweb": [],
     "auto_install": False,
     "application": False,
     "installable": True,
-    "development_status": "Stable",
+    "development_status": "Production/Stable",
 }
--- hr_gamification_email_notification/models/mail_channel.py
+++ hr_gamification_email_notification/models/mail_channel.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2020 Open Source Integrators
-# Copyright (C) 2020 Serpent Consulting Services
+# Copyright (C) 2022 Open Source Integrators
+# Copyright (C) 2022 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import timedelta
@@ -9,31 +9,32 @@ from odoo import api, fields, models
 
 class MailChannel(models.Model):
 
-    _inherit = 'mail.channel'
+    _inherit = "mail.channel"
 
     @api.model
     def _send_badge_awards_emails(self):
-        channel_general = self.env['mail.channel'].search(
-            [('name', '=', 'general')], limit=1)
+        channel_general = self.env["mail.channel"].search(
+            [("name", "=", "general")], limit=1
+        )
         partner_ids = []
         for partner_id in channel_general.channel_last_seen_partner_ids:
-            partner_ids.append(str(partner_id.partner_id.id))
-        partner_ids = ','.join(partner_ids)
+            partner_ids.append(partner_id.partner_id.id)
         template = self.env.ref(
-            'hr_gamification_email_notification.'
-            'email_template_badge_weekly_awards')
+            "hr_gamification_email_notification." "email_template_badge_weekly_awards"
+        )
         date_before_aweek = fields.Date.today() - timedelta(days=7)
-        badge_ids = self.env['gamification.badge.user'].search(
-            [('create_date', '<', fields.Date.today()),
-             ('create_date', '>=', date_before_aweek)])
+        badge_ids = self.env["gamification.badge.user"].search(
+            [
+                ("create_date", "<", fields.Date.today()),
+                ("create_date", ">=", date_before_aweek),
+            ]
+        )
         if badge_ids:
-            channel_general.with_context(
-                {'badge_ids': badge_ids,
-                 'partner_ids': partner_ids}
-            ).message_post_with_template(
-                template.id,
-                model=self._name,
-                composition_mode='mass_mail'
-            )
+            for partner in partner_ids:
+                channel_general.with_context(
+                    **{"badge_ids": badge_ids, "partner_ids": partner}
+                ).message_post_with_template(
+                    template.id, model=self._name, composition_mode="mass_mail"
+                )
 
         return True
