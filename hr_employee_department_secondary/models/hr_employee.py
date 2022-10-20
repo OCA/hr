a1e29546d5b275b96db757e5c0e6c0fa4679d98c
Xavier Terés <66311413+xavierteres@users.noreply.github.com>
2022-10-20 11:28:41 +0200

    Rename departments_ids to secondary_departments_ids and add restriction

--- hr_employee_department_secondary/models/hr_employee.py
+++ hr_employee_department_secondary/models/hr_employee.py
@@ -1,14 +1,22 @@
-from odoo import fields, models
+from odoo import api, exceptions, fields, models, _
 
 
 class Employee(models.Model):
     _inherit = ['hr.employee']
 
-    department_ids = fields.Many2many(
+    secondary_department_ids = fields.Many2many(
         'hr.department', 'employee_department_rel',
         'employee_id', 'department_id',
-        string='Departments')
+        domain="[('id', '!=', department_id)]",
+        string='Secondary departments')
 
+    @api.constrains('department_id', 'secondary_department_ids')
+    def _check_departments(self):
+        for employee in self:
+            if employee.department_id in employee.secondary_department_ids:
+                raise exceptions.ValidationError(
+                    _('The main department must be different '
+                      'from the secondary departments.'))
 
 class Department(models.Model):
     _inherit = ['hr.department']
