cbca535a1c17064f462f4b0f8da2726ea5ef545a
TheerayutEncoder <theerayutattajak@gmail.com>
2022-11-23 13:49:23 +0700

    [MIG] hr_department_code: Migration to 15.0

--- hr_department_code/tests/test_hr_department_code.py
+++ hr_department_code/tests/test_hr_department_code.py
@@ -1,10 +1,10 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestHrDepartmentCode(SavepointCase):
+class TestHrDepartmentCode(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
