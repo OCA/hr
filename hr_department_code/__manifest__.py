4467bc11d40767cfee32618a5a20638367d26dd9
TheerayutEncoder <theerayutattajak@gmail.com>
2022-11-23 13:49:23 +0700

    [MIG] hr_department_code: Migration to 15.0

--- hr_department_code/__manifest__.py
+++ hr_department_code/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR department code",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "category": "Human Resources",
     "author": "Ecosoft, Odoo Community Association (OCA)",
