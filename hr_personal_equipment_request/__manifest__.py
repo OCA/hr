0f5ddbd52655c28fdbf96c804867043f5690779a
FernandoRomera <fromera@astrohotel.es>
2022-12-20 12:51:20 +0100

    [16.0][MIG] hr_personal_equipment_request: Migration to 16.0

--- hr_personal_equipment_request/__manifest__.py
+++ hr_personal_equipment_request/__manifest__.py
@@ -5,17 +5,17 @@
     "name": "Hr Personal Equipment Request",
     "summary": """
         This addon allows to manage employee personal equipment""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "Creu Blanca, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
     "depends": ["product", "hr", "mail"],
     "data": [
         "security/hr_personal_equipment_request_security.xml",
         "security/ir.model.access.csv",
-        "views/product_template.xml",
-        "views/hr_personal_equipment.xml",
-        "views/hr_personal_equipment_request.xml",
-        "views/hr_employee.xml",
+        "views/product_template_views.xml",
+        "views/hr_personal_equipment_views.xml",
+        "views/hr_personal_equipment_request_views.xml",
+        "views/hr_employee_views.xml",
     ],
 }
