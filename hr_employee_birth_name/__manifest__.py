9c5dce0d97e09579e7d68ea7647419c7b9f81a00
BT-anieto <alberto.nieto@braintec.com>
2022-12-27 08:22:57 +0100

    [16.0][MIG] hr_employee_birth_name: Migration to 16.0.

--- hr_employee_birth_name/__manifest__.py
+++ hr_employee_birth_name/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Employee Birth Name",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Human Resources",
     "author": "Camptocamp SA,Odoo Community Association (OCA),"
     "Open Source Integrators",
