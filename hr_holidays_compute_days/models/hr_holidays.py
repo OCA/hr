1c7d62c15fe44a0c9ef52d97983fe51c8414bda6
Juany Davila <juanyadavilap@gmail.com>
2023-02-24 20:10:24 -0500

    [IMP] hr_holidays_compute_days: make half days not true by default

--- hr_holidays_compute_days/models/hr_holidays.py
+++ hr_holidays_compute_days/models/hr_holidays.py
@@ -10,32 +10,53 @@ from dateutil import tz
 class HrHolidays(models.Model):
     _inherit = 'hr.holidays'
 
-    def _default_from_full_day(self):
-        """Detect if we have select full day in calendar and return the
-        full day option accordingly.
-        """
-        context = self.env.context
-        if context.get('default_date_from'):
-            dt = fields.Datetime.from_string(
-                context['default_date_from']
-            ).replace(tzinfo=tz.gettz(self.env.user.tz)).astimezone(tz.tzutc())
-            return dt.hour == 5 and dt.minute == 0 and dt.second == 0
-        return False
+    from_half_day_am_pm = fields.Selection(
+        selection=[('am', 'AM'),
+                   ('pm', 'PM'), ],
+        string='AM/PM',
+        required=False,
+        readonly=True,
+        copy=False,
+        states={
+            'draft': [('readonly', False)],
+            'confirm': [('readonly', False)]
+        },
+        default='am',
+        help="If this the start date is half date please choose AM/PM.")
 
-    def _default_to_full_day(self):
-        """Detect if we have select full day in calendar and return the
-        full day option accordingly.
-        """
-        context = self.env.context
-        if context.get('default_date_to'):
-            dt = fields.Datetime.from_string(
-                context['default_date_to']
-            ).replace(tzinfo=tz.gettz(self.env.user.tz)).astimezone(tz.tzutc())
-            return dt.hour == 17 and dt.minute == 0 and dt.second == 0
-        return False
+    to_half_day_am_pm = fields.Selection(
+        selection=[('am', 'AM'),
+                   ('pm', 'PM'), ],
+        string='AM/PM',
+        required=False,
+        readonly=True,
+        copy=False,
+        default='am',
+        states={
+            'draft': [('readonly', False)],
+            'confirm': [('readonly', False)]
+        },
+        help="If this the end date is half date please choose AM/PM.")
+
+    from_half_day = fields.Boolean(
+        readonly=True,
+        copy=False,
+        states={
+            'draft': [('readonly', False)],
+            'confirm': [('readonly', False)]
+        },
+    )
+    to_half_day = fields.Boolean(
+        readonly=True,
+        copy=False,
+        states={
+            'draft': [('readonly', False)],
+            'confirm': [('readonly', False)]
+        },
+    )
 
     from_full_day = fields.Boolean(
-        default=lambda self: self._default_from_full_day(),
+        default=True,
         readonly=True,
         copy=False,
         states={
@@ -44,7 +65,7 @@ class HrHolidays(models.Model):
         },
     )
     to_full_day = fields.Boolean(
-        default=lambda self: self._default_to_full_day(),
+        default=True,
         readonly=True,
         copy=False,
         states={
@@ -85,6 +106,26 @@ class HrHolidays(models.Model):
             ).astimezone(tz.gettz(tz_name)).date()
             record.date_from_full = fields.Date.to_string(dt)
 
+    @api.depends('date_from')
+    def _compute_date_from_half(self):
+        """Put day in employee's user timezone, or user timezone as fallback"""
+        for record in self.filtered('date_from'):
+            tz_name = record.employee_id.user_id.tz or record.env.user.tz
+            dt = fields.Datetime.from_string(record.date_from).replace(
+                tzinfo=tz.tzutc(),
+            ).astimezone(tz.gettz(tz_name)).date()
+            record.date_from_half = fields.Date.to_string(dt)
+
+    @api.depends('date_to')
+    def _compute_date_to_half(self):
+        """Put day in employee's user timezone, or user timezone as fallback"""
+        for record in self.filtered('date_to'):
+            tz_name = record.employee_id.user_id.tz or record.env.user.tz
+            dt = fields.Datetime.from_string(record.date_to).replace(
+                tzinfo=tz.tzutc(),
+            ).astimezone(tz.gettz(tz_name)).date()
+            record.date_to_half = fields.Date.to_string(dt)
+
     @api.depends('date_to')
     def _compute_date_to_full(self):
         """Put day in employee's user timezone, or user timezone as fallback"""
@@ -99,7 +140,7 @@ class HrHolidays(models.Model):
         """Put start of the day in employee's user timezone, or user timezone
         as fallback.
         """
-        for record in self.filtered('from_full_day'):
+        for record in self.filtered(lambda r: not r.from_half_day):
             if record.date_from_full:
                 tz_name = record.employee_id.user_id.tz or record.env.user.tz
                 dt = fields.Datetime.from_string(record.date_from_full).replace(
@@ -107,12 +148,27 @@ class HrHolidays(models.Model):
                     tzinfo=tz.gettz(tz_name),
                 ).astimezone(tz.tzutc())
                 record.date_from = fields.Datetime.to_string(dt)
+        for record in self.filtered('from_half_day'):
+            tz_name = record.employee_id.user_id.tz or record.env.user.tz
+            if not record.date_from_full:
+                record.date_from_full = fields.Date.context_today(self)
+            if record.from_half_day_am_pm == "am":
+                dt = fields.Datetime.from_string(record.date_from_full).replace(
+                    hour=8, minute=0, second=0, microsecond=0,
+                    tzinfo=tz.gettz(tz_name),
+                ).astimezone(tz.tzutc())
+            else:
+                dt = fields.Datetime.from_string(record.date_from_full).replace(
+                    hour=12, minute=0, second=0, microsecond=0,
+                    tzinfo=tz.gettz(tz_name),
+                ).astimezone(tz.tzutc())
+            record.date_from = fields.Datetime.to_string(dt)
 
     def _inverse_date_to_full(self):
         """Put end of the day in employee's user timezone, or user timezone
         as fallback.
         """
-        for record in self.filtered('to_full_day'):
+        for record in self.filtered(lambda r: not r.to_half_day):
             if record.date_to_full:
                 tz_name = record.employee_id.user_id.tz or record.env.user.tz
                 dt = fields.Datetime.from_string(record.date_to_full).replace(
@@ -120,6 +176,21 @@ class HrHolidays(models.Model):
                     tzinfo=tz.gettz(tz_name),
                 ).astimezone(tz.tzutc())
                 record.date_to = fields.Datetime.to_string(dt)
+        for record in self.filtered('to_half_day'):
+            tz_name = record.employee_id.user_id.tz or record.env.user.tz
+            if not record.date_to_full:
+                record.date_to_full = fields.Date.context_today(self)
+            if record.from_half_day_am_pm == "am":
+                dt = fields.Datetime.from_string(record.date_to_full).replace(
+                    hour=12, minute=0, second=0, microsecond=0,
+                    tzinfo=tz.gettz(tz_name),
+                ).astimezone(tz.tzutc())
+            else:
+                dt = fields.Datetime.from_string(record.date_to_full).replace(
+                    hour=16, minute=0, second=0, microsecond=0,
+                    tzinfo=tz.gettz(tz_name),
+                ).astimezone(tz.tzutc())
+            record.date_to = fields.Datetime.to_string(dt)
 
     @api.onchange('date_from_full', 'from_full_day')
     def _onchange_date_from_full(self):
@@ -145,6 +216,30 @@ class HrHolidays(models.Model):
         self._onchange_data_hr_holidays_compute_days()
         return res
 
+    @api.onchange('from_half_day')
+    def _onchange_from_half_day(self):
+        """Or half or full, no other option for simplicity"""
+        if not self.from_half_day:
+            self.from_full_day = True
+        self.from_full_day = False
+        self._inverse_date_from_full()
+        # if the leave if for one day just put half day in the to date as well
+        # if not self.date_to:
+        #    self.to_half_day = True
+        self._onchange_data_hr_holidays_compute_days()
+
+    @api.onchange('to_half_day')
+    def _onchange_to_half_day(self):
+        """Or half or full, no other option for simplicity"""
+        if not self.to_half_day:
+            self.to_full_day = True
+        self.to_full_day = False
+        self._inverse_date_to_full()
+        # if the leave if for one day just put half day in the to date as well
+        # if not self.date_from:
+        #    self.from_half_day = True
+        self._onchange_data_hr_holidays_compute_days()
+
     @api.onchange('employee_id', 'holiday_status_id')
     def _onchange_data_hr_holidays_compute_days(self):
         if self.date_to and self.date_from and self.date_from <= self.date_to:
@@ -156,11 +251,12 @@ class HrHolidays(models.Model):
                 return
             employee = self.employee_id
             if (self.holiday_status_id.exclude_public_holidays or
-                    not self.holiday_status_id):
+                not self.holiday_status_id):
                 employee = employee.with_context(exclude_public_holidays=True)
             employee = employee.with_context(
                 include_rest_days=not self.holiday_status_id.exclude_rest_days,
                 compute_full_days=self.holiday_status_id.compute_full_days,
+                current_leave=self
             )
             days = employee.get_work_days_count(
                 from_datetime=date_from, to_datetime=date_to,
