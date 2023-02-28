c58412218549c55243d2988359b6dc8e13f4ccc3
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-28 11:04:23 +0100

    [IMP] hr_course: Remove the readonly=True attribute to be able to use the permanence fields.

--- hr_course/models/hr_course.py
+++ hr_course/models/hr_course.py
@@ -15,17 +15,8 @@ class HrCourse(models.Model):
         "hr.course.category", string="Category", required=True
     )
 
-    permanence = fields.Boolean(
-        string="Has Permanence",
-        readonly=True,
-        default=False,
-        tracking=True,
-    )
-    permanence_time = fields.Char(
-        string="Permanence time",
-        readonly=True,
-        tracking=True,
-    )
+    permanence = fields.Boolean(string="Has Permanence", default=False, tracking=True)
+    permanence_time = fields.Char(string="Permanence time", tracking=True)
 
     content = fields.Html()
     objective = fields.Html()
