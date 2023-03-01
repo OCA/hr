bca9b945eee870063a72ee4068dd0c19ce53f8d5
SIN <eltahiryaseen@outlook.com>
2023-03-01 11:09:27 +0000

    [MIG] hr_contract_multi_job: Migration to 15.0

--- hr_contract_multi_job/__manifest__.py
+++ hr_contract_multi_job/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Contract Multi Jobs",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "category": "Generic Modules/Human Resources",
     "author": "Savoir-faire Linux, "
