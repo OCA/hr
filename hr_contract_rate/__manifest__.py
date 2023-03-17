e10f3d4e876835da42418ae7064b1ceb3c73a6fd
nicolasrsande <nicolasrsande@gmail.com>
2023-03-17 17:53:47 -0300

    [14.0] [MIG] hr_contract_rate: Update version in manifest

--- hr_contract_rate/__manifest__.py
+++ hr_contract_rate/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Contract Rate",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "CorporateHub, " "Odoo Community Association (OCA)",
