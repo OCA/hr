619489c633be83b830b828f8f9ed1fae99aaf9dd
Janik von Rotz <login@janikvonrotz.ch>
2023-04-07 09:00:41 +0200

    [hr_work_entry_profile] init

---
+++ hr_work_entry_profile/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "HR Work Entry Profile",
+    "summary": """
+        User can access their work entries from the profile view.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_work_entry"],
+    "data": ["views/res_users.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
