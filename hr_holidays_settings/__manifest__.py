9819e4d9ce9cc3d91db60ff48dfe0512be0c8198
Jordi Terol <jterol@ontinet.com>
2023-06-05 15:53:10 +0200

    [MIG] hr_holidays_settings: Migration to 16.0

--- hr_holidays_settings/__manifest__.py
+++ hr_holidays_settings/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "HR Holidays Settings",
     "summary": "Enables Settings Form for HR Holidays.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
