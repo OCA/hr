045dde510f7c40088bde11826aebf02c1a37a183
Víctor Martínez <victor.martinez@tecnativa.com>
2023-07-04 08:12:08 +0200

    [FIX] hr_employee_calendar_planning: Add ondelete to employee_id and calendar_id fields to allow removal
    TT44093

--- hr_employee_calendar_planning/models/hr_employee.py
+++ hr_employee_calendar_planning/models/hr_employee.py
@@ -155,7 +155,7 @@ class HrEmployeeCalendar(models.Model):
     date_start = fields.Date(string="Start Date",)
     date_end = fields.Date(string="End Date",)
     employee_id = fields.Many2one(
-        comodel_name="hr.employee", string="Employee", required=True,
+        comodel_name="hr.employee", string="Employee", required=True, ondelete="cascade"
     )
     company_id = fields.Many2one(related="employee_id.company_id")
     calendar_id = fields.Many2one(
@@ -163,6 +163,7 @@ class HrEmployeeCalendar(models.Model):
         string="Working Time",
         required=True,
         check_company=True,
+        ondelete="restrict",
     )
 
     _sql_constraints = [
