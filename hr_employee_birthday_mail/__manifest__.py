2f311cb72989e46daeb53a94aaf7f5dfdcee1079
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-07-10 10:40:46 +0200

    [FIX] hr_employee_birthay_mail: res_users configurable

--- hr_employee_birthday_mail/__manifest__.py
+++ hr_employee_birthday_mail/__manifest__.py
@@ -9,7 +9,12 @@
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
     "depends": ["hr", "mail"],
-    "data": ["data/data.xml", "data/ir_cron.xml", "views/hr_employee_views.xml"],
+    "data": [
+        "data/data.xml",
+        "data/ir_cron.xml",
+        "views/hr_employee_views.xml",
+        "views/res_user_views.xml",
+    ],
     "installable": True,
     "application": False,
     "auto_install": False,
