df255f42cbdfb5e9f81413190db21eea2e7e57e4
Víctor Martínez <victor.martinez@tecnativa.com>
2023-07-13 13:52:40 +0200

    [IMP] hr_employee_age: Add groups="hr.group_hr_user" to age field according to birthday field definition
    TT43046

--- hr_employee_age/models/hr_employee.py
+++ hr_employee_age/models/hr_employee.py
@@ -8,7 +8,11 @@ from odoo import api, fields, models
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    age = fields.Integer(compute="_compute_age")
+    # The age field uses a depends (birthday) that has defined
+    # groups="hr.group_hr_user", if a user without permissions in HR tries to get
+    # the value of this field will have an error.
+    # The correct way to avoid this inconsistency is to define groups to field age
+    age = fields.Integer(compute="_compute_age", groups="hr.group_hr_user")
 
     @api.depends("birthday")
     def _compute_age(self):
