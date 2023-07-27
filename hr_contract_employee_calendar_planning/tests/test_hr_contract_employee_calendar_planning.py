128de13421438aa1cfccd7833829cf1e604a37fe
jb <J.Buchholz@cibex.net>
2023-07-27 14:01:18 +0200

    [IMP] hr_contract_employee_calendar_planning: test post_init_hook by creating contracts with different work hours

--- hr_contract_employee_calendar_planning/tests/test_hr_contract_employee_calendar_planning.py
+++ hr_contract_employee_calendar_planning/tests/test_hr_contract_employee_calendar_planning.py
@@ -6,12 +6,10 @@ from ..hooks import post_init_hook
 
 
 class TestHrContractEmployeeCalendarPlanning(TestContractCommon):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.env = cls.env(
+    def test_calendar_migration_from_contracts(self):
+        self.env = self.env(
             context=dict(
-                cls.env.context,
+                self.env.context,
                 mail_create_nolog=True,
                 mail_create_nosubscribe=True,
                 mail_notrack=True,
@@ -19,54 +17,47 @@ class TestHrContractEmployeeCalendarPlanning(TestContractCommon):
                 tracking_disable=True,
             )
         )
-        calendar_ids = [
-            (
-                0,
-                0,
-                {
-                    "date_start": False,
-                    "date_end": datetime.strptime("2020-11-30", "%Y-%m-%d").date(),
-                    "calendar_id": cls.env["resource.calendar"].browse([2]).id,
-                },
-            ),
-            (
-                0,
-                0,
-                {
-                    "date_start": datetime.strptime("2020-12-01", "%Y-%m-%d").date(),
-                    "date_end": False,
-                    "calendar_id": cls.env["resource.calendar"].browse([1]).id,
-                },
-            ),
-        ]
-        cls.employee.calendar_ids = calendar_ids
 
-    def test_calendar_migration_from_contracts(self):
-        self.contract1 = self.env["hr.contract"].create(
+        # newly created contracts get the same calendar as the employee
+        self.employee.resource_calendar_id = self.env["resource.calendar"].browse([1])
+        self.env["hr.contract"].create(
             {
                 "name": "contract1",
-                "employee_id": self.employee.id,
                 "wage": 1,
                 "state": "close",
-                "kanban_state": "normal",
-                "resource_calendar_id": self.env["resource.calendar"].browse([1]).id,
+                "employee_id": self.employee.id,
                 "date_start": datetime.strptime("2018-11-30", "%Y-%m-%d").date(),
                 "date_end": datetime.strptime("2019-11-30", "%Y-%m-%d").date(),
             }
         )
-        self.contract2 = self.env["hr.contract"].create(
+        self.employee.resource_calendar_id = self.env["resource.calendar"].browse([2])
+        self.env["hr.contract"].create(
             {
                 "name": "contract2",
-                "employee_id": self.employee.id,
                 "wage": 1,
                 "state": "open",
-                "kanban_state": "normal",
-                "resource_calendar_id": self.env["resource.calendar"].browse([2]).id,
+                "employee_id": self.employee.id,
                 "date_start": datetime.strptime("2019-12-01", "%Y-%m-%d").date(),
                 "date_end": datetime.strptime("2020-11-30", "%Y-%m-%d").date(),
             }
         )
-        original_cal_ids = self.employee.calendar_ids.ids
+        calendar_ids = self.env["hr.employee.calendar"].create(
+            [
+                {
+                    "date_start": False,
+                    "date_end": datetime.strptime("2020-11-30", "%Y-%m-%d").date(),
+                    "calendar_id": 2,
+                    "employee_id": self.employee.id,
+                },
+                {
+                    "date_start": datetime.strptime("2020-12-01", "%Y-%m-%d").date(),
+                    "date_end": False,
+                    "calendar_id": 1,
+                    "employee_id": self.employee.id,
+                },
+            ]
+        )
+        self.employee.calendar_ids = [(6, 0, calendar_ids.ids)]
         start_dt = datetime(2019, 1, 1, 0, 0, 0)
         end_dt = datetime(2019, 1, 2, 0, 0, 0)
         self.assertEqual(
@@ -85,6 +76,4 @@ class TestHrContractEmployeeCalendarPlanning(TestContractCommon):
                 end_dt=end_dt,
             ),
         )
-        self.assertTrue(
-            all(ids in self.employee.calendar_ids.ids for ids in original_cal_ids)
-        )
+        self.assertTrue(calendar_ids.ids < self.employee.calendar_ids.ids)
