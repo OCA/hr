f5aa3ef4dff775418eac40b9290ddac7489af25c
heliaktiv <heli.aktiv@gmail.com>
2023-09-14 16:13:37 +0530

    [IMP] hr_employee_display_own_info: black, isort, prettier

--- hr_employee_display_own_info/__manifest__.py
+++ hr_employee_display_own_info/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Employee own info',
-    'summary': 'Employee can see own information',
-    'author': 'Onestein, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/hr/',
-    'category': 'Human Resources',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'hr',
+    "name": "Employee own info",
+    "summary": "Employee can see own information",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/hr",
+    "category": "Human Resources",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "hr",
     ],
-    'data': [
-        'views/hr_employee.xml',
+    "data": [
+        "views/hr_employee.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
