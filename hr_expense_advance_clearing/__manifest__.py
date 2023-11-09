ded2950f3a205cf186a12fec636e71a61bc3946f
Rodrigo Neves Trindade <rodrigonevest@gmail.com>
2023-11-09 16:25:39 -0300

    [IMP] hr_expense_advance_clearing: black, isort, prettier

--- hr_expense_advance_clearing/__manifest__.py
+++ hr_expense_advance_clearing/__manifest__.py
@@ -2,20 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Employee Advance and Clearing',
-    'version': '12.0.1.3.0',
-    'category': 'Human Resources',
-    'author': 'Ecosoft, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr_expense',
+    "name": "Employee Advance and Clearing",
+    "version": "12.0.1.3.0",
+    "category": "Human Resources",
+    "author": "Ecosoft, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/hr",
+    "depends": [
+        "hr_expense",
     ],
-    'data': [
-        'data/advance_product.xml',
-        'views/hr_expense_views.xml',
+    "data": [
+        "data/advance_product.xml",
+        "views/hr_expense_views.xml",
     ],
-    'installable': True,
-    'maintainers': ['kittiu'],
+    "installable": True,
+    "maintainers": ["kittiu"],
 }
