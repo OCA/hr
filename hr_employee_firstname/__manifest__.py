525bee25cddb586aa7901557be6e30da8855438d
Andrea Gidalti <andreag@vauxoo.com>
2023-11-14 17:35:13 +0000

    [MIG] hr_employee_firstname: Migration to 17.0
    - Update definition of required modifier as part of [1].
    - Update incoming parameters of post_init_hook method since `cr, registry`
      were replaced by `env` in [2].
    - Deprecate the use of _onchange_spec method and use the method new method
      _get_fields_spec to return the fields specification from a view
      description since it was introduced in [3].
    
    [1] https://github.com/odoo/odoo/pull/104741
    [2] https://github.com/odoo/odoo/commit/b4a7996
    [3] https://github.com/odoo/odoo/commit/f5e6494

--- hr_employee_firstname/__manifest__.py
+++ hr_employee_firstname/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR Employee First Name, Last Name",
-    "version": "16.0.1.0.2",
+    "version": "17.0.1.0.0",
     "author": "Savoir-faire Linux, "
     "Fekete Mihai (Forest and Biomass Services Romania), "
     "Onestein, "
