77f9dd7dd70ffedf633e1c4b0db69f57b552614e
Manish Bohra <manishkumarbohra@outlook.com>
2023-11-14 11:27:21 +0530

    [IMP] hr_employee_birth_name: pre-commit auto fixes

--- hr_employee_birth_name/__manifest__.py
+++ hr_employee_birth_name/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Employee Birth Name",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Human Resources",
     "author": "Camptocamp SA,Odoo Community Association (OCA),"
     "Open Source Integrators",
--- hr_employee_birth_name/readme/DESCRIPTION.md
+++ hr_employee_birth_name/readme/DESCRIPTION.md
@@ -1,3 +1,3 @@
 This module adds the following to employees :
 
-* hr_employee_birth_name
+- hr_employee_birth_name
