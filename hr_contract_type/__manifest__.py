191eb7f46c9e33b08c72acce3d0ed3f58ffbf4c5
Michael Telahun <mtm@trevi.et>
2023-11-26 10:44:36 +0300

    Migration to 16.0

--- hr_contract_type/__manifest__.py
+++ hr_contract_type/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Hr Contract Type",
     "summary": """
         Add a Type for Contracts""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
