2f33af8809b0d41767f1f29996ffc8908f33146a
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-03-01 08:37:50 -0600

    [FIX] hr_recruitment_torecruit

--- hr_recruitment_torecruit/models/hr_job.py
+++ hr_recruitment_torecruit/models/hr_job.py
@@ -11,8 +11,8 @@ class HrJob(models.Model):
                 [("job_id", "=", rec.id), ("state", "=", "open")]
             )
             if (rec.no_of_recruitment - rec.to_recruit) > 0:
-                rec.is_published = True
+                rec.website_published = True
             else:
-                rec.is_published = False
+                rec.website_published = False
 
     to_recruit = fields.Integer(string="To be recruited", compute="_compute_to_recruit")
