dec5427d74252766ed00638bb1a28e678cf88d2d
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-03-07 12:32:49 -0600

    [FIX] hr_contract

--- hr_recruitment_torecruit/models/hr_contract.py
+++ hr_recruitment_torecruit/models/hr_contract.py
@@ -1,11 +1,13 @@
-from odoo import api, models
+from odoo import models
 
 
 class HrContract(models.Model):
     _inherit = "hr.contract"
 
-    @api.onchange("state")
-    def _onchange_state(self):
-        job_obj = self.env["hr.job"].search([("to_recruit", ">", 0)])
-        for job in job_obj:
-            job._compute_to_recruit()
+    def write(self, vals):
+        res = super().write(vals)
+        if "state" in vals:
+            job_obj = self.env["hr.job"].search([("to_recruit", ">", 0)])
+            for job in job_obj:
+                job._compute_to_recruit()
+        return res
