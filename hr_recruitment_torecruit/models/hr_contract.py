0f4ebea153d70596ee2ce6455bbf1af2b2aa88a8
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-03-07 12:44:04 -0600

    [FIX] hr_contract

--- hr_recruitment_torecruit/models/hr_contract.py
+++ hr_recruitment_torecruit/models/hr_contract.py
@@ -7,7 +7,6 @@ class HrContract(models.Model):
     def write(self, vals):
         res = super().write(vals)
         if "state" in vals:
-            job_obj = self.env["hr.job"].search([("to_recruit", ">", 0)])
-            for job in job_obj:
-                job._compute_to_recruit()
+            job_obj = self.job_id.filtered(lambda j: j.to_recruit > 0)
+            job_obj._compute_to_recruit()
         return res
