310873017df599c237707d1449b5a475e8db9d7f
Christoph Abenthung <christoph.abenthung@cibex.net>
2024-03-25 11:17:58 +0100

    [FIX]hr_employee_calendar_planning: only necessary leaves
    Only create and unlink necessary resource_calendar_leaves
    
    [BOT] post-merge updates

--- hr_employee_calendar_planning/__manifest__.py
+++ hr_employee_calendar_planning/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Employee Calendar Planning",
-    "version": "16.0.1.1.6",
+    "version": "16.0.1.1.7",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr",
     "author": "Tecnativa,Odoo Community Association (OCA)",
