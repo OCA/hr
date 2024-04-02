f0999ec5fbffb546b3e99b5dd5914c996434d922
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-04-02 08:50:43 -0600

    [ADD] hr_employee_number_generate_and_search

---
+++ hr_employee_number_generate_and_search/models/__init__.py
@@ -0,0 +1,6 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import hr_employee
+from . import res_company
+from . import hr_contract_history
+from . import res_config_settings
