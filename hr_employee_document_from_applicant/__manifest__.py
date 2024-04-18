9862f9d1a4ec539f4f63d2a903e0a5f21308766c
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-04-18 11:12:55 -0600

    [FIX] hr_employee_document_from_applicant

---
+++ hr_employee_document_from_applicant/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 Open Source Integrators (https://www.opensourceintegrators.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "HR Employee Document from Applicant",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "HR Employee Document from Applicant",
+    "author": "Odoo Community Association (OCA), Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr_employee_document", "hr_recruitment"],
+    "data": [],
+    "maintainers": ["ursais"],
+}
---
+++ hr_employee_document_from_applicant/readme/USAGE.rst
@@ -0,0 +1,7 @@
+* Go to Recruitment
+* Create a job position and a candidate
+* Attach documents to the candidate
+* Move the candidate through the recruitment process until the "Create
+employee" button appears to click on it
+* Go to the newly created employee: all the documents of the candidate are
+available
