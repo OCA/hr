f9de8fb3de49e892126dcb33dbc6490d705eea8b
Duy (Đỗ Anh) <duyda@trobz.com>
2024-08-08 18:08:26 +0700

    [IMP] hr_work_entry_profile: pre-commit auto fixes

--- hr_work_entry_profile/models/res_users.py
+++ hr_work_entry_profile/models/res_users.py
@@ -1,10 +1,12 @@
-from odoo import models, fields
 import logging
+
+from odoo import models
+
 _logger = logging.getLogger(__name__)
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     def action_open_work_entries(self):
         self.ensure_one()
