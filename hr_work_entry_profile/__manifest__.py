ebd0cb8ed1cd21940bb71d56b95dfc298e1262ee
Duy (Đỗ Anh) <duyda@trobz.com>
2024-08-08 18:08:26 +0700

    [IMP] hr_work_entry_profile: pre-commit auto fixes

--- hr_work_entry_profile/__manifest__.py
+++ hr_work_entry_profile/__manifest__.py
@@ -4,7 +4,7 @@
         User can access their work entries from the profile view.
     """,
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
-    "website": "https://www.mint-system.ch",
+    "website": "https://github.com/OCA/hr",
     "category": "Human Resources",
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
