9fe6c3f4f5ff1af94bdf5491a5b007ca892e1baa
Duy (Đỗ Anh) <duyda@trobz.com>
2024-08-08 19:20:15 +0700

    [MIG] hr_work_entry_profile: Migration to 17.0

--- hr_work_entry_profile/__manifest__.py
+++ hr_work_entry_profile/__manifest__.py
@@ -1,3 +1,6 @@
+# Copyright 2023 Janik von Rotz <janik.vonrotz@mint-system.ch>
+# Copyright 2024 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "HR Work Entry Profile",
     "summary": """
@@ -6,7 +9,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
     "category": "Human Resources",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["hr_work_entry"],
     "data": ["views/res_users.xml"],
---
+++ hr_work_entry_profile/readme/CREDITS.md
@@ -0,0 +1,4 @@
+The original development of this module has been done by Mint System.
+It can be found in: https://github.com/Mint-System/Odoo-Apps-HR/blob/15.0/hr_work_entry_profile
+
+This module has been ported to the OCA with their agreement.
