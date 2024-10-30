364678b866400e7dda82954b6ab3750a377c0907
Edilianny Sánchez <edilianny@vauxoo.com>
2024-10-30 18:56:56 +0000

    [FIX] hr_employee_firstname: fix DeprecationWarning
    Run the README generator to fix the warning message in the log:
    
        DeprecationWarning: XML declarations in HTML module
        descriptions are deprecated since Odoo 17,
        hr_employee_firstname can just have a UTF8 description
        with not need for a declaration.

--- hr_employee_firstname/__manifest__.py
+++ hr_employee_firstname/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR Employee First Name, Last Name",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "author": "Savoir-faire Linux, "
     "Fekete Mihai (Forest and Biomass Services Romania), "
     "Onestein, "
