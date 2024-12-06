f23617fe05f5f165a6b76f97a8e300c1676e26f6
Vang - Nguyen Phu <vang-np@komit-consulting.com>
2024-12-06 16:56:16 +0700

    [MIG] hr_employee_age: Migration to 17.0

--- hr_employee_age/tests/test_hr_employee.py
+++ hr_employee_age/tests/test_hr_employee.py
@@ -3,14 +3,16 @@
 from dateutil.relativedelta import relativedelta
 
 from odoo import fields
-from odoo.tests import common
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestHrEmployee(common.TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.employee_admin = self.env.ref("hr.employee_admin")
-        self.employee_admin.write({"birthday": "1990-05-15"})
+class TestHrEmployee(BaseCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.employee_admin = cls.env.ref("hr.employee_admin")
+        cls.employee_admin.write({"birthday": "1990-05-15"})
 
     def test_compute_age(self):
         self.employee_admin._compute_age()
