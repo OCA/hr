7c5171a6a481c7ec21c1485bfbcc61990c6edad7
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2025-02-24 16:27:23 +0100

    [MIG] hr_employee_birthday_mail: Migration to 18.0

--- hr_employee_birthday_mail/__manifest__.py
+++ hr_employee_birthday_mail/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": """
         Automating birthday mail messages and fostering for a positive work environment.
         """,
-    "version": "17.0.1.1.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
