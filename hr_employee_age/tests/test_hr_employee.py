e29968994fda4a96260cc2b54393dab111d8d9d7
hitrosol <ppakpahan@solusiaglis.co.id>
2025-03-03 17:23:25 +0700

    [18.0][MIG] hr_employee_age: Migration to 18.0

--- hr_employee_age/tests/test_hr_employee.py
+++ hr_employee_age/tests/test_hr_employee.py
@@ -1,8 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from dateutil.relativedelta import relativedelta
-
-from odoo import fields
+from freezegun import freeze_time
 
 from odoo.addons.base.tests.common import BaseCommon
 
@@ -14,7 +12,7 @@ class TestHrEmployee(BaseCommon):
         cls.employee_admin = cls.env.ref("hr.employee_admin")
         cls.employee_admin.write({"birthday": "1990-05-15"})
 
+    @freeze_time("2024-05-15")
     def test_compute_age(self):
         self.employee_admin._compute_age()
-        age = relativedelta(fields.Date.today(), self.employee_admin.birthday).years
-        self.assertEqual(self.employee_admin.age, age)
+        self.assertEqual(self.employee_admin.age, 34)
