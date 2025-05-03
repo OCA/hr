cb50a412be8c54444741dc5e1138b8c49da7bcc1
Gil Arasa Verge <gav@wvbs.eu>
2025-05-03 17:13:16 +0200

    [MIG] passport_expiration: Migration to 17.0

--- passport_expiration/__manifest__.py
+++ passport_expiration/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Passport Expiration",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
     "license": "AGPL-3",
