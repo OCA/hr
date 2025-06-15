7ee161bf2543bf1cffa3a19832d81931660438f8
Stefano Consolaro <stefano.consolaro@mymage.it>
2025-06-15 15:01:19 +0200

    [MIG] hr_employee_language: Migration to 16.0

--- hr_employee_language/models/hr_employee_language.py
+++ hr_employee_language/models/hr_employee_language.py
@@ -8,29 +8,10 @@ class HrEmployeeLanguage(models.Model):
     _name = "hr.employee.language"
     _description = "HR Employee Language"
 
-    name = fields.Selection(
-        tools.scan_languages(),
-        string="Language",
-        required=True)
+    name = fields.Selection(tools.scan_languages(), string="Language", required=True)
     description = fields.Char()
-    employee_id = fields.Many2one(
-        "hr.employee",
-        string="Employee",
-        required=True
-        )
-    can_read = fields.Boolean(
-        string="Read",
-        default=True
-        )
-    can_write = fields.Boolean(
-        string="Write",
-        default=True
-        )
-    can_speak = fields.Boolean(
-        string="Speak",
-        default=True
-        )
-    can_listen = fields.Boolean(
-        string="Listen",
-        default=True
-        )
+    employee_id = fields.Many2one("hr.employee", string="Employee", required=True)
+    can_read = fields.Boolean(string="Read", default=True)
+    can_write = fields.Boolean(string="Write", default=True)
+    can_speak = fields.Boolean(string="Speak", default=True)
+    can_listen = fields.Boolean(string="Listen", default=True)
