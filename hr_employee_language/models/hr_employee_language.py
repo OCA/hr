632e6bf70534c263275f504c8a37805363c6bc28
Stefano Consolaro <stefano.consolaro@mymage.it>
2025-06-15 14:19:20 +0200

    [IMP] hr_employee_language: black, isort, prettier

--- hr_employee_language/models/hr_employee_language.py
+++ hr_employee_language/models/hr_employee_language.py
@@ -8,10 +8,29 @@ class HrEmployeeLanguage(models.Model):
     _name = "hr.employee.language"
     _description = "HR Employee Language"
 
-    name = fields.Selection(tools.scan_languages(), string="Language", required=True)
-    description = fields.Char(size=64)
-    employee_id = fields.Many2one("hr.employee", string="Employee", required=True)
-    can_read = fields.Boolean(string="Read", default=True)
-    can_write = fields.Boolean(string="Write", default=True)
-    can_speak = fields.Boolean(string="Speak", default=True)
-    can_listen = fields.Boolean(string="Listen", default=True)
+    name = fields.Selection(
+        tools.scan_languages(),
+        string="Language",
+        required=True)
+    description = fields.Char()
+    employee_id = fields.Many2one(
+        "hr.employee",
+        string="Employee",
+        required=True
+        )
+    can_read = fields.Boolean(
+        string="Read",
+        default=True
+        )
+    can_write = fields.Boolean(
+        string="Write",
+        default=True
+        )
+    can_speak = fields.Boolean(
+        string="Speak",
+        default=True
+        )
+    can_listen = fields.Boolean(
+        string="Listen",
+        default=True
+        )
