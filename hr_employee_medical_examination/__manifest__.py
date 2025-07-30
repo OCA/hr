0d84b74e1d9029021c4c417eb3685d7883becbeb
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-30 08:32:57 +0200

    [IMP] hr_employee_medical_examination: Edit the model description to remove "HR"
    Related to https://github.com/OCA/hr/pull/1490#pullrequestreview-3068562926
    
    TT57402

--- hr_employee_medical_examination/__manifest__.py
+++ hr_employee_medical_examination/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Hr Employee Medical Examination",
+    "name": "Employee Medical Examination",
     "summary": """
         Adds information about employee's medical examinations""",
     "version": "18.0.1.0.1",
