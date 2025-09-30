dcb3eb6b64efc7a79f245f0aed9a192ada769a46
Ruchir Shukla <ruchir@bizzappdev.com>
2025-09-30 22:29:47 +0530

    [MIG] hr_department_code: Migration to 19.0

--- hr_department_code/__manifest__.py
+++ hr_department_code/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR department code",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "category": "Human Resources",
     "author": "Ecosoft, Odoo Community Association (OCA)",
