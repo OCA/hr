d2e5fe7598ef89fd0deaabcad842edcc7403dd28
Ruchir Shukla <ruchir@bizzappdev.com>
2025-10-01 11:52:54 +0530

    [MIG] hr_employee_second_lastname: Migration to 19.0

--- hr_employee_second_lastname/models/__init__.py
+++ hr_employee_second_lastname/models/__init__.py
@@ -1,2 +1,1 @@
-from . import hr_employee_base
 from . import hr_employee
--- hr_employee_second_lastname/models/hr_employee_base.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class HrEmployeeBase(models.AbstractModel):
-    _inherit = "hr.employee.base"
-
-    firstname = fields.Char("First name")
-    lastname = fields.Char("Last name")
-    lastname2 = fields.Char("Second last name")
