0edcc09a2d5e0b3146625b32c295ac5666080bef
Bhavesh Heliconia <oca@heliconia.in>
2025-12-22 15:10:37 +0530

    [MIG] hr_employee_partner_external: Migration to 19.0

--- hr_employee_partner_external/__manifest__.py
+++ hr_employee_partner_external/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Employee external Partner",
     "summary": "Associate an external Partner to Employee",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Human Resources",
     "author": "Stefano Consolaro Associazione PNLUG - Gruppo Odoo, "
     "Odoo Community Association (OCA)",
