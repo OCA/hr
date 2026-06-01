ab754d65f26eab695bfb6ac6ac200c321ffcc361
Manfred Nelvin H <manfred2002nelvin@gmail.com>
2026-06-01 10:32:34 +0530

    [MIG] hr_announcement: Migration to 18.0
    [FIX] hr_announcement: Use model_create_multi
    
    [TEST] hr_announcement: Add functional tests
    
    [FIX] hr_announcement: Fix pre-commit issues

--- hr_announcement/__manifest__.py
+++ hr_announcement/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Announcement",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
