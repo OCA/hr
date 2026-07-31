e14400951c16523206e13327dfc72d6e9e1003b9
Víctor Martínez <victor.martinez@tecnativa.com>
2026-07-31 16:36:36 +0200

    [IMP] hr_employee_calendar_planning: Add flexible_hours compatibility
    TT62028

--- hr_employee_calendar_planning/hooks.py
+++ hr_employee_calendar_planning/hooks.py
@@ -4,6 +4,35 @@
 from collections import defaultdict
 
 
+def pre_init_hook(env):
+    env.cr.execute(
+        """
+        ALTER TABLE resource_calendar
+        ADD COLUMN IF NOT EXISTS stored_flexible_hours BOOL
+        """,
+    )
+    env.cr.execute(
+        """
+        ALTER TABLE resource_calendar
+        ADD COLUMN IF NOT EXISTS stored_full_time_required_hours double precision
+        """,
+    )
+    env.cr.execute(
+        """
+        ALTER TABLE resource_calendar
+        ADD COLUMN IF NOT EXISTS stored_hours_per_day double precision
+        """,
+    )
+    env.cr.execute(
+        """
+        UPDATE resource_calendar
+        SET stored_flexible_hours = flexible_hours,
+            stored_full_time_required_hours = full_time_required_hours,
+            stored_hours_per_day = hours_per_day
+        """,
+    )
+
+
 def post_init_hook(env, employees=None):
     """Split current calendars by date ranges and assign new ones for
     having proper initial data.
--- hr_employee_calendar_planning/models/resource_calendar.py
+++ hr_employee_calendar_planning/models/resource_calendar.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Tecnativa - Pedro M. Baeza
-# Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2021-2026 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
@@ -11,7 +11,159 @@ class ResourceCalendar(models.Model):
 
     active = fields.Boolean(default=True)
     auto_generate = fields.Boolean()
+    employee_ids = fields.One2many(
+        "hr.employee",
+        "resource_calendar_id",
+    )
     employee_calendar_ids = fields.One2many("hr.employee.calendar", "calendar_id")
+    # flexible_hours compatibility
+    # For the flexible_hours, full_time_required_hours, and hours_per_day fields
+    # to function correctly, you must create three fields:
+    # - stored_flexible_hours: This field will behave the same as the flexible_hours
+    # field
+    # - stored_full_time_required_hours: This field will behave the same as the
+    # full_time_required_hours field
+    # - stored_hours_per_day: This will behave the same as the hours_per_day field
+    # The flexible_hour and full_time_required_hours fields are converted to compute
+    # fields with store=False to ensure the correct stored_* field is used, except
+    # when the calendar is auto_generated and dates are passed via context; in that
+    # case, the appropriate value will be defined.
+    stored_flexible_hours = fields.Boolean(
+        help="When enabled, it will allow employees to work flexibly, without relying"
+        " on the company's working schedule (working hours)."
+    )
+    stored_full_time_required_hours = fields.Float(
+        help="Number of hours to work on the company schedule to be considered as"
+        " fulltime."
+    )
+    stored_hours_per_day = fields.Float(
+        store=True,
+        compute="_compute_stored_hours_per_day",
+        digits=(2, 2),
+        readonly=False,
+        help="Average hours per day a resource is supposed to work with this calendar.",
+    )
+    flexible_hours = fields.Boolean(
+        compute="_compute_flexible_hours",
+    )
+    full_time_required_hours = fields.Float(
+        compute="_compute_full_time_required_hours",
+    )
+    hours_per_day = fields.Float(store=False)
+
+    @api.depends(
+        "auto_generate",
+        "employee_ids.calendar_ids",
+        "employee_ids.calendar_ids.calendar_id.stored_flexible_hours",
+    )
+    @api.depends_context(
+        "flexible_hours_from_date",
+        "flexible_hours_to_date",
+    )
+    def _compute_flexible_hours(self):
+        """The value of stored_flexible_hours is defined unless the calendar is
+        auto_generate; in that case, the hours for the applicable calendars will
+        be defined based on the dates specified by context.
+        """
+        for item in self:
+            flexible_hours = item.stored_flexible_hours
+            if item.auto_generate:
+                from_date = self.env.context.get("flexible_hours_from_date")
+                to_date = self.env.context.get("flexible_hours_to_date")
+                employee = fields.first(item.employee_ids)
+                if (from_date or to_date) and employee:
+                    calendars = employee._get_planning_calendars(from_date, to_date)
+                    flexible_hours = (
+                        any(c.stored_flexible_hours for c in calendars.calendar_id)
+                        if calendars
+                        else False
+                    )
+            item.flexible_hours = flexible_hours
+
+    @api.depends(
+        "auto_generate",
+        "flexible_hours",
+        "employee_ids.calendar_ids",
+        "employee_ids.calendar_ids.calendar_id.stored_flexible_hours",
+        "employee_ids.calendar_ids.calendar_id.stored_full_time_required_hours",
+    )
+    @api.depends_context(
+        "flexible_hours_from_date",
+        "flexible_hours_to_date",
+    )
+    def _compute_full_time_required_hours(self):
+        """The value of full_time_required_hours is defined unless the calendar is
+        auto_generate; in that case, the hours for the applicable calendars will
+        be defined based on the dates specified by context.
+        """
+        for item in self:
+            hours = item.stored_full_time_required_hours
+            if item.auto_generate and item.flexible_hours:
+                from_date = self.env.context.get("flexible_hours_from_date")
+                to_date = self.env.context.get("flexible_hours_to_date")
+                employee = fields.first(item.employee_ids)
+                if (from_date or to_date) and employee:
+                    calendars = employee._get_planning_calendars(from_date, to_date)
+                    hours = (
+                        (
+                            sum(
+                                c.stored_full_time_required_hours
+                                for c in calendars.calendar_id
+                            )
+                        )
+                        if calendars
+                        else False
+                    )
+            item.full_time_required_hours = hours
+
+    @api.depends(
+        "attendance_ids",
+        "attendance_ids.hour_from",
+        "attendance_ids.hour_to",
+        "two_weeks_calendar",
+        "flexible_hours",
+    )
+    def _compute_stored_hours_per_day(self):
+        """This method is the same as _compute_hours_per_day() in the resource
+        module.
+        """
+        for item in self:
+            if item.flexible_hours:
+                continue
+            attendances = item._get_global_attendances()
+            item.stored_hours_per_day = item._get_hours_per_day(attendances)
+
+    @api.depends(
+        "auto_generate",
+        "flexible_hours",
+        "employee_ids.calendar_ids",
+        "employee_ids.calendar_ids.calendar_id.stored_flexible_hours",
+        "employee_ids.calendar_ids.calendar_id.stored_hours_per_day",
+    )
+    @api.depends_context(
+        "flexible_hours_from_date",
+        "flexible_hours_to_date",
+    )
+    def _compute_hours_per_day(self):
+        """The value of hours_per_day is defined unless the calendar is auto_generate;
+        in that case, the hours for the applicable calendars will be defined based
+        on the dates specified by context.
+        """
+        res = super()._compute_hours_per_day()
+        for item in self.filtered(lambda x: x.auto_generate and x.flexible_hours):
+            from_date = self.env.context.get("flexible_hours_from_date")
+            to_date = self.env.context.get("flexible_hours_to_date")
+            employee = fields.first(item.employee_ids)
+            hours = item.stored_hours_per_day
+            if (from_date or to_date) and employee:
+                calendars = employee._get_planning_calendars(from_date, to_date)
+                hours = (
+                    (sum(c.stored_hours_per_day for c in calendars.calendar_id))
+                    if calendars
+                    else False
+                )
+            item.hours_per_day = hours
+        return res
 
     @api.constrains("active")
     def _check_active(self):
@@ -65,3 +217,40 @@ class ResourceCalendar(models.Model):
                 for employee in calendars.mapped("employee_id"):
                     employee._regenerate_calendar()
         return res
+
+    def _attendance_intervals_batch(
+        self, start_dt, end_dt, resources=None, domain=None, tz=None, lunch=False
+    ):
+        # It is important to define the appropriate context keys so that the value is
+        # as expected.
+        flexible_hours_from_date = start_dt.date()
+        flexible_hours_to_date = end_dt.date()
+        self = self.with_context(
+            flexible_hours_from_date=flexible_hours_from_date,
+            flexible_hours_to_date=flexible_hours_to_date,
+        )
+        resources = (
+            resources.with_context(
+                flexible_hours_from_date=flexible_hours_from_date,
+                flexible_hours_to_date=flexible_hours_to_date,
+            )
+            if resources
+            else None
+        )
+        return super()._attendance_intervals_batch(
+            start_dt=start_dt,
+            end_dt=end_dt,
+            resources=resources,
+            domain=domain,
+            tz=tz,
+            lunch=lunch,
+        )
+
+    def _get_unusual_days(self, start_dt, end_dt, company_id=False):
+        # It is important to define the appropriate context keys so that the value is
+        # as expected.
+        self = self.with_context(
+            flexible_hours_from_date=start_dt.date(),
+            flexible_hours_to_date=end_dt.date(),
+        )
+        return super()._get_unusual_days(start_dt, end_dt, company_id)
