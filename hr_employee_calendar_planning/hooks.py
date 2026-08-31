9f6e7e838ea45a536418abfae66b04afc3658512
Víctor Martínez <victor.martinez@tecnativa.com>
2026-08-31 14:25:35 +0200

    [IMP] hr_employee_calendar_planning: Add post_init_hook
    Related to https://github.com/OCA/hr/pull/1540#discussion_r3881646899

--- hr_employee_calendar_planning/hooks.py
+++ hr_employee_calendar_planning/hooks.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.fields import Command
 
 
 def pre_init_hook(env):
@@ -29,3 +30,32 @@ def pre_init_hook(env):
             stored_hours_per_day = hours_per_day
         """,
     )
+
+
+def post_init_hook(env, employees=None):
+    """Set the appropriate calendar for employees who do not yet have one."""
+    employee_model = env["hr.employee"].with_context(active_test=False)
+    if not employees:
+        employees = employee_model.search([])
+    employees = employee_model.search(
+        [("id", "in", employees.ids), ("calendar_ids", "=", False)]
+    )
+    for employee in employees:
+        leaves = employee.version_ids.resource_calendar_id.leave_ids.filtered(
+            lambda x, e=employee: x.resource_id == e.resource_id
+        )
+        calendar_data = [
+            Command.create(
+                {
+                    "date_start": version.date_start,
+                    "date_end": version.date_end,
+                    "calendar_id": version.resource_calendar_id.id,
+                }
+            )
+            for version in employee.version_ids
+        ]
+        employee.write({"calendar_ids": calendar_data})
+        employee.copy_global_leaves()
+        # Now the automatic calendar has been created, so we link the
+        # leaves to that one so they count correctly.
+        leaves.write({"calendar_id": employee.resource_calendar_id.id})
